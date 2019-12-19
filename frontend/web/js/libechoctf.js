153980769dff6c253ae038f582d79d8bb4702339
Pantelis Roditis <proditis@echothrust.com>
2019-12-19 12:39:51 +0200

    make empty hints and notifications show something

--- frontend/web/js/libechoctf.js
+++ frontend/web/js/libechoctf.js
@@ -41,75 +41,86 @@ jQuery( document ).ready(function() {
 
   function processHints( data,textStatus,jqXHR )
   {
-      for(i=0;i<data.length;i++)
-      {
-        var thelink = $('<a />',{
-            class: "dropdown-item",
-            text: data[i].title,
-            title: data[i].title,
-            href: '#'
-          });
-          if(parseInt(data[i].status)===1)
-          {
-            thelink.addClass("text-success");
-            thelink.prepend('<i class="material-icons">whatshot</i> ');
-          }
-          thelink.appendTo('#hintsMenu');
-      }
-      total_pages=parseInt( jqXHR.getResponseHeader('X-Pagination-Page-Count'), 0 );
-      current_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Current-Page'), 0 );
-      per_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Per-Page'), 0 );
-      total_recs=parseInt( jqXHR.getResponseHeader('X-Pagination-Total-Count'), 0 );
-      if(total_pages>current_page)
-      {
-        var theLink=$('<a>',{
-          class: 'dropdown-item',
-          text: "Load More",
-          href: $('#navbarHintsDropDown').attr('href')+'?playerHint-page='+parseInt(current_page+1)
-        })
-        .on('click',function(){
-          $(this).remove();
-          $.get($(this).attr('href'),processHints);
-          return false;
-        })
-        .appendTo('#gintsMenu');
-      }
+    if(data.length==0)
+    {
+      $('#hintsMenu').prepend('<p class="dropdown-item">nothing here yet</p>');
+      return;
     }
-  /* Generic Notifications Handler */
-  function processNotifications( data,textStatus,jqXHR )
-  {
-      for(i=0;i<data.length;i++)
-      {
-        var thelink = $('<a>',{
-            class: "dropdown-item",
-            text: data[i].title,
-            title: data[i].title,
-            href: '#'
-          });
-        if(parseInt(data[i].archived)===0)
+
+    for(i=0;i<data.length;i++)
+    {
+      var thelink = $('<a />',{
+          class: "dropdown-item",
+          text: data[i].title,
+          title: data[i].title,
+          href: '#'
+        });
+        if(parseInt(data[i].status)===1)
         {
           thelink.addClass("text-success");
           thelink.prepend('<i class="material-icons">whatshot</i> ');
         }
-        thelink.appendTo('#notificationsMenu');
-      }
-      total_pages=parseInt( jqXHR.getResponseHeader('X-Pagination-Page-Count'), 0 );
-      current_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Current-Page'), 0 );
-      per_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Per-Page'), 0 );
-      total_recs=parseInt( jqXHR.getResponseHeader('X-Pagination-Total-Count'), 0 );
-      if(total_pages>current_page)
-      {
-        var theLink=$('<a>',{
-          class: 'dropdown-item',
-          text: "Load More",
-          href: $('#navbarDropdownMenuLink').attr('href')+'?notifications-page='+parseInt(current_page+1)
-        })
-        .on('click',function(){
-          $(this).remove();
-          $.get($(this).attr('href'),processNotifications);
-          return false;
-        })
-        .appendTo('#notificationsMenu');
-      }
+        thelink.appendTo('#hintsMenu');
     }
+    total_pages=parseInt( jqXHR.getResponseHeader('X-Pagination-Page-Count'), 0 );
+    current_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Current-Page'), 0 );
+    per_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Per-Page'), 0 );
+    total_recs=parseInt( jqXHR.getResponseHeader('X-Pagination-Total-Count'), 0 );
+    if(total_pages>current_page)
+    {
+      var theLink=$('<a>',{
+        class: 'dropdown-item',
+        text: "Load More",
+        href: $('#navbarHintsDropDown').attr('href')+'?playerHint-page='+parseInt(current_page+1)
+      })
+      .on('click',function(){
+        $(this).remove();
+        $.get($(this).attr('href'),processHints);
+        return false;
+      })
+      .appendTo('#gintsMenu');
+    }
+  }
+  /* Generic Notifications Handler */
+  function processNotifications( data,textStatus,jqXHR )
+  {
+    if(data.length==0)
+    {
+      $('#notificationsMenu').prepend('<p class="dropdown-item">nothing here yet</p>');
+      return;
+    }
+    for(i=0;i<data.length;i++)
+    {
+      var thelink = $('<a>',{
+          class: "dropdown-item",
+          text: data[i].title,
+          title: data[i].title,
+          href: '#'
+        });
+      if(parseInt(data[i].archived)===0)
+      {
+        thelink.addClass("text-success");
+        thelink.prepend('<i class="material-icons">whatshot</i> ');
+      }
+      thelink.appendTo('#notificationsMenu');
+    }
+    total_pages=parseInt( jqXHR.getResponseHeader('X-Pagination-Page-Count'), 0 );
+    current_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Current-Page'), 0 );
+    per_page=parseInt( jqXHR.getResponseHeader('X-Pagination-Per-Page'), 0 );
+    total_recs=parseInt( jqXHR.getResponseHeader('X-Pagination-Total-Count'), 0 );
+    if(total_pages>current_page)
+    {
+      var theLink=$('<a>',{
+        class: 'dropdown-item',
+        text: "Load More",
+        href: $('#navbarDropdownMenuLink').attr('href')+'?notifications-page='+parseInt(current_page+1)
+      })
+      .on('click',function(){
+        $(this).remove();
+        $.get($(this).attr('href'),processNotifications);
+        return false;
+      })
+      .appendTo('#notificationsMenu');
+    }
+  }
 });
