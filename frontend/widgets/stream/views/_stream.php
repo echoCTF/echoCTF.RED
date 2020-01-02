21bbe49d65c384d5ab81251afc7782781a36e731
Pantelis Roditis <proditis@echothrust.com>
2020-01-02 16:56:35 +0200

    add some debug information on the stream

--- frontend/widgets/stream/views/_stream.php
+++ frontend/widgets/stream/views/_stream.php
@@ -5,8 +5,8 @@ use app\widgets\Twitter;
 <div class="leader">
     <div class="leader-wrap">
       <div class="leader-name" style="margin-bottom: 0.3em">
-        <?=$model->formatted;?>, <?=$model->ts_ago?>
-         <?=Twitter::widget(['message'=>'Hey check this out'.strip_tags($model->formatted.', '.$model->ts_ago)]);?>
+        <!--<?=$model->id?>--> <?=$model->formatted;?>, <?=$model->ts_ago?>
+         <?=Twitter::widget(['message'=>'Hey check this out'.strip_tags($model->formatted.', '.$model->ts_ago)]);?><!--<?=$model->ts?>-->
       </div>
     </div>
     <div class="border"></div>
