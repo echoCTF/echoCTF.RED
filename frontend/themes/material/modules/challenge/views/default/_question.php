f17a6e727b0842eccf809f788355f6809952404d
Pantelis Roditis <proditis@echothrust.com>
2020-01-03 11:30:34 +0200

    make question points a bit more presentable

--- frontend/themes/material/modules/challenge/views/default/_question.php
+++ frontend/themes/material/modules/challenge/views/default/_question.php
@@ -5,6 +5,6 @@ use app\widgets\Twitter;
 <div class="question-view">
   <details>
   <?php echo $model->description; ?>
-  <summary><b id="question-<?=$index+1?>">Q<?php echo $index+1?>: <?php echo Html::encode($model->name); ?> (<?php echo $model->points?>) <?php if($model->answered!=null):?><i class="fas fa-check"></i><?php endif;?></b></summary>
+  <summary><b id="question-<?=$index+1?>">Q<?php echo $index+1?>: <?php echo Html::encode($model->name); ?> (<small><?php echo $model->points?> pts</small>) <?php if($model->answered!=null):?><i class="fas fa-check"></i><?php endif;?></b></summary>
   </details>
 </div>
