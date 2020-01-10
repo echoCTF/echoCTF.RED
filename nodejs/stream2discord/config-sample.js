cf7e336963d5e9813760660db8a74cd4b2a2f036
Pantelis Roditis <proditis@echothrust.com>
2020-01-10 10:54:01 +0200

    add nodejs to post stream updates through webhook

---
+++ nodejs/stream2discord/config-sample.js
@@ -0,0 +1,8 @@
+var config = {};
+config.db_host="localhost";
+config.db_user="root";
+config.db_pass="";
+config.db_name="ets_ctf";
+config.lastID=0;
+config.DiscordHook="https://discordapp.com/api/webhooks/"
+module.exports = config;
