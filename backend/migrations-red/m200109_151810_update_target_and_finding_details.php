16996c317c454dcee4d2bcbb1ab2f78da13ae71f
Pantelis Roditis <proditis@echothrust.com>
2020-01-20 11:13:28 +0200

    minor updates for the migration
    Removed updates that have been applied by hand.

--- backend/migrations-red/m200109_151810_update_target_and_finding_details.php
+++ backend/migrations-red/m200109_151810_update_target_and_finding_details.php
@@ -62,28 +62,11 @@ int main(int argc, char **argv) {
         </p>
       </details> <details> <summary>References</summary> <ul> <li><a target="_blank" href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-1010174">CVE-2019-1010174</a> <li><a target="_blank" href="https://framagit.org/dtschump/CImg/commit/5ce7a426b77f814973e56182a0e76a2b04904146">Fix commit</a> </ul></details>
 ENDHTML;
-    $this->update('target',['image'=>'registry.echoctf.com:5000/red/lfi-tutorial:v0.9'],['id'=>26]);
-    $this->update('target',['image'=>'registry.echoctf.com:5000/red/cve-2019-1010174:v0.9'],['id'=>23]);
-    $this->update('target',['description'=>$DESCRIPTION],['id'=>23]);
-    $this->db->createCommand("insert into player_score (player_id,points) select player_id,sum(points) from stream group by player_id ON DUPLICATE KEY UPDATE points=values(points)")->execute();
-    $this->db->createCommand("CALL calculate_ranks()")->execute();
-
-    $this->update('target',['description'=>'<p>Chef is generally more level-headed than the other adult residents of the
-town, and quite sympathetic to the children that speak to him. His advice is
-always to the point, with focus on solving the <code>root</code> cause of the
-problem. See if you can manage to speak to him, in the right way and gain
-entrance to his system.</p>
-<p><small>This is Chef\'s own workstation, he likes nodejs and webmin. Chef is
-not a noob, he knows better than to run webmin on a public port and he always
-uses random passwords (so forget about brute-forcing your way)...</small></p>'],['id'=>19]); // chef
-
-//    $this->update('target',['description'=>''],['id'=>22]); // harrison
-    $this->update('target',['description'=>'<p>Tweek has setup his own blog to share his thoughts on school and the latest adventures of <i>Wonder Tweek</i>.</p>
-<p><small>His blog is closely associated with Craig\'s, since both systems are installed by Wendy. And those who know Wendy, know that she likes her drupal crontabs executed every minute on the spot, by the infamous <code>DrupalConsole</code>...</small><p>'],['id'=>24]); // tweek
-    $this->update('target',['description'=>'<p>Craig has setup his own blog to share his thoughts on school and the latest adventures of <i>Super Craig</i>.</p>
-<p><small>His blog is closely associated with Tweek\'s, since both systems are installed by Wendy. And those who know Wendy, know that she likes her drupal crontabs executed every minute on the spot, by the infamous <code>DrupalConsole/</code>...</small><p>'],['id'=>25]); // craig
-
-
+      $this->update('target',['image'=>'registry.echoctf.com:5000/red/lfi-tutorial:v0.9'],['id'=>26]);
+      $this->update('target',['image'=>'registry.echoctf.com:5000/red/cve-2019-1010174:v0.9'],['id'=>23]);
+      $this->update('target',['description'=>$DESCRIPTION],['id'=>23]);
+      $this->db->createCommand("insert into player_score (player_id,points) select player_id,sum(points) from stream group by player_id ON DUPLICATE KEY UPDATE points=values(points)")->execute();
+      $this->db->createCommand("CALL calculate_ranks()")->execute();
     }
 
     /**
@@ -91,7 +74,6 @@ uses random passwords (so forget about brute-forcing your way)...</small></p>'],
      */
     public function safeDown()
     {
-      $this->update('target',['image'=>'registry.echoctf.com:5000/red/lfi-tutorial:v0.7.1'],['id'=>26]);
 
     }
 }
