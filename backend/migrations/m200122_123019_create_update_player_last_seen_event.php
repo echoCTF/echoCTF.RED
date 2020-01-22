a87b221d0859032025a4ed2fedcb11f5675b010b
Pantelis Roditis <proditis@echothrust.com>
2020-01-22 17:10:57 +0200

    make sure we store the last_seen key in appropriate format Pass the value of the key `last_seen`, through FROM_UNIXTIME() so that we dont get wrong dates

--- backend/migrations/m200122_123019_create_update_player_last_seen_event.php
+++ backend/migrations/m200122_123019_create_update_player_last_seen_event.php
@@ -9,7 +9,7 @@ class m200122_123019_create_update_player_last_seen_event extends Migration
 {
     public $DROP_SQL="DROP EVENT IF EXISTS {{%update_player_last_seen}}";
     public $CREATE_SQL="CREATE EVENT {{%update_player_last_seen}} ON SCHEDULE EVERY 1 HOUR ON COMPLETION PRESERVE ENABLE DO BEGIN
-     UPDATE `player_last` SET `on_pui`=memc_get(CONCAT('last_seen:',id)) WHERE memc_get(CONCAT('last_seen:',id)) IS NOT NULL;
+     UPDATE `player_last` SET `on_pui`=FROM_UNIXTIME(memc_get(CONCAT('last_seen:',id))) WHERE memc_get(CONCAT('last_seen:',id)) IS NOT NULL;
     END";
     public function up()
     {
