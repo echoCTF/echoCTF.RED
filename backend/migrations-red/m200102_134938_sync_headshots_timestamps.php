49b9cfbac725604b5e33b08c6128583b3584e9a4
Pantelis Roditis <proditis@echothrust.com>
2020-01-27 16:25:50 +0200

    ensure migration acts in UTC timezone both on php and mysql

--- backend/migrations-red/m200102_134938_sync_headshots_timestamps.php
+++ backend/migrations-red/m200102_134938_sync_headshots_timestamps.php
@@ -15,11 +15,13 @@ class m200102_134938_sync_headshots_timestamps extends Migration
      */
     public function safeUp()
     {
+      date_default_timezone_set('UTC');
+      $this->db->createCommand("SET time_zone = '+00:00'")->execute();
       foreach((new Query)->select('player_id,target_id,unix_timestamp(created_at) as created_at')->from('headshot')->each() as $hs) {
         $FLAG_MAX=(int)(new Query)->select('max(unix_timestamp(ts))')->from('player_treasure')->where('player_id=:player_id and treasure_id IN (SELECT id FROM treasure WHERE target_id=:target_id)',[':target_id'=>$hs['target_id'],':player_id'=>$hs['player_id']])->scalar();
         $FINDING_MAX=(int)(new Query)->select('max(unix_timestamp(ts))')->from('player_finding')->where('player_id=:player_id and finding_id IN (SELECT id FROM finding WHERE target_id=:target_id)',[':target_id'=>$hs['target_id'],':player_id'=>$hs['player_id']])->scalar();
         $MAXVAL=max([$FLAG_MAX,$FINDING_MAX]);
-        $MAXVAL++;
+        //$MAXVAL++;
         printf("Processing: uid=>%d, target=>%d, ts=>%s, maxfinding=>%d, maxflag=>%d\n",$hs['player_id'],$hs['target_id'],$hs['created_at'],$FINDING_MAX,$FLAG_MAX);
         $this->update('headshot',['created_at'=>new Expression("FROM_UNIXTIME($MAXVAL)")],['player_id'=>$hs['player_id'],'target_id'=>$hs['target_id']]);
         $this->update('stream',['ts'=>new Expression("FROM_UNIXTIME($MAXVAL)")],['player_id'=>$hs['player_id'],'model'=>'headshot','model_id'=>$hs['target_id']]);
