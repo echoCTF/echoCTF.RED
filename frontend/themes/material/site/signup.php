3383e559bee280f2b290b43b4e683031bd839a7d
Pantelis Roditis <proditis@echothrust.com>
2020-02-03 11:24:21 +0200

    make sure we tell users who is going to mail them

--- frontend/themes/material/site/signup.php
+++ frontend/themes/material/site/signup.php
@@ -12,7 +12,8 @@ $this->title = Yii::$app->sys->event_name. ' Signup';
 <div class="site-signup">
   <div class="body-content">
     <h2><?=Html::encode($this->title)?></h2>
-    <p class="text-primary">Please fill out the following fields to signup</p>
+    <p class="text-primary">Please fill out the following fields to register for an <code style="color: red">echoCTF.RED</code> account</p>
+    <p class="text-warning">All our email communications come from the following address: <small><code class="text-warning"><?=Html::encode(Yii::$app->sys->mail_from)?></code></small></p>
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
