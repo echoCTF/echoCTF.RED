c610a18ba05835085286a6bb68e47ddb42fa2569
Pantelis Roditis <proditis@echothrust.com>
2020-02-11 11:14:45 +0200

    remove YII_ENV_DEV checking we can re-enable whenever is needed

--- frontend/config/web.php
+++ frontend/config/web.php
@@ -234,20 +234,19 @@ $config = [
     'params' => $params,
 ];
 
+/*
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-//    $config['bootstrap'][] = 'debug';
-//    $config['modules']['debug'] = [
-//        'class' => 'yii\debug\Module',
-//    // uncomment the following to add your IP if you are not connecting from localhost.
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
 //        'allowedIPs' => ['127.0.0.1', '::1'],
-//    ];
+    ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-  //      'allowedIPs' => ['127.0.0.1', '::1'],
+//        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
-
+*/
 return $config;
