aa23509284ccb313ef5253746b4f2b9525f72bd6
Pantelis Roditis <proditis@echothrust.com>
2020-02-11 18:57:13 +0200

    rename migration

--- backend/migrations/m200210_162528_add_timer_and_rating_columns_to_headshot_table.php
+++ backend/migrations/m200210_162528_add_timer_and_rating_columns_to_headshot_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles adding columns to table `{{%headshot}}`.
  */
-class m200210_162528_add_timer_column_to_headshot_table extends Migration
+class m200210_162528_add_timer_and_rating_columns_to_headshot_table extends Migration
 {
     /**
      * {@inheritdoc}
@@ -13,6 +13,7 @@ class m200210_162528_add_timer_column_to_headshot_table extends Migration
     public function safeUp()
     {
         $this->addColumn('{{%headshot}}', 'timer', $this->bigInteger()->unsigned()->defaultValue(0));
+        $this->addColumn('{{%headshot}}', 'rating', $this->smallInteger()->defaultValue(-1));
     }
 
     /**
@@ -21,5 +22,6 @@ class m200210_162528_add_timer_column_to_headshot_table extends Migration
     public function safeDown()
     {
         $this->dropColumn('{{%headshot}}', 'timer');
+        $this->dropColumn('{{%headshot}}', 'rating');
     }
 }
