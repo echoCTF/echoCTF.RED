aa43215e697f7b86c6e9d2f3972f3bc7e86ce33c
Pantelis Roditis <proditis@echothrust.com>
2020-03-08 13:55:53 +0200

    start work on a dockerfile for people to be able to test the platform
    This is still a wip

--- frontend/config/db-local.php
+++ frontend/config/db-local.php
@@ -2,8 +2,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=echoCTF',
-    'username' => 'username',
-    'password' => 'password',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
