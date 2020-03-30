09d01706ead5061db74b8589f5300118e3387a64
Pantelis Roditis <proditis@echothrust.com>
2020-03-30 17:19:47 +0300

    update example

--- ansible/Dockerfiles/example/healthcheck.sh
+++ ansible/Dockerfiles/example/healthcheck.sh
@@ -7,11 +7,11 @@ function do_exit()
 
 nc -z localhost 375 || do_exit 1 "Port 375/tcp down"
 
-curl -s -f http://localhost:10888/ >/dev/null || exit 1
+#curl -s -f http://localhost:10888/ >/dev/null || exit 1
 
 # for memcached
-MEMFLAG=$(echo -e "get ETSCTF\r"|nc -N localhost 11211|sha256sum|awk '{print $1}')
-[ "$MEMFLAG" == "" ] || exit 2
+#MEMFLAG=$(echo -e "get ETSCTF\r"|nc -N localhost 11211|sha256sum|awk '{print $1}')
+#[ "$MEMFLAG" == "" ] || exit 2
 
 # for interesting files
 sha512sum -c --status < /usr/local/lib/.sha512sum || exit 1
