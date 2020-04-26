61f9c18fda5d10d47278e8d9d9ee5e47d256b1a6
Pantelis Roditis <proditis@echothrust.com>
2020-04-26 23:18:04 +0300

    ensure player is not null

--- frontend/models/forms/LoginForm.php
+++ frontend/models/forms/LoginForm.php
@@ -60,7 +60,7 @@ class LoginForm extends Model
      */
     public function login()
     {
-        if ($this->validate()) {
+        if ($this->validate() && $this->player!==null) {
             return Yii::$app->user->login($this->player, $this->rememberMe ? 3600*24*30 : 0);
         }
         return false;
