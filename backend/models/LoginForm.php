0e48a99cffac13ea1d5c60529d56ab531b337294
Pantelis Roditis <proditis@echothrust.com>
2020-04-27 12:32:31 +0300

    deadcode

--- backend/models/LoginForm.php
+++ backend/models/LoginForm.php
@@ -38,7 +38,7 @@ class LoginForm extends Model
      * @param string $attribute the attribute currently being validated
      * @param array $params the additional name-value pairs given in the rule
      */
-    public function validatePassword($attribute, $params)
+    public function validatePassword($attribute)
     {
         if (!$this->hasErrors()) {
             $user = $this->getUser();
