4564be7404d4bce6882ee8d848ceed5b9d909a71
Pantelis Roditis <proditis@echothrust.com>
2020-04-27 17:49:55 +0300

    minor update to remove a bit of complexity

--- frontend/models/Stream.php
+++ frontend/models/Stream.php
@@ -143,10 +143,7 @@ class Stream extends \yii\db\ActiveRecord
         break;
       case 'headshot':
         $headshot=\app\modules\game\models\Headshot::findOne(['target_id'=>$this->model_id,'player_id'=>$this->player_id]);
-        if($headshot->timer>0)
-          $message=sprintf("%s managed to headshot [<code>%s</code>] in <i class='fas fa-stopwatch'></i> %s minutes%s",$this->prefix,Html::a(Target::findOne(['id'=>$this->model_id])->fqdn,['/target/default/index','id'=>$this->model_id]),number_format($headshot->timer/60),$this->suffix);
-        else
-          $message=sprintf("%s managed to headshot [<code>%s</code>]%s",$this->prefix,Html::a(Target::findOne(['id'=>$this->model_id])->fqdn,['/target/default/index','id'=>$this->model_id]),$this->suffix);
+        $message=sprintf("%s managed to headshot [<code>%s</code>] in <i class='fas fa-stopwatch'></i> %s minutes%s",$this->prefix,Html::a(Target::findOne(['id'=>$this->model_id])->fqdn,['/target/default/index','id'=>$this->model_id]),number_format($headshot->timer/60),$this->suffix);
         break;
 //      case 'team_player':
 //        $message=sprintf("%s Team <b>%s</b> welcomes their newest member <b>%s</b> ", $this->icon,$this->player->teamMembership ? $this->player->teamMembership->name: "N/A", $this->player->profile->link);
