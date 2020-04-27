726c0d1bb76c037b71cc744660db214b0699ef33
Pantelis Roditis <proditis@echothrust.com>
2020-04-27 12:32:16 +0300

    classname=>class

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -18,7 +18,7 @@ class SiteController extends Controller
     {
         return [
             'access' => [
-                'class' => AccessControl::className(),
+                'class' => AccessControl::class,
                 'only' => ['logout'],
                 'rules' => [
                     [
@@ -29,7 +29,7 @@ class SiteController extends Controller
                 ],
             ],
             'verbs' => [
-                'class' => VerbFilter::className(),
+                'class' => VerbFilter::class,
                 'actions' => [
                     'logout' => ['post'],
                 ],
