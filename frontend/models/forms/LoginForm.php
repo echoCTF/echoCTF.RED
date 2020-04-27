f33e36b5c9480fa0b24de05209b479dcb1b53ac3
Pantelis Roditis <proditis@echothrust.com>
2020-04-27 11:28:00 +0300

    params not used can be ignored

--- frontend/models/forms/LoginForm.php
+++ frontend/models/forms/LoginForm.php
@@ -44,7 +44,7 @@ class LoginForm extends Model
      * @param string $attribute the attribute currently being validated
      * @param array $params the additional name-value pairs given in the rule
      */
-    public function validatePassword($attribute, $params)
+    public function validatePassword($attribute)
     {
         if (!$this->hasErrors()) {
             $player = $this->player;
