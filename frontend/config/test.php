b0b42ab224201903096f40fa83e330fee30e777c
Pantelis Roditis <proditis@echothrust.com>
2020-05-06 11:41:39 +0300

    mino updates for tests

--- frontend/config/test.php
+++ frontend/config/test.php
@@ -25,7 +25,7 @@ return [
             'showScriptName' => true,
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => 'app\models\Player',
         ],
         'request' => [
             'cookieValidationKey' => 'test',
