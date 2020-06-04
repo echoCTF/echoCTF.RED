a99c60719c3db66c27e12e41ea6ca4279795afaf
Pantelis Roditis <proditis@echothrust.com>
2020-06-04 10:44:15 +0300

    add saveSession method for issue #124 This way we can destroy player sessions for banned and blocked users

--- frontend/components/echoCTFView.php
+++ frontend/components/echoCTFView.php
@@ -25,44 +25,56 @@ class echoCTFView extends \yii\web\View
     {
         \Yii::$app->cache->Memcache->set("last_seen:".\Yii::$app->user->id, time());
         \Yii::$app->cache->Memcache->set("online:".\Yii::$app->user->id, time(), 0, \Yii::$app->sys->online_timeout);
+        $this->saveSession();
     }
     parent::init();
   }
 
+  public function saveSession()
+  {
+    if(\Yii::$app->cache->Memcache->get("player_session:".\Yii::$app->user->id)!==false)
+      return;
+
+    \Yii::$app->cache->Memcache->set("player_session:".\Yii::$app->user->id, \Yii::$app->session->id, 0, \Yii::$app->sys->online_timeout);
+    $session=\app\models\Sessions::findOne(\Yii::$app->session->id);
+    if($session===null)
+      $session=new \app\models\Sessions;
+    $session->player_id=\Yii::$app->user->id;
+    $session->ipoctet=\Yii::$app->getRequest()->userIP;
+    $session->id=\Yii::$app->session->id;
+    $session->data=json_encode($_SESSION);
+    $session->expire=new \yii\db\Expression('UNIX_TIMESTAMP(NOW()+INTERVAL 15 DAY)');
+    $session->save(false);
+    unset($session);
+  }
   public function getOg_title()
   {
-    //<meta property="og:title" content="Key Concepts: Dependency Injection Container" />
     if($this->_title === null)
       return ['property'=>'og:title', 'content'=>trim($this->title)];
   }
 
   public function getOg_description()
   {
-    //<meta property="og:description" content="" />
     return ['property'=>'og:description', 'content'=>trim($this->_description)];
   }
 
   public function getOg_site_name()
   {
-    // <meta property="og:site_name" content="Yii Framework" />
     return ['property'=>'og:site_name', 'content'=>trim(\Yii::$app->sys->event_name)];
   }
 
   public function getOg_image()
   {
-    //<meta property="og:image" content="https://www.yiiframework.com/image/facebook_cover.png" />
     return ['property'=>'og:image', 'content'=>sprintf("%s?%s", $this->_image, \Yii::$app->security->generateRandomString(5))];
   }
 
   public function getOg_url()
   {
-    //<meta property="og:url" content="/doc/guide/2.0/en/concept-di-container" />
     return ['property'=>'og:url', 'content'=>$this->_url];
   }
 
   public function getTwitter_card()
   {
-    //<meta name="twitter:card" content="summary" />
     return ['name'=>'twitter:card', 'content'=>$this->_card];
   }
 
@@ -73,37 +85,26 @@ class echoCTFView extends \yii\web\View
 
   public function getTwitter_title()
   {
-//    <meta name="twitter:title" content="Key Concepts: Dependency Injection Container" />
     return ['name'=>'twitter:title', 'content'=>$this->title];
   }
 
   public function getTwitter_description()
   {
-//    <meta name="twitter:description" content="" />
     return ['name'=>'twitter:description', 'content'=>trim($this->_description)];
   }
 
   public function getTwitter_image()
   {
-//    <meta name="twitter:image" content="https://www.yiiframework.com/image/twitter_cover.png" />
     return ['name'=>'twitter:image', 'content'=>sprintf("%s?%s", $this->_image, \Yii::$app->security->generateRandomString(5))];
   }
 
   public function getTwitter_image_width()
   {
-//    <meta name="twitter:image:width" content="120" />
     return ['name'=>'twitter:image:width', 'content'=>$this->_image_width];
   }
 
   public function getTwitter_image_height()
   {
-//    <meta name="twitter:image:height" content="120" />
     return ['name'=>'twitter:image:height', 'content'=>$this->_image_height];
   }
-
-/*
-  <meta name="msapplication-config" content="/favico/browserconfig.xml">
-  <meta name="theme-color" content="#ffffff">
-
-*/
 }
