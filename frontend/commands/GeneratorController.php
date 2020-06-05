560995c58fe0d9e24e0bdcc4c38a5a411dfbf386
Pantelis Roditis <proditis@echothrust.com>
2020-06-05 19:49:46 +0300

    add frontend robohash avatar generation console command

--- frontend/commands/GeneratorController.php
+++ frontend/commands/GeneratorController.php
@@ -4,6 +4,7 @@ namespace app\commands;
 use yii\console\Controller;
 use yii\console\ExitCode;
 use app\modules\target\models\Target;
+use app\models\Player;
 
 class GeneratorController extends Controller {
 
@@ -16,4 +17,26 @@ class GeneratorController extends Controller {
       $contents=$this->renderFile(\Yii::getAlias('@app/views/sitemap.php'), ['targets'=>$targets, 'BASEURL'=>$baseurl, 'profiles'=>[], 'TvsP'=>[]]);
       file_put_contents(\Yii::getAlias('@app/web/sitemap.xml'), $contents);
     }
+
+    /**
+     * Generate participant avatars based on robohash
+     */
+      public function actionAvatar()
+      {
+        $players=Player::find()->active()->all();
+        foreach($players as $player)
+        {
+          $robohash=new \app\models\Robohash($player->profile->id,'set1');
+          $image=$robohash->generate_image();
+          if(get_resource_type($image)=== 'gd')
+          {
+            $dst_img=\Yii::getAlias('@app/web/images/avatars/'.$player->profile->id.'.png');
+            imagepng($image,$dst_img);
+            imagedestroy($image);
+            $player->profile->avatar=$player->profile->id.'.png';
+            $player->profile->save(false);
+          }
+        }
+      }
+
 }
