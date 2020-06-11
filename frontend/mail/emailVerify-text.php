d285fc1c4dbb5632dbcad09ca876de8009e8fd8c
Pantelis Roditis <proditis@echothrust.com>
2020-06-11 12:00:42 +0300

    update verification and password reset email contents to make it humane

--- frontend/mail/emailVerify-text.php
+++ frontend/mail/emailVerify-text.php
@@ -5,8 +5,14 @@
 
 $verifyLink=Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
 ?>
-Hello <?= $user->username ?>,
+Hello and welcome to echoCTF.RED
 
-Follow the link below to verify your email:
+You (or possibly someone else), just requested that this email address be used
+to create an account on our platform.
+
+To complete this verification process and activate the account on our platform
+follow the link below:
 
 <?= $verifyLink ?>
+
+If you didn't request the account registration, just ignore this email.
--- frontend/mail/passwordResetToken-html.php
+++ frontend/mail/passwordResetToken-html.php
@@ -9,7 +9,13 @@ $resetLink=Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'tok
 <div class="password-reset">
     <p>Hello <?= Html::encode($user->username) ?>,</p>
 
+    <p>You (or possibly someone else), just requested a password reset operation
+    to be performed on an account associated with this email address.</p>
+
     <p>Follow the link below to reset your password:</p>
 
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
+
+    <p>If you didn't request this password reset, just ignore this email.</p>
+
 </div>
