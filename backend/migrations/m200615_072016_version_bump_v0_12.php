0bac522115c9a160269f76cfdd857e9e673f35fd
Pantelis Roditis <proditis@echothrust.com>
2020-06-15 10:22:33 +0300

    bump version and be ready for todays dt

---
+++ backend/migrations/m200615_072016_version_bump_v0_12.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200615_072016_version_bump_v0_12
+ */
+class m200615_072016_version_bump_v0_12 extends Migration
+{
+  public function safeUp()
+  {
+    $this->update('sysconfig', ['val'=>'v0.12'], ['id'=>'platform_version']);
+    $this->update('sysconfig', ['val'=>new Expression('REPLACE(val,"v0.11","v0.12")')], ['id' => 'frontpage_scenario']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function safeDown()
+  {
+    $this->update('sysconfig', ['val'=>'v0.11'], ['id'=>'platform_version']);
+    $this->update('sysconfig', ['val'=>new Expression('REPLACE(val,"v0.12","v0.11")')], ['id' => 'frontpage_scenario']);
+  }
+}
