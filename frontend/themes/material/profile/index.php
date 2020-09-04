076d2cb1e09957dda2c0f63f50e3e048a5abb846
Pantelis Roditis <proditis@echothrust.com>
2020-09-04 23:03:49 +0300

    minor cleanups and link to target

--- frontend/themes/material/profile/index.php
+++ frontend/themes/material/profile/index.php
@@ -27,24 +27,11 @@ $this->_url=\yii\helpers\Url::to(['index', 'id'=>$profile->id], 'https');
         <?=$this->render('_card', ['profile'=>$profile]);?>
       </div><!-- // end profile card col-md-4 -->
     </div><!--/row-->
-    <div class="row">
-    <div class="col-sm-8"><?php
-    \yii\widgets\Pjax::begin(['id'=>'stream-listing', 'enablePushState'=>false, 'linkSelector'=>'#stream-pager a', 'formSelector'=>false]);
-    echo Stream::widget(['divID'=>'stream', 'dataProvider' => null, 'player_id'=>$profile->player_id, 'pagerID'=>'stream-pager']);
-    \yii\widgets\Pjax::end();
-    ?></div>
-    <div class="col-sm-4">
-      <?php
-      Pjax::begin(['id'=>'leaderboard-listing', 'enablePushState'=>false, 'linkSelector'=>'#leaderboard-pager a', 'formSelector'=>false]);
-      echo Leaderboard::widget(['divID'=>"Leaderboard", 'player_id'=>$profile->player_id, 'pageSize'=>8, 'title'=>'Leaderboard', 'category'=>'Listing current player page. <small>Updated every 10 minutes</small>']);
-      Pjax::end();
-      ?>
-    </div>
+
     <?php if($game->badges !== null && $game->badges->received_by($profile->player_id)->count() > 0):?><h3>Badges</h3>
-    </div>
     <div class="row game-badges">
       <?php foreach($game->badges->received_by($profile->player_id)->all() as $badge):?>
-      <div class="col col-sm-1 col-md-4 col-lg-3">
+      <div class="col col-sm-12 col-md-4 col-lg-3">
         <div class="iconic-card">
           <center><?=$badge->pubname?></center>
           <h3><?=$badge->name?></h3>
@@ -65,14 +52,40 @@ $this->_url=\yii\helpers\Url::to(['index', 'id'=>$profile->id], 'https');
       <div class="col col-sm-1 col-md-5 col-lg-3">
         <div class="iconic-card">
           <img align="right" src="/images/targets/_<?=$headshot->target->name?>-thumbnail.png"/>
-          <p><b style="color:#222222"><?=$headshot->target->name?> / <?=long2ip($headshot->target->ip)?></b></p>
-          <p><b><i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($headshot->timer)?></b></p>
-          <p><b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($headshot->created_at,'long')?></b></p>
+          <p><b><?=Html::a(
+                      $headshot->target->name.' / '.long2ip($headshot->target->ip) ,
+                        Url::to(['/target/default/versus', 'id'=>$headshot->target_id, 'profile_id'=>$profile->id]),
+                        [
+                          //'class'=>'btn-primary',
+                          'style'=>'float: bottom;',
+                          'title' => 'View target vs player card',
+                          'aria-label'=>'View target vs player card',
+                          'data-pjax' => '0',
+                        ]
+                    );?></b></p>
+          <p><b><i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($headshot->timer)?></b></br>
+          <b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($headshot->created_at,'long')?></b></p>
+
         </div>
       </div>
       <?php endforeach;?>
     </div>
     <?php endif;?>
 
+
+    <div class="row">
+      <div class="col-sm-8"><?php
+      \yii\widgets\Pjax::begin(['id'=>'stream-listing', 'enablePushState'=>false, 'linkSelector'=>'#stream-pager a', 'formSelector'=>false]);
+      echo Stream::widget(['divID'=>'stream', 'dataProvider' => null, 'player_id'=>$profile->player_id, 'pagerID'=>'stream-pager','title'=>'User History','category'=>'User history on the platform']);
+      \yii\widgets\Pjax::end();
+      ?></div>
+      <div class="col-sm-4">
+        <?php
+        Pjax::begin(['id'=>'leaderboard-listing', 'enablePushState'=>false, 'linkSelector'=>'#leaderboard-pager a', 'formSelector'=>false]);
+        echo Leaderboard::widget(['divID'=>"Leaderboard", 'player_id'=>$profile->player_id, 'pageSize'=>8, 'title'=>'Leaderboard', 'category'=>'Listing current player page. <small>Updated every 10 minutes</small>']);
+        Pjax::end();
+        ?>
+      </div>
+    </div>
   </div><!--//body-content-->
 </div><!--//profile-index-->
