84eb6e46ad9b3bad83d7bba30e851e8f7a75d377
Pantelis Roditis <proditis@echothrust.com>
2020-09-04 12:58:16 +0300

    convert into widget

--- frontend/themes/material/modules/challenge/views/default/view.php
+++ frontend/themes/material/modules/challenge/views/default/view.php
@@ -6,6 +6,7 @@ use yii\widgets\ListView;
 use yii\data\ActiveDataProvider;
 use yii\bootstrap\ActiveForm;
 use app\widgets\Twitter;
+use app\widgets\solver\SolverWidget;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Challenge */
@@ -54,29 +55,7 @@ $this->_url=\yii\helpers\Url::to(['view', 'id'=>$model->id], 'https');
     <?php ActiveForm::end();?>
   </div>
   <div class="col-lg-4 col-md-6 col-sm-6">
-    <div class="card bg-dark solves">
-      <h4><i class="fas fa-tasks"></i> <?=$solvers->count()?> Solvers (older first)</h4>
-      <div class="card-body table-responsive"><?php
-        $solves=[];
-        foreach($solvers->orderBy(['created_at'=>SORT_ASC, 'player_id'=>SORT_ASC])->all() as $solver)
-        {
-          if((int) $solver->player->active === 1)
-            $solves[]=$solver->player->profile->link;
-        }
-        if(!empty($solves))
-        {
-          echo "<code>",implode(", ", array_slice($solves, 0,19)),"</code>";
-          if(count($solves)>19){
-            echo "<details class=\"headshotters\">";
-            echo "<summary data-open=\"Hide more\" data-close=\"Show more\"></summary>";
-            echo "<code>",implode(", ", array_slice($solves, 19)),"</code>";
-            echo "</details>";
-          }
-        }
-        else
-          echo '<code>none yet...</code>';?>
-      </div>
-    </div>
+    <?= SolverWidget::widget(['model' => $solvers,'slice'=>30]) ?>
   </div>
 
   </div>
