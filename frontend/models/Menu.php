0731cb25e83de91e197e5c1dcc9b6b9c4d497469
Pantelis Roditis <proditis@echothrust.com>
2020-09-08 01:57:31 +0300

    add menu item for tutorial

--- frontend/models/Menu.php
+++ frontend/models/Menu.php
@@ -18,7 +18,7 @@ class Menu
                     ['label' => 'Dashboard', 'icon'=>'dashboard', 'url' => ['/dashboard/index'], 'visible'=>!Yii::$app->user->isGuest],
                     //['label' => 'Networks', 'icon'=>'bug_report','url' => ['/network/default/index'],'visible'=>!Yii::$app->user->isGuest,'active'=>\Yii::$app->controller->module->id=="network"],
                     ['label' => 'Challenges', 'icon'=>'extension', 'url' => ['/challenge/default/index'], 'visible'=>!Yii::$app->user->isGuest, 'active'=>\Yii::$app->controller->module->id == "challenge"],
-                    //['label' => 'Tutorials', 'icon'=>'developer_board','url' => ['/tutorial/default/index'],'visible'=>!Yii::$app->user->isGuest,'active'=>\Yii::$app->controller->module->id=="tutorial"],
+                    ['label' => 'Tutorials', 'icon'=>'developer_board','url' => ['/tutorial/default/index'],'visible'=>!Yii::$app->user->isGuest,'active'=>\Yii::$app->controller->module->id=="tutorial"],
                     //['label' => 'Rules','icon'=>'list_alt', 'url' => ['/help/rule/index']],
                     ['label' => 'Instructions', 'icon'=>'info', 'url' => ['/help/instruction/index']],
                     ['label' => 'FAQ', 'icon'=>'help', 'url' => ['/help/faq/index']],
