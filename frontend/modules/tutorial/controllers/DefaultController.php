82f201d2a354e8371571abb18263cd8018c19785
Pantelis Roditis <proditis@echothrust.com>
2020-09-08 02:12:23 +0300

    add initial view operation

--- frontend/modules/tutorial/controllers/DefaultController.php
+++ frontend/modules/tutorial/controllers/DefaultController.php
@@ -8,7 +8,8 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\data\ActiveDataProvider;
 use yii\filters\AccessControl;
-
+use app\modules\tutorial\models\Tutorial;
+use app\modules\tutorial\models\TutorialTask;
 /**
  * TutorialController implements the CRUD actions for Tutorial model.
  */
@@ -46,6 +47,42 @@ class DefaultController extends Controller
         return $this->render('index', [
             'dataProvider' => $dataProvider,
         ]);
+    }
+
+    /**
+     * Displays a single Tutorial model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView(int $id)
+    {
+      $model=Tutorial::find()->where(['id'=>$id])->one();
+//      $model=Tutorial::find()->where(['t.id'=>$id])->player_progress(Yii::$app->user->id)->one();
+      if($model===null)
+      {
+          throw new NotFoundHttpException('The requested challenge could not be found.');
+      }
+      $query=TutorialTask::find()->orderBy(['weight'=>SORT_ASC, 'id'=>SORT_ASC]);
+      $dataProvider=new ActiveDataProvider([
+          'query' => $query,
+      ]);
+      $query->andFilterWhere(['tutorial_id'=>$model->id]);
+
+//      $answer=new AnswerForm();
+//      if($answer->load(Yii::$app->request->post()) && $answer->validate() && $answer->give($id))
+//      {
+//            Yii::$app->session->setFlash('success', sprintf('Accepted answer for question [%s] for %d pts.', $answer->question->name, intval($answer->question->points)));
+//            return $this->redirect(Yii::$app->request->referrer);
+//      }
+
+
+      return $this->render('view', [
+//          'answer'=>$answer,
+          'model' => $model,
+  //        'solvers' => $solvers,
+          'dataProvider' => $dataProvider,
+      ]);
     }
 
     /**
