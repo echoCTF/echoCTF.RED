47b399d5864f6582c648452911fd1c3b63408106
Pantelis Roditis <proditis@echothrust.com>
2020-09-07 23:15:12 +0300

    leg work to see writeups on headshot targets

--- frontend/themes/material/profile/index.php
+++ frontend/themes/material/profile/index.php
@@ -2,13 +2,21 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\ListView;
-use yii\grid\GridView;
 use app\components\JustGage;
 use app\widgets\stream\StreamWidget as Stream;
 use app\widgets\target\TargetWidget;
 use yii\widgets\Pjax;
 use app\widgets\leaderboard\Leaderboard;
-
+use app\modules\target\models\PlayerTargetHelp as PTH;
+use app\modules\target\models\Writeup;
+use yii\data\ActiveDataProvider;
+$query = Writeup::find()->where(['player_id'=>$profile->player_id]);
+$provider = new ActiveDataProvider([
+    'query' => $query,
+    'pagination' => [
+        'pageSize' => 10,
+    ],
+]);
 $game=Yii::$app->getModule('game');
 $this->_fluid="-fluid";
 $this->title=Yii::$app->sys->event_name.' Profile of: '.Html::encode($profile->owner->username);
@@ -27,7 +35,9 @@ $this->_url=\yii\helpers\Url::to(['index', 'id'=>$profile->id], 'https');
         <?=$this->render('_card', ['profile'=>$profile]);?>
       </div><!-- // end profile card col-md-4 -->
     </div>
-
+    <?php if((int)Writeup::find()->where(['player_id'=>$profile->player_id])->count()>0):?>
+      <?php /* echo $this->render('_writeups', ['profile'=>$profile,'provider'=>$provider]);*/?>
+    <?php endif;?>
     <?php if($profile->headshotsCount>0):?><h3><code><?=$profile->headshotsCount?></code> Headshots / <small>Average time: <?php
       $hs=\app\modules\game\models\Headshot::find()->player_avg_time($profile->player_id)->one();
       if($hs && $hs->average > 0)
@@ -48,8 +58,10 @@ $this->_url=\yii\helpers\Url::to(['index', 'id'=>$profile->id], 'https');
                           'data-pjax' => '0',
                         ]
                     );?></b></p>
-          <p><b><i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($headshot->timer)?></b></br>
-          <b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($headshot->created_at,'long')?></b></p>
+          <p><b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($headshot->created_at,'long')?></b><br/>
+            <?php if($headshot->writeup):?><b><i class="fas fa-book text-secondary"></i> Writeup submitted</b><br/><?php endif;?>
+          <i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($headshot->timer)?>
+          </p>
         </div>
       </div>
       <?php endforeach;?>
