16ba34f81b50220a51ff6b559d892af8d73368d9
Pantelis Roditis <proditis@echothrust.com>
2020-09-08 21:31:09 +0300

    update view accordingly

--- frontend/themes/material/modules/target/views/writeup/create.php
+++ frontend/themes/material/modules/target/views/writeup/create.php
@@ -5,15 +5,15 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\activity\models\Writeup */
 
-$this->title=Yii::$app->sys->event_name.' Writeup for '.$target->name. ' / '.long2ip($target->ip). ' #'.$target->id;
-$this->_description=$target->purpose;
-$this->_image=\yii\helpers\Url::to($target->fullLogo, 'https');
-$this->_url=\yii\helpers\Url::to(['index', 'id'=>$target->id], 'https');
+$this->title=Yii::$app->sys->event_name.' Writeup for '.$headshot->target->name. ' / '.long2ip($headshot->target->ip). ' #'.$headshot->target->id;
+$this->_description=$headshot->target->purpose;
+$this->_image=\yii\helpers\Url::to($headshot->target->fullLogo, 'https');
+$this->_url=\yii\helpers\Url::to(['index', 'id'=>$headshot->target->id], 'https');
 $this->_fluid='-fluid';
 ?>
 <div class="writeup-create">
   <div class="body-content">
-    <h2><?= Html::encode('Writeup for '.$target->name. ' / '.long2ip($target->ip). ' #'.$target->id) ?></h2>
+    <h2><?= Html::encode('Writeup for '.$headshot->target->name. ' / '.long2ip($headshot->target->ip). ' #'.$headshot->target->id) ?></h2>
     <div class="col">
       <div class="card bg-dark">
         <div class="card-body">
