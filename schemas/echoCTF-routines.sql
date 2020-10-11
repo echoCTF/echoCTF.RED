dbdd36e360aaa6fc27d02f3fc05858f2ba3433d3
Pantelis Roditis <proditis@echothrust.com>
2020-10-11 15:27:24 +0300

    procedure to generate and update country based ranks

--- schemas/echoCTF-routines.sql
+++ schemas/echoCTF-routines.sql
@@ -243,3 +243,31 @@ BEGIN
   insert into team_score (team_id,points) select id,0 from team;
   insert into player_score (player_id,points) select id,0 from player;
 END ;;
+
+DROP PROCEDURE IF EXISTS calculate_country_rank;;
+CREATE PROCEDURE calculate_country_rank ()
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE ccode VARCHAR(3);
+  DECLARE cur1 CURSOR FOR SELECT DISTINCT country FROM profile;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  CREATE TEMPORARY TABLE country_ranking (id int primary key AUTO_INCREMENT,player_id int) ENGINE=MEMORY;
+  OPEN cur1;
+  read_loop: LOOP
+    FETCH cur1 INTO ccode;
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+    START TRANSACTION;
+      delete from player_country_rank WHERE country=ccode;
+      insert into country_ranking SELECT NULL,t.player_id FROM player_score AS t
+        LEFT JOIN player AS t2 ON t.player_id=t2.id
+        LEFT JOIN profile AS t3 ON t.player_id=t3.player_id
+        WHERE t2.active=1 AND t3.country=ccode ORDER BY points DESC,t.ts ASC, t.player_id ASC;
+      insert into player_country_rank select *,ccode from country_ranking ON DUPLICATE KEY UPDATE id=values(id),country=values(country);
+    COMMIT;
+    TRUNCATE country_ranking;
+  END LOOP;
+  CLOSE cur1;
+  DROP TABLE country_ranking;
+END ;;
