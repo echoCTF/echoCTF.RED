b4ba321ebc89791a09fd96c918253cef071ec96a
Pantelis Roditis <proditis@echothrust.com>
2020-10-26 12:31:00 +0200

    add enabled method and seperate route extraction When action is instanceof inlineaction extract route otherwise use it as string

--- frontend/components/DisabledRoute.php
+++ frontend/components/DisabledRoute.php
@@ -16,17 +16,33 @@ class DisabledRoute extends Component
 
   static function disabled($action)
   {
-    if(property_exists($action->controller->module,'requestedRoute'))
-      $route=$action->controller->module->requestedRoute;
-    elseif(property_exists($action->controller->module,'module') && property_exists($action->controller->module->module,'requestedRoute'))
-      $route=$action->controller->module->module->requestedRoute;
+    if($action instanceof yii\base\InlineAction)
+    {
+      $route=self::RequestedRoute($action);
+    }
     else
-      $route=$action->controller->id.'/'.$action->id;
-
+    {
+      $route=$action;
+    }
     if((int)\Yii::$app->db->createCommand("SELECT count(*) FROM disabled_route WHERE :route LIKE route")->bindValue(':route', $route)->queryScalar()>0)
       return true;
 
     return false;
+  }
+
+  static function enabled($action)
+  {
+    return !self::disabled($action);
+  }
+
+  public static function RequestedRoute($action)
+  {
+    if(property_exists($action->controller->module,'requestedRoute'))
+      return $action->controller->module->requestedRoute;
+    elseif(property_exists($action->controller->module,'module') && property_exists($action->controller->module->module,'requestedRoute'))
+      return $action->controller->module->module->requestedRoute;
+
+    return $action->controller->id.'/'.$action->id;
   }
 
 }
