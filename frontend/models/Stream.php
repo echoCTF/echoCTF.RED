b5674d6d8d111dc190f1821e97c6f89d47e4be57
Pantelis Roditis <proditis@echothrust.com>
2020-10-27 00:19:03 +0200

    make stream feel a bit more alive fixes #220

--- frontend/models/Stream.php
+++ frontend/models/Stream.php
@@ -31,15 +31,15 @@ use app\modules\game\models\Badge;
 class Stream extends \yii\db\ActiveRecord
 {
   const MODEL_ICONS=[
-    'headshot'=>'<i class="fas fa-skull" style="font-size: 1.5em;"></i>',
-    'challenge'=>'<i class="fas fa-tasks" style="font-size: 1.5em;"></i>',
-    'treasure'=>'<i class="fas fa-flag" style="font-size: 1.5em;"></i>',
-    'finding'=>'<i class="fas fa-fingerprint" style="font-size: 1.5em;"></i>',
-    'question'=>'<i class="fas fa-list-ul" style="font-size: 1.5em;"></i>',
+    'headshot'=>'<i class="fas fa-skull" style="color: #FF1A00;font-size: 1.5em;" data-toggle="tooltip" title="Target Headshot"></i>',
+    'challenge'=>'<i class="fas fa-tasks" style="color: #FF1A00; font-size: 1.5em;" data-toggle="tooltip" title="Challenge Solve"></i>',
+    'treasure'=>'<i class="fas fa-flag text-danger" style="font-size: 1.5em;" data-toggle="tooltip" title="Target Flag"></i>',
+    'finding'=>'<i class="fas fa-fingerprint" style="color:#FF7400; font-size: 1.5em;" data-toggle="tooltip" title="Target Service"></i>',
+    'question'=>'<i class="fas fa-list-ul text-info" style="font-size: 1.5em;" data-toggle="tooltip" title="Challenge Question"></i>',
     'team_player'=>'<i class="fas fa-users" style="font-size: 1.5em;"></i>',
-    'user'=>'<i class="fas fa-user-ninja" style="font-size: 1.5em;"></i>',
+    'user'=>'<i class="fas fa-user-ninja " style="color: #4096EE;font-size: 1.5em;" data-toggle="tooltip" title="Player"></i>',
     'report'=>'<i class="fas fa-clipboard-list" style="font-size: 1.5em;"></i>',
-    'badge'=>'<i class="fas fa-trophy" style="font-size: 1.5em;"></i>',
+    'badge'=>'<i class="fas fa-trophy" style="color: #C79810;font-size: 1.5em;" data-toggle="tooltip" title="Badge"></i>',
   ];
 
   public $ts_ago;
@@ -124,7 +124,7 @@ class Stream extends \yii\db\ActiveRecord
 
   public function getPrefix()
   {
-    return sprintf("%s <b>%s</b>", $this->icon, $this->player->profile->link);
+    return sprintf("%s <img src='/images/avatars/%s' class='rounded' width='30px'><b>%s</b>", $this->icon, $this->player->profile->avtr,$this->player->profile->link);
   }
 
   public function Title(bool $pub=true)
