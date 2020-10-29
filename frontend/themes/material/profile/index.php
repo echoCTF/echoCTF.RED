fec633be05df41aff08ac3a111952a700c3b5ea4
Pantelis Roditis <proditis@echothrust.com>
2020-10-29 13:05:57 +0200

    add completed challenges on player profile

--- frontend/themes/material/profile/index.php
+++ frontend/themes/material/profile/index.php
@@ -38,6 +38,32 @@ $this->_url=\yii\helpers\Url::to(['index', 'id'=>$profile->id], 'https');
     <?php if((int)Writeup::find()->where(['player_id'=>$profile->player_id])->count()>0):?>
       <?php /* echo $this->render('_writeups', ['profile'=>$profile,'provider'=>$provider]);*/?>
     <?php endif;?>
+
+    <?php if(count($profile->owner->challengeSolvers)>0):?>
+      <h3><code><?=count($profile->owner->challengeSolvers)?></code> Challenges solved</h2>
+      <div class="row">
+        <?php foreach($profile->owner->challengeSolvers as $cs):?>
+          <div class="col col-sm-1 col-md-5 col-lg-3">
+            <div class="iconic-card">
+                  <?=$cs->challenge->icon?>
+                  <p><b><?=Html::a(
+                          $cs->challenge->name,
+                            Url::to(['/challenge/default/view', 'id'=>$cs->challenge_id]),
+                            [
+                              'style'=>'float: bottom;',
+                              'title' => 'View target vs player card',
+                              'aria-label'=>'View target vs player card',
+                              'data-pjax' => '0',
+                            ]
+                        );?></b></p>
+                          <p><b><i class="fas fa-list-ul text-info"></i> <?=count($cs->challenge->questions)?> Questions / <?=$cs->challenge->difficulty?></b><br /><b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($cs->created_at,'long')?></b><br/>
+                          <i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($cs->timer)?></p>
+            </div>
+          </div>
+        <?php endforeach;?>
+      </div>
+    <?php endif;?>
+
     <?php if($profile->headshotsCount>0):?><h3><code><?=$profile->headshotsCount?></code> Headshots / <small>Average time: <?php
       $hs=\app\modules\game\models\Headshot::find()->player_avg_time($profile->player_id)->one();
       if($hs && $hs->average > 0)
