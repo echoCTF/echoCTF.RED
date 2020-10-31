843368aee595d77fb44434456cea75c5eb88333d
Pantelis Roditis <proditis@echothrust.com>
2020-11-01 00:53:36 +0200

    rename challenge name

--- backend/migrations-red/m201029_105417_update_challenge_icons.php
+++ backend/migrations-red/m201029_105417_update_challenge_icons.php
@@ -9,7 +9,7 @@ class m201029_105417_update_challenge_icons extends Migration
 {
   public $challenges=[
     [ 'id'=> 1,'icon'=> '<img align="right" src="/images/challenge/category/tutorial.svg" style="height: 58px;"/>'],
-    [ 'id'=> 2,'icon'=> '<img align="right" src="/images/challenge/category/tutorial.svg" style="max-height: 58px;"/>'],
+    [ 'id'=> 2,'name'=>'LFI Tutorial 101','icon'=> '<img align="right" src="/images/challenge/category/tutorial.svg" style="max-height: 58px;"/>'],
     [ 'id'=> 3,'icon'=> '<img align="right" src="/images/challenge/category/code-analysis.svg"/>'],
     [ 'id'=> 4,'icon'=> '<img align="right" src="/images/challenge/category/code-analysis.svg"/>'],
     [ 'id'=> 5,'icon'=> '<img align="right" src="/images/challenge/category/docker.svg"/>'],
