6f59c486b5ff5cd6dcef9840797c4fc3f7da449f
Pantelis Roditis <proditis@echothrust.com>
2020-11-04 22:43:57 +0200

    add anchors and section to each record

--- frontend/themes/material/modules/help/views/instruction/_instruction.php
+++ frontend/themes/material/modules/help/views/instruction/_instruction.php
@@ -2,8 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\components\formatters\Anchor;
 ?>
 <div class="instruction-view">
+    <section id="<?=Html::encode(Anchor::to($model->title))?>">
     <h3><b><?=Html::encode($model->title)?></b></h3>
     <?=$model->message;?>
+    </section>
 </div>
--- frontend/themes/material/modules/help/views/rule/_rule.php
+++ frontend/themes/material/modules/help/views/rule/_rule.php
@@ -2,8 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\components\formatters\Anchor;
 ?>
 <div class="rule-view">
+    <section id="<?=Html::encode(Anchor::to($model->title))?>">
     <h3><b><?=Html::encode($model->title)?></b></h3>
     <p><?=$model->message;?></p>
+    </section>
 </div>
