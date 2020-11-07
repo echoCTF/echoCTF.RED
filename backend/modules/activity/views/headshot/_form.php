8787bf64a20702bbd2aab1ea10159306ce677a0d
Pantelis Roditis <proditis@echothrust.com>
2020-11-07 22:54:56 +0200

    fix having to press a submit button twice

--- backend/modules/activity/views/headshot/_form.php
+++ backend/modules/activity/views/headshot/_form.php
@@ -13,16 +13,16 @@ use app\modules\gameplay\models\Target;
 
 <div class="headshot-form">
 
-    <?php $form=ActiveForm::begin();?>
+    <?php $form=ActiveForm::begin(['validateOnSubmit' => false]);?>
     <?= $form->field($model, 'target_id')->dropDownList(ArrayHelper::map(Target::find()->orderBy(['fqdn'=>SORT_ASC])->all(), 'id', 'fqdn'), ['prompt'=>'Select Target'])->hint('The target for the headshot.') ?>
 
     <?= $form->field($model, 'player_id')->dropDownList(ArrayHelper::map(Player::find()->orderBy(['username'=>SORT_ASC])->all(), 'id', 'username'), ['prompt'=>'Select player'])->Label('Player')->hint('The player id that the headshot will be given.') ?>
 
-    <?= $form->field($model, 'timer')->textInput()->hint('Headshot timer in seconds.') ?>
+    <?= $form->field($model, 'timer')->textInput()->hint('Headshot timer in seconds. Leave empty for random') ?>
 
     <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success', 'value'=>'save', 'name'=>'submit']) ?>
-        <?= Html::submitButton(Yii::t('app', 'Give'), ['class' => 'btn btn-primary', 'value'=>'give', 'name'=>'submit']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success', 'value'=>'save', 'name'=>'submit[]','id'=>'saveBtn']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Give'), ['class' => 'btn btn-primary', 'value'=>'give', 'name'=>'submit[]', 'id'=>'giveBtn']) ?>
     </div>
 
     <?php ActiveForm::end();?>
