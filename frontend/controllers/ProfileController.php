2afa7e00bf92382d1a463297e9b1d5620172dbf6
Pantelis Roditis <proditis@echothrust.com>
2020-11-10 02:05:33 +0200

    update profile controller with operations for teams and event status

--- frontend/controllers/ProfileController.php
+++ frontend/controllers/ProfileController.php
@@ -27,8 +27,45 @@ class ProfileController extends \yii\web\Controller
                 'class' => AccessControl::class,
                 'only' => ['me', 'index', 'notifications', 'hints', 'update', 'ovpn', 'settings'],
                 'rules' => [
+                     [
+                        'actions' => ['ovpn'],
+                        'allow' => false,
+                        'matchCallback' => function ($rule, $action) {
+                          return Yii::$app->sys->event_start!==false && (time()<Yii::$app->sys->event_start || time()>Yii::$app->sys->event_end);
+                        },
+                        'denyCallback' => function() {
+                          Yii::$app->session->setFlash('info', 'This area is disabled until the competition starts');
+                          return  \Yii::$app->getResponse()->redirect(['/profile/me']);
+                        }
+
+                    ],
                     [
-                        'actions' => ['me', 'notifications', 'hints', 'update', 'ovpn', 'settings', 'robohash'],
+                       'actions' => ['ovpn'],
+                       'allow' => false,
+                       'matchCallback' => function ($rule, $action) {
+                         if(Yii::$app->sys->teams===false)
+                         {
+                            return false;
+                         }
+
+                         if(Yii::$app->user->identity->teamPlayer===NULL)
+                         {
+                           Yii::$app->session->setFlash('warning', 'You need to join a team before being able claim flags.');
+                           return true;
+                         }
+                         if(Yii::$app->user->identity->teamPlayer->approved!==1)
+                         {
+                           Yii::$app->session->setFlash('warning', 'You need to have your team membership approved before being able claim flags.');
+                           return true;
+                         }
+                         return false;
+                       },
+                       'denyCallback' => function() {
+                         return  \Yii::$app->getResponse()->redirect(['/dashboard/index']);
+                       }
+                    ],
+                    [
+                        'actions' => ['me', 'notifications', 'hints', 'update', 'ovpn', 'settings'],
                         'allow' => true,
                         'roles' => ['@'],
                         'matchCallback' => function ($rule, $action) {
@@ -39,6 +76,7 @@ class ProfileController extends \yii\web\Controller
                         'actions' => ['index'],
                         'allow' => true,
                     ],
+
                 ],
             ],
             [
