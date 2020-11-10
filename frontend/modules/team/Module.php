c6803a53199e65b8bf3d36109387ff34519268cd
Pantelis Roditis <proditis@echothrust.com>
2020-11-10 02:36:00 +0200

    add module check permissions if teams are enabled

--- frontend/modules/team/Module.php
+++ frontend/modules/team/Module.php
@@ -2,6 +2,8 @@
 
 namespace app\modules\team;
 
+use yii\filters\AccessControl;
+
 /**
  * target module definition class
  */
@@ -11,6 +13,30 @@ class Module extends \yii\base\Module
      * {@inheritdoc}
      */
     public $controllerNamespace='app\modules\team\controllers';
+
+  public function behaviors()
+  {
+    return [
+      'access' => [
+      'class' => AccessControl::class,
+        'rules' => [
+          [
+            'allow' => false,
+            'matchCallback' => function ($rule, $action) {
+              if(\Yii::$app->sys->teams===false)
+              {
+                 return true;
+              }
+              return false;
+            },
+            'denyCallback' => function() {
+              return  \Yii::$app->getResponse()->redirect(['/site/index']);
+            }
+          ]
+        ],
+      ],
+    ];
+  }
 
     /**
      * {@inheritdoc}
