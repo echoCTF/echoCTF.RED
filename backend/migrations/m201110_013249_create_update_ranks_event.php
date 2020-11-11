19042ce0d66619d2399b2c734f372d3447f3c2c3
Pantelis Roditis <proditis@echothrust.com>
2020-11-11 03:39:43 +0200

    make event execution every 30 seconds and add missing begin statement

--- backend/migrations/m201110_013249_create_update_ranks_event.php
+++ backend/migrations/m201110_013249_create_update_ranks_event.php
@@ -8,10 +8,13 @@ use yii\db\Migration;
 class m201110_013249_create_update_ranks_event extends Migration
 {
   public $DROP_SQL="DROP EVENT IF EXISTS {{%update_ranks}}";
-  public $CREATE_SQL="CREATE EVENT {{%update_ranks}} ON SCHEDULE EVERY 1 MINUTE ON COMPLETION PRESERVE ENABLE DO
+  public $CREATE_SQL="CREATE EVENT {{%update_ranks}} ON SCHEDULE EVERY 30 SECOND ON COMPLETION PRESERVE ENABLE DO
+  BEGIN
+    ALTER EVENT {{%update_ranks}} DISABLE;
     call calculate_ranks();
     call calculate_country_rank();
     call calculate_team_ranks();
+    ALTER EVENT {{%update_ranks}} ENABLE;
   END";
     // Use up()/down() to run migration code without a transaction.
     public function up()
