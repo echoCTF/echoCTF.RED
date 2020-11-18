fa31a8c95c9b00d0bfac51221c4127bf2926d715
Pantelis Roditis <proditis@echothrust.com>
2020-11-18 23:08:19 +0200

    add named queries for targets in and not in networks

--- frontend/modules/target/models/TargetQuery.php
+++ frontend/modules/target/models/TargetQuery.php
@@ -10,6 +10,10 @@ class TargetQuery extends \yii\db\ActiveQuery
           $this->andOnCondition([$this->modelClass::tableName() . '.branch_id' => Yii::$app->user->identity->branch_id ]);
           parent::init();
       }*/
+      public function forNet(int $network_id)
+      {
+        return $this->join('LEFT JOIN','network_target','network_target.target_id=t.id')->andWhere(['network_target.network_id'=>$network_id]);
+      }
       public function timed()
       {
         return $this->andWhere(['timer' => 1]);
@@ -21,8 +25,11 @@ class TargetQuery extends \yii\db\ActiveQuery
       }
       public function forWidgets()
       {
-        $this->select(['t.id,t.ip,t.difficulty,t.rootable, count(distinct treasure.id) as total_treasures, count(distinct finding.id) as total_findings,count(distinct player_treasure.treasure_id) as player_treasures,count(distinct player_finding.finding_id) as player_findings, (((count(distinct player_treasure.treasure_id)+count(distinct player_finding.finding_id))*100)/(count(distinct finding.id)+count(distinct treasure.id))) as progress']);
-
+        return $this->select(['t.id,t.ip,t.difficulty,t.rootable, count(distinct treasure.id) as total_treasures, count(distinct finding.id) as total_findings,count(distinct player_treasure.treasure_id) as player_treasures,count(distinct player_finding.finding_id) as player_findings, (((count(distinct player_treasure.treasure_id)+count(distinct player_finding.finding_id))*100)/(count(distinct finding.id)+count(distinct treasure.id))) as progress']);
+      }
+      public function not_in_network()
+      {
+        return $this->andWhere('t.id not in (select target_id from network_target)');
       }
       public function player_progress($player_id=0)
       {
