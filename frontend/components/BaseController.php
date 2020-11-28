c9b44bdec41f7a43bfed0275577e978b5d57b358
Pantelis Roditis <proditis@echothrust.com>
2020-11-28 15:50:25 +0200

    initial base controller with defaults access rules these rules check for `event_start`/`event_end`, teams enabled and teams required, as well as disabled routes

---
+++ frontend/components/BaseController.php
@@ -0,0 +1,69 @@
+<?php
+namespace app\components;
+
+use Yii;
+use yii\filters\AccessControl;
+
+class BaseController extends \yii\web\Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    'eventStartEnd'=>[
+                       'allow' => false,
+                       'matchCallback' => function ($rule, $action) {
+                         return Yii::$app->sys->event_start!==false && (time()<Yii::$app->sys->event_start || time()>Yii::$app->sys->event_end);
+                       },
+                       'denyCallback' => function() {
+                         Yii::$app->session->setFlash('info', 'This area is disabled until the competition starts');
+                         return  \Yii::$app->getResponse()->redirect(['/profile/me']);
+                       }
+
+                   ],
+                   'teamsAccess'=>[
+                      'allow' => false,
+                      'matchCallback' => function ($rule, $action) {
+                        if(Yii::$app->sys->team_required===false)
+                        {
+                           return false;
+                        }
+
+                        if(Yii::$app->user->identity->teamPlayer===NULL)
+                        {
+                          Yii::$app->session->setFlash('warning', 'You need to join a team before being able to access this area.');
+                          return true;
+                        }
+                        if(Yii::$app->user->identity->teamPlayer->approved!==1)
+                        {
+                          Yii::$app->session->setFlash('warning', 'You need to have your team membership approved before being able to access this area.');
+                          return true;
+                        }
+                        return false;
+                      },
+                      'denyCallback' => function() {
+                        return  \Yii::$app->getResponse()->redirect(['/team/default/index']);
+                      }
+                   ],
+                   'disabledRoute'=>[
+                       'allow' => false,
+                       'matchCallback' => function ($rule, $action) {
+                         return Yii::$app->DisabledRoute->disabled($action);
+                       },
+                       'denyCallback' => function() {
+                         throw new \yii\web\HttpException(404,'This area is disabled.');
+                       },
+                   ],
+                ],
+            ],
+        ];
+    }
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
