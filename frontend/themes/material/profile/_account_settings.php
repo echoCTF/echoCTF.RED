6862c691ead073d3462e6246d2110904ef8d53be
Pantelis Roditis <proditis@echothrust.com>
2020-12-09 16:40:30 +0200

    add hint about changing email address

--- frontend/themes/material/profile/_account_settings.php
+++ frontend/themes/material/profile/_account_settings.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 <div class="account-form">
     <?=$form->field($model, 'username')->textInput(['maxlength' => true, 'autocomplete'=>"off"]) ?>
 		<?=$form->field($model, 'fullname')->textInput(['maxlength' => true, 'autocomplete'=>"off"]) ?>
-		<?=$form->field($model, 'email')->textInput(['maxlength' => true, 'autocomplete'=>"off"]) ?>
+		<?=$form->field($model, 'email')->textInput(['maxlength' => true, 'autocomplete'=>"off"])->hint('<small class="text-danger">Changing your email address will require verification again. Your account will be deactivated in the meantime.</small>')?>
     <div class="row">
       <div class="col-lg-6">
 		      <?=$form->field($model, 'new_password')->passwordInput(['autocomplete'=>"new-password"]) ?>
