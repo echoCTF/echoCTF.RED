d0921f3ec7a1beda1ecde0854bf7bb986eace477
Pantelis Roditis <proditis@echothrust.com>
2020-12-12 01:10:35 +0200

    remove cron related methods

--- backend/commands/TargetController.php
+++ backend/commands/TargetController.php
@@ -18,49 +18,6 @@ use Http\Client\Socket\Exception\ConnectionException;
 class TargetController extends Controller {
 
   /*
-   * Check for targets that need to power up based on their scheduled_at details
-   * or targets that have been changed during the past $interval per $unit
-   * (default is during the last 5 minutes)
-   * @param int $interval.
-   * @param string $unit (MySQL INTERVAL eg MONTH, DAY, HOUR, MINUTE, SECOND).
-   */
-  public function actionCron($interval=5, $unit="MINUTE")
-  {
-    // Check targets with scheduled_at>=NOW()
-    $targets=Target::find();
-    $targets->where(new \yii\db\Expression("IFNULL(scheduled_at,NOW())<NOW() OR (ts >= NOW() - INTERVAL {$interval} {$unit} and active=1)"))
-    ->orWhere(new \yii\db\Expression("id IN (SELECT target_id FROM finding WHERE ts >=NOW() - INTERVAL {$interval} {$unit})"));
-    //echo $targets->createCommand()->getRawSql();
-
-    foreach($targets->all() as $target)
-    {
-      printf("Target %s ", $target->fqdn);
-      switch($target->status)
-      {
-        case 'offline':
-        case 'powerdown':
-          printf("scheduled for [%s] at [%s]", $target->status, $target->scheduled_at);
-          $target->powerdown();
-          printf(", destroyed: %s\n", $requirePF ? "success" : "fail");
-          break;
-        case 'powerup':
-          $target->pull();
-          printf("scheduled for [%s] at %s, spin: %s\n", $target->status, $target->scheduled_at, $target->spin() ? "success" : "fail");
-          $target->status='online';
-          $target->scheduled_at=null;
-          $target->active=1;
-          $target->save();
-          break;
-        default:
-          printf("updated at %s\n", $target->ts);
-          break;
-      }
-      $this->actionPf(true);
-    }
-    //foreach target check
-  }
-
-  /*
    * Destroy a docker container for the given $target_id
    * @param int $target_id.
    */
@@ -253,66 +210,6 @@ class TargetController extends Controller {
         }
       }
     } // end docker servers
-  }
-
-  /**
-   * Populate pf related tables and rules for targets
-   */
-  public function actionPf($load=false,$base="/etc")
-  {
-    $this->active_targets_pf($base);
-    $this->match_findings($load,$base);
-  }
-
-  /*
-   * Geneate match rules for target findings and load them
-   */
-  private function match_findings($load,$base="/etc")
-  {
-    $networks=$rules=$frules=array();
-    $findings=Finding::find()->joinWith(['target'])->where(['target.active'=>true])->all();
-    foreach($findings as $finding)
-    {
-      if($finding->target->network)
-      {
-        $networks[$finding->target->network->codename]=$finding->target->network;
-      }
-      $frules[]=$finding->matchRule;
-    }
-
-    Pf::store($base.'/match-findings-pf.conf',ArrayHelper::merge($frules,$rules));
-
-    if($load)
-      Pf::load_anchor_file("offense/findings","$base/match-findings-pf.conf");
-  }
-
-  /*
-   * Find and store active targets IP addresses on their PF table
-   */
-  private function active_targets_pf($base="/etc")
-  {
-    $ips=$networks=$rules=array();
-    $targets=Target::find()->where(['active'=>true])->all();
-    foreach($targets as $target)
-    {
-      if($target->networkTarget === NULL)
-        $ips[]=$target->ipoctet;
-      else {
-        $networks[$target->network->codename][]=$target->ipoctet;
-        $rules[]=Pf::allowToNetwork($target);
-        $rules[]=Pf::allowToClient($target);
-      }
-    }
-    Pf::store($base.'/targets.conf',$ips);
-    Pf::load_table_file('targets',$base.'/targets.conf');
-    foreach($networks as $key => $val) {
-      Pf::store($base.'/'.$key.'.conf', $val);
-      Pf::load_table_file($key,$base.'/'.$key.'.conf');
-      $rules[]=sprintf("pass inet proto udp from <%s> to (targets:0) port 53",$key);
-    }
-
-    Pf::store("$base/targets_networks.conf",$rules);
-    Pf::load_anchor_file("targets/networks","$base/targets_networks.conf");
   }
 
   /*
