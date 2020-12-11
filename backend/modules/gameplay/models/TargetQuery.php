33978ad48bb26d4618614874f3958fbb0731826e
Pantelis Roditis <proditis@echothrust.com>
2020-12-12 01:10:52 +0200

    update target query to filter based on pending powerup/down and offline

--- backend/modules/gameplay/models/TargetQuery.php
+++ backend/modules/gameplay/models/TargetQuery.php
@@ -14,6 +14,22 @@ class TargetQuery extends \yii\db\ActiveQuery
         return $this->andWhere('[[status]]=1');
     }*/
 
+    public function powerup()
+    {
+      return $this->andWhere(['status'=>'powerup'])->andWhere(new \yii\db\Expression("scheduled_at is not null"))->andWhere('scheduled_at<NOW()');
+    }
+
+    public function powerdown()
+    {
+      return $this->andWhere(['status'=>'powerdown'])->andWhere(new \yii\db\Expression("scheduled_at is not null"))->andWhere('scheduled_at<NOW()');
+    }
+
+    public function offline()
+    {
+      return $this->andWhere(['status'=>'offline'])->andWhere(new \yii\db\Expression("scheduled_at is not null"))->andWhere('scheduled_at<NOW()');
+    }
+
+
     public function docker_servers()
     {
         return $this->select('server')->distinct()->andWhere(['like','server',"tcp://%:2376"]);
