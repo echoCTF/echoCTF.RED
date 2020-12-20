a26013d0f761978622bbb54f9d50e9f0d54260ab
Pantelis Roditis <proditis@echothrust.com>
2020-12-21 01:22:18 +0200

    introduce mailer component

---
+++ backend/components/Mailer.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\components;
+use Yii;
+class Mailer extends \yii\swiftmailer\Mailer
+{
+  public function init()
+  {
+    parent::init();
+
+    $this->useFileTransport=Yii::$app->sys->mail_useFileTransport;
+
+    if(Yii::$app->sys->mail_host !== false)
+      $this->transport->setHost(Yii::$app->sys->mail_host);
+
+    if(Yii::$app->sys->mail_port !== false)
+      $this->transport->setPort(Yii::$app->sys->mail_port);
+
+    if(Yii::$app->sys->mail_username !== false)
+      $this->transport->setUserName(Yii::$app->sys->mail_username);
+
+    if(Yii::$app->sys->mail_password !== false)
+      $this->transport->setPassword(Yii::$app->sys->mail_password);
+  }
+}
