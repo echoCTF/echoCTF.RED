e0ae63a318f61a00de69ed44f4c7af42e5c36df5
Pantelis Roditis <proditis@echothrust.com>
2020-12-21 01:32:34 +0200

    phpdoc and removal of init

--- frontend/models/PlayerQuery.php
+++ frontend/models/PlayerQuery.php
@@ -2,18 +2,19 @@
 namespace app\models;
 
 use Yii;
-
+/**
+ * This is the ActiveQuery class for [[Player]].
+ *
+ * @see Player
+ */
 class PlayerQuery extends \yii\db\ActiveQuery
 {
-      public function init()
-      {
-//          $this->andOnCondition([$this->modelClass::tableName() . '.branch_id' => Yii::$app->user->identity->branch_id ]);
-          parent::init();
-      }
+
       public function active()
       {
         return $this->andWhere(['active' => 1, 'status'=>10]);
       }
+
       public function with_score()
       {
         $this->joinWith(['playerScore']);
