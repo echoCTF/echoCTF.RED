74871b75e1b4ed38644721638a8f3dc56df353a6
Pantelis Roditis <proditis@echothrust.com>
2020-12-20 02:51:49 +0200

    attempt to split some operations

--- frontend/components/User.php
+++ frontend/components/User.php
@@ -20,7 +20,7 @@ use yii\web\ForbiddenHttpException;
  *
  */
 
-class User extends \yii\web\User
+class User extends UserExtraMethods
 {
     private $_identity = false;
     private $_access;
@@ -82,22 +82,6 @@ class User extends \yii\web\User
     }
 
     /**
-     * Logs in a user by the given access token.
-     * This method will first authenticate the user by calling [[IdentityInterface::findIdentityByAccessToken()]]
-     * with the provided access token. If successful, it will call [[login()]] to log in the authenticated user.
-     * If authentication fails or [[login()]] is unsuccessful, it will return null.
-     * @param string $token the access token
-     * @param mixed $type the type of the token. The value of this parameter depends on the implementation.
-     * For example, [[\yii\filters\auth\HttpBearerAuth]] will set this parameter to be `yii\filters\auth\HttpBearerAuth`.
-     * @return null the identity associated with the given access token. Null is returned if
-     * the access token is invalid or [[login()]] is unsuccessful.
-     */
-    public function loginByAccessToken($token, $type = null)
-    {
-        return null;
-    }
-
-    /**
      * Logs in a user by cookie.
      *
      * This method attempts to log in a user using the ID and authKey information
@@ -117,54 +101,6 @@ class User extends \yii\web\User
                 $this->afterLogin($identity, true, $duration);
             }
         }
-    }
-
-    /**
-     * Returns a value indicating whether the user is a guest (not authenticated).
-     * @return bool whether the current user is a guest.
-     * @see getIdentity()
-     */
-    public function getIsGuest()
-    {
-        return $this->getIdentity() === null;
-    }
-
-    /**
-     * Returns a value that uniquely represents the user.
-     * @return string|int the unique identifier for the user. If `null`, it means the user is a guest.
-     * @see getIdentity()
-     */
-    public function getId()
-    {
-        $identity = $this->getIdentity();
-
-        return $identity !== null ? $identity->getId() : null;
-    }
-
-    /**
-     * Returns the URL that the browser should be redirected to after successful login.
-     *
-     * This method reads the return URL from the session. It is usually used by the login action which
-     * may call this method to redirect the browser to where it goes after successful authentication.
-     *
-     * @param string|array $defaultUrl the default return URL in case it was not set previously.
-     * If this is null and the return URL was not set previously, [[Application::homeUrl]] will be redirected to.
-     * Please refer to [[setReturnUrl()]] on accepted format of the URL.
-     * @return string the URL that the user should be redirected to after login.
-     * @see loginRequired()
-     */
-    public function getReturnUrl($defaultUrl = null)
-    {
-        $url = Yii::$app->getSession()->get($this->returnUrlParam, $defaultUrl);
-        if (is_array($url)) {
-            if (isset($url[0])) {
-                return Yii::$app->getUrlManager()->createUrl($url);
-            }
-
-            $url = null;
-        }
-
-        return $url === null ? Yii::$app->getHomeUrl() : $url;
     }
 
 
@@ -246,39 +182,6 @@ class User extends \yii\web\User
         return null;
     }
 
-    protected function identitySanityCheck($identity,$class)
-    {
-      if (!$identity instanceof \app\models\Player) {
-          throw new InvalidValueException("$class::findIdentity() must return an object implementing IdentityInterface.");
-      }
-    }
-
-
-    protected function getIdFromSession($session)
-    {
-      return $session->getHasSessionId() || $session->getIsActive() ? $session->get($this->idParam) : null;
-    }
-
-    protected function getTimeoutsSet()
-    {
-      return ($this->authTimeout !== null || $this->absoluteAuthTimeout !== null);
-    }
-    protected function getExpire()
-    {
-      return $this->authTimeout !== null ? $session->get($this->authTimeoutParam) : null;
-    }
-    protected function getExpireAbsolute()
-    {
-      $session=Yii::$app->getSession();
-      return $this->absoluteAuthTimeout !== null ? $session->get($this->absoluteAuthTimeoutParam) : null;
-    }
-    protected function getExpired()
-    {
-      $expire = $this->expire;
-      $expireAbsolute = $this->expireAbsolute;
-      return ($expire !== null && $expire < time() || $expireAbsolute !== null && $expireAbsolute < time());
-
-    }
     /**
      * Updates the authentication status using the information from session and cookie.
      *
@@ -318,22 +221,5 @@ class User extends \yii\web\User
         $this->doAutoLogin($identity,$id);
     }
 
-    protected function doAutoLogin($identity,$id)
-    {
-      if ($this->enableAutoLogin) {
-          if ($this->getIsGuest()) {
-              $this->loginByCookie();
-          } elseif ($this->autoRenewCookie) {
-              $authKey = json_decode(Yii::$app->getRequest()->getCookies()->getValue($this->identityCookie['name']),TRUE)[1];
-              if($identity->validateAuthKey($authKey)) {
-                  $this->renewIdentityCookie();
-                  Yii::info("User $id succeeded authKey validation");
-              } else {
-                  $this->logout();
-                  Yii::info("User $id failed authKey validation");
-              }
-          }
-      }
-    }
 
 }
