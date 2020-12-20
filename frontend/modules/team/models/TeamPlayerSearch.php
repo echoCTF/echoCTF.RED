a83ad0eb24ad0bbc3a0af189b06c327258d3890e
Pantelis Roditis <proditis@echothrust.com>
2020-12-20 23:01:47 +0200

    propert path for TeamPlayer model

--- frontend/modules/team/models/TeamPlayerSearch.php
+++ frontend/modules/team/models/TeamPlayerSearch.php
@@ -4,7 +4,7 @@ namespace app\modules\team\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\modules\frontend\models\TeamPlayer;
+use app\modules\team\models\TeamPlayer;
 
 /**
  * TeamPlayerSearch represents the model behind the search form of `app\modules\frontend\models\TeamPlayer`.
