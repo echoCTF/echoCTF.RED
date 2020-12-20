a4f5ad631f7f9946485af651a8922874344d5252
Pantelis Roditis <proditis@echothrust.com>
2020-12-20 15:45:17 +0200

    move missing methods

--- backend/commands/CronController.php
+++ backend/commands/CronController.php
@@ -231,4 +231,41 @@ class CronController extends Controller {
     return $unhealthy;
   }
 
+  /**
+   * Connect to a docker server API and return docker client object
+   */
+  private function docker_connect($remote_socket)
+  {
+      $client=DockerClientFactory::create([
+        'remote_socket' => $remote_socket,
+        'ssl' => false,
+      ]);
+    try
+    {
+      $docker=Docker::create($client);
+    }
+    catch(\Exception $e)
+    {
+      return false;
+    }
+    return $docker;
+  }
+
+  /**
+   * Get a list of containers from a connected docker
+   */
+  private function containers_list($docker)
+  {
+    if($docker === false) return [];
+    try
+    {
+      $containerList=$docker->containerList();
+    }
+    catch(\Exception $e)
+    {
+      return [];
+    }
+    return $containerList;
+  }
+
 }
