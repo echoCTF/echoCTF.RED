4f0d39b9c93fd2346fd0741ab6e87c536ef74a8f
Pantelis Roditis <proditis@echothrust.com>
2020-12-24 02:52:03 +0200

    remove tutorial target since we added instructions on the docs

--- ansible/Dockerfiles/alephone.tutorial/stack_overflow.c
+++
@@ -1,9 +0,0 @@
-/* stack_overflow.c */
-// EXAMPLE_REPLACE_PLACEHOLDER
-void main(int argc, char *argv[]) {
-  char buffer[512];
-  char ETSCTF_FLAG[]="EXAMPLE_FLAG_PLACEHOLDER";
-
-  if (argc > 1)
-    strcpy(buffer,argv[1]);
-}
