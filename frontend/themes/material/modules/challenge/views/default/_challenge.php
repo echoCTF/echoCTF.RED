0cef71ce1e5c483f42a3fa778a4c8d3e924447e4
Pantelis Roditis <proditis@echothrust.com>
2021-01-07 22:25:32 +0200

    change challenge listing into cards

--- frontend/themes/material/modules/challenge/views/default/_challenge.php
+++ frontend/themes/material/modules/challenge/views/default/_challenge.php
@@ -5,13 +5,13 @@ use yii\helpers\Url;
 use yii\widgets\DetailView;
 use app\widgets\Twitter;
 ?>
-<div class="challenge-view well">
-    <h3><b><?=Html::a($model->name, ['view', 'id'=>$model->id])?> <?php if($model->completed):?><i class="fas fa-check-double text-primary"></i> <?=Twitter::widget(['message'=>'Hey check this out, I completed the challenge '.$model->name, 'url'=>Url::to(['view', 'id'=>$model->id], 'https')]);?><?php else:?><?=Twitter::widget(['message'=>'I currently grinding the challenge '.$model->name, 'url'=>Url::to(['view', 'id'=>$model->id], 'https')]);?><?php endif;?></b></h3>
-  	<h4><b>Category:</b> <?=Html::encode($model->category);?></h4>
-  	<h4><b>Difficulty:</b> <?=Html::encode($model->difficulty)?></h4>
-    <h4><b>Points:</b> <?=Html::encode(number_format($model->points));?></h4>
-
-    <p><?=$model->description;?></p>
+<div class="col-xl-4 col-sm-6 d-flex align-items-stretch">
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title"><b><?=Html::a($model->name, ['view', 'id'=>$model->id])?>
+      <?php if($model->completed):?><i class="fas fa-check-double text-primary"></i> <?=Twitter::widget(['message'=>'Hey check this out, I completed the challenge '.$model->name, 'url'=>Url::to(['view', 'id'=>$model->id], 'https')]);?><?php else:?><?=Twitter::widget(['message'=>'I currently grinding the challenge '.$model->name, 'url'=>Url::to(['view', 'id'=>$model->id], 'https')]);?><?php endif;?></b></h4>
+      <p class="card-text"><b>Category:</b> <?=Html::encode($model->category);?><br/><b>Difficulty:</b> <?=Html::encode($model->difficulty)?></p>
+    </div>
+    <div class="card-footer"><?=Html::a("Go to challenge", ['view', 'id'=>$model->id],['class'=>'btn btn-primary card-link'])?></div>
+  </div>
 </div>
-<br/>
-<br/>
