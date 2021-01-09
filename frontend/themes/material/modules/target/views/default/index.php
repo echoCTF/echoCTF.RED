3438243f4d5d9834f3345c596e5c0cd66804160e
Pantelis Roditis <proditis@echothrust.com>
2021-01-09 22:11:25 +0200

    update target/default/index * div class names for target/default/index * Change statistics cards * Remove leaderboard

--- frontend/themes/material/modules/target/views/default/index.php
+++ frontend/themes/material/modules/target/views/default/index.php
@@ -7,9 +7,10 @@ use app\widgets\stream\StreamWidget as Stream;
 $this->_fluid="-fluid";
 $this->title=Yii::$app->sys->event_name.' Targets';
 $this->_description="The echoCTF dashboard page";
+$hidden_attributes=['id'];
 ?>
 
-<div class="dashboard-index">
+<div class="target-index">
   <div class="body-content">
     <div class="row">
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
@@ -18,10 +19,23 @@ $this->_description="The echoCTF dashboard page";
                 'type'=>'card-stats',
                 'icon'=>'<i class="fa fa-flag"></i>',
                 'color'=>'primary',
-                'title'=>sprintf('%d / %d', $treasureStats->claimed, $treasureStats->total),
-                'subtitle'=>'Claimed / Total Flags',
+                'title'=>\app\modules\target\models\Treasure::find()->count(),
+                'subtitle'=>'Flags',
                 'footer'=>'<div class="stats">
-                        <i class="material-icons flag-claim">flag</i>'.number_format($treasureStats->claims).' total claims
+                        <i class="material-icons text-danger">flag</i>'.$treasureStats->claimed.' claimed by you
+                      </div>',
+            ]);Card::end();?>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+            <?php Card::begin([
+                'header'=>'header-icon',
+                'type'=>'card-stats',
+                'icon'=>'<i class="fas fa-fingerprint"></i>',
+                'color'=>'warning',
+                'title'=>\app\modules\target\models\Finding::find()->count(),
+                'subtitle'=>'Services',
+                'footer'=>'<div class="stats">
+                        <i class="material-icons text-danger">track_changes</i> '.count(Yii::$app->user->identity->playerFindings).' services found by you
                       </div>',
             ]);Card::end();?>
         </div>
@@ -34,57 +48,38 @@ $this->_description="The echoCTF dashboard page";
                 'title'=>sprintf('%d', $totalHeadshots),
                 'subtitle'=>'Headshots',
                 'footer'=>'<div class="stats">
-                        <i class="material-icons">memory</i> You have '.count(Yii::$app->user->identity->headshots).' headshots
+                        <i class="material-icons text-danger">memory</i> '.count(Yii::$app->user->identity->headshots).' headshots by you
                       </div>',
             ]);Card::end();?>
         </div>
+
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
             <?php Card::begin([
                 'header'=>'header-icon',
                 'type'=>'card-stats',
-                'icon'=>'<i class="fas fa-server"></i>',
-                'color'=>'warning',
-                'title'=>\app\modules\target\models\Target::find()->active()->count(),
-                'subtitle'=>'Targets',
-                'footer'=>'<div class="stats">
-                        <i class="material-icons">memory</i> '.number_format($totalPoints).' Total points
-                      </div>',
-            ]);Card::end();?>
-        </div>
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-            <?php Card::begin([
-                'type'=>'card-stats',
-                'header'=>'header-icon',
-                'icon'=>'<i class="fas fa-user-secret"></i>',
+                'icon'=>'<i class="fas fa-medal"></i>',
                 'color'=>'info',
-                'title'=>\app\models\Player::find()->active()->count(),
-                'subtitle'=>'Users',
+                'title'=>number_format($totalPoints),
+                'subtitle'=>'Points',
                 'footer'=>'<div class="stats">
-                        <i class="material-icons">update</i> '.\app\models\Player::find()->active()->with_score()->count().' with score
+                        <i class="material-icons text-danger">format_list_numbered</i> '.number_format(Yii::$app->user->identity->playerScore->points).' yours
                       </div>',
             ]);Card::end();?>
         </div>
+
     </div>
-
-
     <div class="row">
-      <div class="col-xl-8 col-lg-12 col-sm-12">
+      <div class="col">
       <?php Pjax::begin(['id'=>'target-listing', 'enablePushState'=>false, 'linkSelector'=>'#target-pager a', 'formSelector'=>false]);?>
-      <?php echo TargetWidget::widget(['dataProvider' => null, 'player_id'=>Yii::$app->user->id,'pageSize'=>8,'buttonsTemplate'=>'{tweet}']);?>
+      <?php echo TargetWidget::widget(['dataProvider' => null, 'hidden_attributes'=>$hidden_attributes,'player_id'=>Yii::$app->user->id,'pageSize'=>10,'buttonsTemplate'=>null]);?>
       <?php Pjax::end();?>
       </div>
-      <div class="col-xl-4 col-sm-12">
-<?php
-Pjax::begin(['id'=>'leaderboard-listing', 'enablePushState'=>false, 'linkSelector'=>'#leaderboard-pager a', 'formSelector'=>false]);
-echo Leaderboard::widget(['dataProvider'=>null, 'player_id'=>Yii::$app->user->id, 'divID'=>"Leaderboard", 'title'=>'Leaderboard','pageSize'=>10]);
-Pjax::end();
-?>
-      </div>
     </div><!-- //row -->
-<?php
-Pjax::begin(['id'=>'stream-listing', 'enablePushState'=>false, 'linkSelector'=>'#stream-pager a', 'formSelector'=>false]);
-echo Stream::widget(['divID'=>'stream', 'dataProvider' => null, 'pagerID'=>'stream-pager']);
-Pjax::end();
-?>
+      <?php
+      Pjax::begin(['id'=>'stream-listing','enablePushState'=>false, 'linkSelector'=>'#stream-pager a', 'formSelector'=>false]);
+      echo Stream::widget(['divID'=>'stream', 'dataProvider' => null, 'pagerID'=>'stream-pager']);
+      Pjax::end();
+      ?>
+
   </div><!-- //body-content -->
 </div>
