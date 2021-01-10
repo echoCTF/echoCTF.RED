f5247dd60b62c6f30099ba54de4a21fd32d61f0e
Pantelis Roditis <proditis@echothrust.com>
2021-01-10 04:19:01 +0200

    add estimation based on current page and size + index

--- frontend/themes/material/modules/game/views/leaderboards/_most_headshot.php
+++ frontend/themes/material/modules/game/views/leaderboards/_most_headshot.php
@@ -5,7 +5,7 @@ use app\components\formatters\RankFormatter;
 <div class="leader" >
     <div class="border"></div>
     <div class="leader-wrap">
-      <div class="leader-place"><?=RankFormatter::ordinalPlace($index+1);?>.</div>
+      <div class="leader-place"><?=RankFormatter::ordinalPlace((($widget->dataProvider->pagination->getPage())*$widget->dataProvider->pagination->pageSize)+($index+1));?>.</div>
       <div class="leader-ava"><img src="/images/avatars/<?=$model->player->profile->avtr?>"  class="rounded" width="30px"/></div>
       <div class="leader-name" style="width: 100%"><?=$model->player->profile->link?></div>
       <div class="leader-score_title" style="width: 100%"><?=number_format($model->timer);?></div>
