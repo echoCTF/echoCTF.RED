c2b9b9ef7ca8ab0de0c909520f4d9f64a1fc1976
Pantelis Roditis <proditis@echothrust.com>
2021-01-14 12:00:27 +0200

    add trigger

---
+++ backend/migrations/m210114_095711_create_tbi_challenge_solver_trigger.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210114_095711_create_tbi_challenge_solver_trigger
+ */
+class m210114_095711_create_tbi_challenge_solver_trigger extends Migration
+{
+  public $DROP_SQL="DROP TRIGGER IF EXISTS {{%tbi_challenge_solver}}";
+  public $CREATE_SQL="CREATE TRIGGER {{%tbi_challenge_solver}} BEFORE INSERT ON {{%challenge_solver}} FOR EACH ROW
+  thisBegin:BEGIN
+    IF (@TRIGGER_CHECKS = FALSE) THEN
+      LEAVE thisBegin;
+    END IF;
+    IF (SELECT count(*) FROM challenge_solver WHERE target_id=NEW.target_id)=0 THEN
+      SET NEW.first=1;
+    END IF;
+  END";
+
+  public function up()
+  {
+    $this->db->createCommand($this->DROP_SQL)->execute();
+    $this->db->createCommand($this->CREATE_SQL)->execute();
+  }
+
+  public function down()
+  {
+    $this->db->createCommand($this->DROP_SQL)->execute();
+  }
+}
