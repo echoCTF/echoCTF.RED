719c399ae888fb0c92db9e8bc045601a32071fb9
Pantelis Roditis <proditis@echothrust.com>
2021-01-14 14:13:36 +0200

    fix challenge_solver rating default to -1

---
+++ backend/migrations/m210114_121019_alter_rating_default_value_on_challenge_solver_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210114_121019_alter_rating_default_value_on_challenge_solver_table
+ */
+class m210114_121019_alter_rating_default_value_on_challenge_solver_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+      $this->alterColumn('challenge_solver', 'rating', $this->integer()->defaultValue(-1));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+      $this->alterColumn('challenge_solver', 'rating', $this->integer()->defaultValue(null));
+    }
+}
