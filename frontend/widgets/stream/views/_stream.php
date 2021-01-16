5461169a4b413a699a4ced92ed8d99980687c59b
Pantelis Roditis <proditis@echothrust.com>
2021-01-16 21:02:32 +0200

    add stream widget support to have other twitter message than the default

--- frontend/widgets/stream/views/_stream.php
+++ frontend/widgets/stream/views/_stream.php
@@ -6,7 +6,7 @@ use app\widgets\Twitter;
     <div class="leader-wrap">
       <div class="leader-name" style="margin-bottom: 0.3em">
         <!--<?=$model->id?>--> <?=$model->formatted;?>, <?=$model->ts_ago?>
-         <?=Twitter::widget(['message'=>'Hey check this out'.strip_tags($model->formatted.', '.$model->ts_ago)]);?><!--<?=$model->ts?>-->
+         <?php $model->twitter=true; echo Twitter::widget(['message'=>'Hey check this out '.strip_tags($model->formatted)]);?><!--<?=$model->ts?>-->
       </div>
     </div>
     <div class="border"></div>
