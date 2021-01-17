f101b08f1c93b92a7069420084ed430eb8009dd2
Pantelis Roditis <proditis@echothrust.com>
2021-01-17 02:34:33 +0200

    make hints widget return all pending or the 10 last hints also make the hints related to treasure or finding link to their corresponding target

--- frontend/widgets/HintsWidget.php
+++ frontend/widgets/HintsWidget.php
@@ -7,6 +7,7 @@ namespace app\widgets;
 
 use yii\base\Widget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 class HintsWidget extends Widget
 {
     public function init()
@@ -17,13 +18,29 @@ class HintsWidget extends Widget
     public function run()
     {
       $links=[];
-      $playerHints=\app\models\PlayerHint::find()->forAjax()->forPlayer((int) \Yii::$app->user->id)->all();
+      $pending=false;
+      $playerHints=\app\models\PlayerHint::find()->forAjax()->forPlayer((int) \Yii::$app->user->id)->pending()->all();
+      if($playerHints==null)
+      {
+        $playerHints=\app\models\PlayerHint::find()->forAjax()->forPlayer((int) \Yii::$app->user->id)->limit(5)->all();
+      }
+
       foreach($playerHints as $ph)
       {
+        $pending=true;
         if(intval($ph->status)===1)
           $ph->updateAttributes(['status' => 0]);
-        $links[]=Html::a($ph->title,'#',['class' => "dropdown-item"]);
+        if($ph->hint->finding_id!=null)
+          $links[]=Html::a($ph->title,Url::to(['/target/default/view','id'=>$ph->hint->finding->target_id]),['class' => "dropdown-item"]);
+        elseif($ph->hint->treasure_id!=null)
+          $links[]=Html::a($ph->title,Url::to(['/target/default/view','id'=>$ph->hint->treasure->target_id]),['class' => "dropdown-item"]);
+        else
+          $links[]=Html::a($ph->title,'#',['class' => "dropdown-item"]);
       }
+
+      if($playerHints==null)
+        $links[]=Html::a('nothing here...','#',['class' => "dropdown-item"]);
+
 
       return implode($links);
     }
