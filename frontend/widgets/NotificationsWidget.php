13a29b06cdd9cd3bb905b29701065dd0c30487ee
Pantelis Roditis <proditis@echothrust.com>
2021-01-17 02:34:48 +0200

    show all pending notifications or last 10

--- frontend/widgets/NotificationsWidget.php
+++ frontend/widgets/NotificationsWidget.php
@@ -17,7 +17,12 @@ class NotificationsWidget extends Widget
     public function run()
     {
       $links=[];
-      $notifications=\app\models\Notification::find()->forPlayer(\Yii::$app->user->id)->forAjax()->orderBy(['created_at'=>SORT_DESC, 'id'=>SORT_DESC])->all();
+      $notifications=\app\models\Notification::find()->forPlayer(\Yii::$app->user->id)->forAjax()->pending()->orderBy(['created_at'=>SORT_DESC, 'id'=>SORT_DESC])->all();
+      if($notifications==null)
+      {
+        $notifications=\app\models\Notification::find()->forPlayer(\Yii::$app->user->id)->forAjax()->orderBy(['created_at'=>SORT_DESC, 'id'=>SORT_DESC])->limit(5)->all();
+      }
+
       foreach($notifications as $n)
       {
         if(intval($n->archived) === 0)
@@ -27,6 +32,9 @@ class NotificationsWidget extends Widget
         }
         $links[]=Html::a($n->title,'#',['class' => "dropdown-item"]);
       }
+      if($notifications==null)
+        $links[]=Html::a('nothing here...','#',['class' => "dropdown-item"]);
+
       return implode($links);
     }
 }
