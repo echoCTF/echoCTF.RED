3aa03ab03dd35518228ffd65e6f5be7f7f43d920
Pantelis Roditis <proditis@echothrust.com>
2021-01-19 22:04:02 +0200

    change primaryKey definition, previous one used auto increment

--- backend/migrations/m210119_125422_create_target_ondemand_table.php
+++ backend/migrations/m210119_125422_create_target_ondemand_table.php
@@ -17,14 +17,14 @@ class m210119_125422_create_target_ondemand_table extends Migration
     public function safeUp()
     {
         $this->createTable('{{%target_ondemand}}', [
-            'target_id' => $this->primaryKey(),
+            'target_id' => $this->integer()->notNull(),
             'player_id' => $this->integer()->unsigned(),
             'state' => $this->tinyInteger()->notNull()->defaultValue(-1),
             'heartbeat' => $this->datetime(),
             'created_at' => $this->datetime(),
             'updated_at' => $this->datetime(),
         ]);
-
+        $this->addPrimaryKey ( 'pidPK', '{{%target_ondemand}}', 'target_id' );
         // creates index for column `target_id`
         $this->createIndex(
             '{{%idx-target_ondemand-target_id}}',
