5242f48aa6baa8d59fd27a8a3b4d5589c15624cf
Pantelis Roditis <proditis@echothrust.com>
2021-03-24 01:33:30 +0200

    update controller to include the necessary extended operations The action now supports the native `filter`, `fields`, `sort`, `per-page `

--- frontend/modules/api/controllers/HeadshotController.php
+++ frontend/modules/api/controllers/HeadshotController.php
@@ -5,15 +5,14 @@ namespace app\modules\api\controllers;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\data\ActiveDataProvider;
-
+use yii\data\ActiveDataFilter;
 class HeadshotController extends \yii\rest\ActiveController
 {
-  public $modelClass="\app\modules\game\models\Headshot";
+  public $modelClass="\app\modules\api\models\Headshot";
   public $serializer=[
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
-
 //    public function behaviors()
 //      {
 //          return ArrayHelper::merge(parent::behaviors(), [
@@ -40,28 +39,66 @@ class HeadshotController extends \yii\rest\ActiveController
 
   public function prepareDataProvider()
   {
-    //\Yii::$app->response->format=\yii\web\Response::FORMAT_JSON;
+    \Yii::$app->response->format=\yii\web\Response::FORMAT_JSON;
 
     $requestParams = Yii::$app->getRequest()->getBodyParams();
     if (empty($requestParams)) {
         $requestParams = Yii::$app->getRequest()->getQueryParams();
     }
-    $query = (new \yii\db\Query())
-        ->select(['profile.id as profile_id', 'target_id','timer','rating','headshot.created_at'])
-        ->from('headshot')
-        ->leftJoin('profile', '`profile`.`player_id` = `headshot`.`player_id`')
-        ->where(['profile.visibility'=>'public'])
-        ->orderBy(['headshot.created_at'=>SORT_DESC,'target_id'=>SORT_ASC]);
+    $filter = new ActiveDataFilter([
+        'searchModel' => '\app\modules\api\models\HeadshotSearch',
+        'attributeMap' => [
+            'profile_id' => 'profile.id',
+            'target_name' => 'target.name',
+            'created_at' => 'headshot.created_at',
+            'timer' => 'headshot.timer'
+        ]
+    ]);
 
-    return new ActiveDataProvider([
+    $filterCondition = null;
+
+    // You may load filters from any source. For example,
+    // if you prefer JSON in request body,
+    // use Yii::$app->request->getBodyParams() below:
+    if ($filter->load(\Yii::$app->request->get())) {
+        $filterCondition = $filter->build();
+        if ($filterCondition === false) {
+            // Serializer would get errors out of it
+            return $filter;
+        }
+    }
+    $query=\app\modules\api\models\Headshot::find()->rest();
+    if ($filterCondition !== null) {
+        $query->andWhere($filterCondition);
+    }
+
+    $dataProvider=new ActiveDataProvider([
       'query'=>$query,
       'pagination' => [
+          'pageSizeLimit' => [1,100],
+          'defaultPageSize'=>10,
           'params' => $requestParams,
       ],
       'sort' => [
-          'params' => $requestParams,
+        'defaultOrder' => ['created_at' => SORT_DESC],
+        'params' => $requestParams,
       ],
     ]);
-
+    $dataProvider->setSort([
+        'attributes' => array_merge(
+            $dataProvider->getSort()->attributes,
+            [
+              'profile_id' => [
+                  'asc' => ['profile.id' => SORT_ASC],
+                  'desc' => ['profile.id' => SORT_DESC],
+              ],
+              'target_name' => [
+                  'asc' => ['target.name' => SORT_ASC],
+                  'desc' => ['target.name' => SORT_DESC],
+              ],
+            ]
+        ),
+    ]);
+    return $dataProvider;
   }
 }
