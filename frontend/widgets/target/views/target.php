8ebd83be12a2fbcd132f87fa6bfb50b66d6d7f31
Pantelis Roditis <proditis@echothrust.com>
2021-03-24 19:26:49 +0200

    add `new` indicator

--- frontend/widgets/target/views/target.php
+++ frontend/widgets/target/views/target.php
@@ -67,7 +67,10 @@ echo GridView::widget([
 
           if(!Yii::$app->user->isGuest && Yii::$app->user->identity->getPlayerHintsForTarget($model->id)->count() > 0)
             $append.=' <sup><abbr title="You have hints on the target"><i class="fas fa-lightbulb text-primary" aria-hidden="true"></i></abbr></sup>';
-
+          if($model->created_at!==NULL && strtotime($model->created_at) >= strtotime('-10 days'))
+          {
+            $append.=' <small class="text-danger">new</small>';
+          }
           return Html::a(Html::encode($model->name), ['/target/default/view', 'id'=>$model->id]).$append;
         }
       ],
