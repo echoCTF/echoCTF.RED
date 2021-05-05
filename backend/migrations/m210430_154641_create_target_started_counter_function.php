ab4d2563e169e1ca2a01c053a5ffa6b1d19114f8
Pantelis Roditis <proditis@echothrust.com>
2021-05-06 01:24:31 +0300

    this is safer option

--- backend/migrations/m210430_154641_create_target_started_counter_function.php
+++ backend/migrations/m210430_154641_create_target_started_counter_function.php
@@ -8,7 +8,7 @@ use yii\db\Migration;
 class m210430_154641_create_target_started_counter_function extends Migration
 {
   public $DROP_SQL="DROP FUNCTION IF EXISTS {{%target_started_count}}";
-  public $CREATE_SQL="CREATE FUNCTION {{%target_started_count}}(n INT UNSIGNED) returns INT NOT DETERMINISTIC
+  public $CREATE_SQL="CREATE FUNCTION {{%target_started_count}}(n INT UNSIGNED) returns INT READS SQL DATA
 BEGIN
 DECLARE counter INT;
 SET counter=(select count(distinct player_id) from stream where (model='finding' and model_id in (select id from finding where target_id=n)) or (model='treasure' and model_id in (select id from treasure where target_id=n)));
--- backend/migrations/m210430_155141_create_target_solved_percentage_function.php
+++ backend/migrations/m210430_155141_create_target_solved_percentage_function.php
@@ -8,7 +8,7 @@ use yii\db\Migration;
 class m210430_155141_create_target_solved_percentage_function extends Migration
 {
   public $DROP_SQL="DROP FUNCTION IF EXISTS {{%target_solved_percentage}}";
-  public $CREATE_SQL="CREATE FUNCTION {{%target_solved_percentage}}(n INT UNSIGNED) returns float NOT DETERMINISTIC
+  public $CREATE_SQL="CREATE FUNCTION {{%target_solved_percentage}}(n INT UNSIGNED) returns float READS SQL DATA
   BEGIN
   DECLARE average_pct float;
   SET average_pct=(select ((select count(*) from headshot where target_id=n)*100)/count(distinct player_id) from stream where (model='finding' and model_id in (select id from finding where target_id=n)) or (model='treasure' and model_id in (select id from treasure where target_id=n)));
