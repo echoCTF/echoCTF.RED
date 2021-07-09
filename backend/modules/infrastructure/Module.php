c456b946c3438cef837b972836182e50f80df924
Pantelis Roditis <proditis@echothrust.com>
2021-07-09 21:39:32 +0300

    add the infrastructure module

---
+++ backend/modules/infrastructure/Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\modules\infrastructure;
+
+/**
+ * infrastructure module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace='app\modules\infrastructure\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
