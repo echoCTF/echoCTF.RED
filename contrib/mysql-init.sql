48dcc22e708ba97b6e1357a6a64edfdfd757bf2e
Pantelis Roditis <proditis@echothrust.com>
2021-11-08 12:52:24 +0200

    add some extra options for memcached performance

--- contrib/mysql-init.sql
+++ contrib/mysql-init.sql
@@ -1,5 +1,7 @@
 DELIMITER ;;
 IF (SELECT count(*) FROM information_schema.tables WHERE table_schema = 'echoCTF' AND table_name = 'devnull' LIMIT 1)>0 THEN
   CALL echoCTF.init_mysql();
+  SELECT memc_servers_behavior_set('MEMCACHED_BEHAVIOR_TCP_NODELAY','1') INTO @devnull;
+  SELECT memc_servers_behavior_set('MEMCACHED_BEHAVIOR_NO_BLOCK','1') INTO @devnull;
 END IF;;
 DELIMITER ;
