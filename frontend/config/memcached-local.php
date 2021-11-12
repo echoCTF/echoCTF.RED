c745d37a56b9a382078f041fc1f7fe58909cb350
Pantelis Roditis <proditis@echothrust.com>
2021-11-12 14:14:23 +0200

    add some memcached optimisations by default

--- frontend/config/memcached-local.php
+++ frontend/config/memcached-local.php
@@ -1,6 +1,11 @@
 <?php
 return [
     'class' => 'yii\caching\MemCache',
+    'options' => [
+            \Memcached::OPT_NO_BLOCK => true,
+            \Memcached::OPT_NOREPLY => true,
+            \Memcached::OPT_TCP_NODELAY => true,
+    ],
     'servers' => [
         [
             'host' => '127.0.0.1',
