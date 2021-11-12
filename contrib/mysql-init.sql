a89e7ce36577f490ebe6d63f29a7698f45cf9454
Pantelis Roditis <proditis@echothrust.com>
2021-11-12 14:13:39 +0200

    change selects to to DO's

--- contrib/mysql-init.sql
+++ contrib/mysql-init.sql
@@ -1,7 +1,7 @@
 DELIMITER ;;
 IF (SELECT count(*) FROM information_schema.tables WHERE table_schema = 'echoCTF' AND table_name = 'devnull' LIMIT 1)>0 THEN
-  CALL echoCTF.init_mysql();
-  SELECT memc_servers_behavior_set('MEMCACHED_BEHAVIOR_TCP_NODELAY','1') INTO @devnull;
-  SELECT memc_servers_behavior_set('MEMCACHED_BEHAVIOR_NO_BLOCK','1') INTO @devnull;
+  DO echoCTF.init_mysql();
+  DO memc_servers_behavior_set('MEMCACHED_BEHAVIOR_TCP_NODELAY','1');
+  DO memc_servers_behavior_set('MEMCACHED_BEHAVIOR_NO_BLOCK','1');
 END IF;;
 DELIMITER ;
