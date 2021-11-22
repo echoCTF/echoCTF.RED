2497184872d31dbac92db4cf01ed9de6da0db182
Pantelis Roditis <proditis@echothrust.com>
2021-11-22 14:00:36 +0200

    DO will not do for init :facepalm:

--- contrib/mysql-init.sql
+++ contrib/mysql-init.sql
@@ -1,6 +1,6 @@
 DELIMITER ;;
 IF (SELECT count(*) FROM information_schema.tables WHERE table_schema = 'echoCTF' AND table_name = 'devnull' LIMIT 1)>0 THEN
-  DO echoCTF.init_mysql();
+  CALL echoCTF.init_mysql();
   DO memc_servers_behavior_set('MEMCACHED_BEHAVIOR_TCP_NODELAY','1');
   DO memc_servers_behavior_set('MEMCACHED_BEHAVIOR_NO_BLOCK','1');
 END IF;;
