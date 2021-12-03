4b4bcc248fb91075bf48d5f5aee51923de1c468d
Pantelis Roditis <proditis@echothrust.com>
2021-12-04 01:37:04 +0200

    add table to hold email templates

---
+++ backend/migrations/m211203_230639_create_email_template_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%email_template}}`.
+ */
+class m211203_230639_create_email_template_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%email_template}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(32),
+            'title' => $this->string(),
+            'html' => $this->text(),
+            'txt' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%email_template}}');
+    }
+}
