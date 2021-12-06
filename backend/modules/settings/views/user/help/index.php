e30e4572f5029095af26e717d36f585aa7f38269
Pantelis Roditis <proditis@echothrust.com>
2021-12-06 14:43:43 +0200

    Add a bit of information on the user pages

--- backend/modules/settings/views/user/help/index.php
+++ backend/modules/settings/views/user/help/index.php
@@ -1,1 +1,5 @@
-<p>Comming soon...</p>
+These are users that are able to access the administrative interfaces. These
+users are not visible nor are able to access the frontend interface with these
+credentials.
+
+These users are also allowed to manipulate platform parameters through the REST API.
