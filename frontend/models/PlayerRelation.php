74c05476dfd6e317f71a2e2ff3f8b81802f4fa01
Pantelis Roditis <proditis@echothrust.com>
2021-12-07 19:54:23 +0200

    introduce frontend player relations model

---
+++ frontend/models/PlayerRelation.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "player_relation".
+ *
+ * @property int $player_id
+ * @property int|null $referred_id
+ *
+ * @property Player $player
+ */
+class PlayerRelation extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'player_relation';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['player_id'], 'required'],
+            [['player_id', 'referred_id'], 'integer'],
+            [['player_id'], 'unique'],
+            [['player_id'], 'exist', 'skipOnError' => true, 'targetClass' => Player::className(), 'targetAttribute' => ['player_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'player_id' => Yii::t('app', 'Player ID'),
+            'referred_id' => Yii::t('app', 'Referred ID'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Player]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getPlayer()
+    {
+        return $this->hasOne(Player::className(), ['id' => 'player_id']);
+    }
+
+
+    /**
+     * Gets query for referred [[Player]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getReferred()
+    {
+        return $this->hasOne(Player::className(), ['id' => 'referred_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return PlayerRelationQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new PlayerRelationQuery(get_called_class());
+    }
+}
---
+++ frontend/models/PlayerRelationQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the ActiveQuery class for [[PlayerRelation]].
+ *
+ * @see PlayerRelation
+ */
+class PlayerRelationQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * {@inheritdoc}
+     * @return PlayerRelation[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return PlayerRelation|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
