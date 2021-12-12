dd9cb76d54bec83bee81259eedf236d7ccc6d24a
Pantelis Roditis <proditis@echothrust.com>
2021-12-12 11:56:34 +0200

    add the relative models on frontend

---
+++ frontend/modelscli/BannedMxServerQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\modelscli;
+
+/**
+ * This is the ActiveQuery class for [[BannedMxServer]].
+ *
+ * @see BannedMxServer
+ */
+class BannedMxServerQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * {@inheritdoc}
+     * @return BannedMxServer[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return BannedMxServer|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
