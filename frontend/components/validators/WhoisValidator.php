1ebbd7d811597287a4aebfedd51e5f23069e640c
Pantelis Roditis <proditis@echothrust.com>
2021-12-12 02:26:45 +0200

    Add initial whois validation

---
+++ frontend/components/validators/WhoisValidator.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Check memcache key for registrations performed by this IP.
+ */
+namespace app\components\validators;
+
+use yii\validators\Validator;
+use yii\helpers\ArrayHelper;
+
+class WhoisValidator extends Validator
+{
+    public $message="Domain validation error.";
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function validateAttribute($model, $attribute)
+    {
+        $value = $model->$attribute;
+        if (!preg_match('/^(?P<name>(?:"?([^"]*)"?\s)?)(?:\s+)?(?:(?P<open><?)((?P<local>.+)@(?P<domain>[^>]+))(?P<close>>?))$/i', $value, $matches)) {
+          $model->addError($attribute, $this->message);
+        }
+        $domain = new \overals\whois\Whois($matches['domain']);
+        if ($domain->isAvailable())
+        {
+            $model->addError($attribute, $this->message);
+        }
+    }
+
+}
