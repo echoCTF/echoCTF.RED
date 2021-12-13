079dcf774576f0ca1a8d66263cd7997c847f94fb
Pantelis Roditis <proditis@echothrust.com>
2021-12-13 14:36:37 +0200

    fixes #503

--- contrib/entrypoint-mariadb.sh
+++ contrib/entrypoint-mariadb.sh
@@ -244,7 +244,7 @@ docker_setup_db() {
 
 	if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ]; then
 		mysql_note "Creating user ${MYSQL_USER}"
-		docker_process_sql --database=mysql <<<"CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
+		docker_process_sql --database=mysql <<<"CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 
 		if [ -n "$MYSQL_DATABASE" ]; then
 			mysql_note "Giving user ${MYSQL_USER} access to schema ${MYSQL_DATABASE}"
