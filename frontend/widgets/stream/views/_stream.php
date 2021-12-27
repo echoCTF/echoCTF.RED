0339cc1537b1d307ac5c8ebdc959b1b23b6d048f
Pantelis Roditis <proditis@echothrust.com>
2021-12-28 01:57:19 +0200

    remove ts we no longer need it in the comments

--- frontend/widgets/stream/views/_stream.php
+++ frontend/widgets/stream/views/_stream.php
@@ -6,7 +6,7 @@ use app\widgets\Twitter;
     <div class="leader-wrap">
       <div class="leader-name" style="margin-bottom: 0.3em">
          <?=$model->formatted;?>, <?=$model->ts_ago?>
-         <?php $model->twitter=true; echo Twitter::widget(['message'=>'Hey check this out '.strip_tags($model->formatted)]);?><!--<?=$model->ts?>-->
+         <?php $model->twitter=true; echo Twitter::widget(['message'=>'Hey check this out '.strip_tags($model->formatted)]);?>
       </div>
     </div>
     <div class="border"></div>
