30b4a1cfb2673a3cb8ecbfc3c37fb978e4e237e5
Pantelis Roditis <proditis@echothrust.com>
2021-12-27 03:32:13 +0200

    split badges

---
+++ frontend/themes/material/profile/_badges.php
@@ -0,0 +1,13 @@
+  <?php foreach($game->badges->received_by($profile->player_id)->all() as $badge):?>
+  <div class="col col-xl-4 col-sm-12 col-md-6 col-lg-6">
+    <div class="iconic-card">
+      <center><?=$badge->pubname?></center>
+      <h3><?=$badge->name?></h3>
+      <?php if(!Yii::$app->user->isGuest && $profile->player_id===Yii::$app->user->id):?>
+        <p><?=$badge->description?></p>
+      <?php else:?>
+        <p><?=$badge->pubdescription?></p>
+      <?php endif;?>
+    </div>
+  </div>
+  <?php endforeach;?>
