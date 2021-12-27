8f2a27f8257eb4e45f1659ed6f805f4beed13ed7
Pantelis Roditis <proditis@echothrust.com>
2021-12-27 10:52:58 +0200

    split a few more views

---
+++ frontend/themes/material/profile/_challenge_solves.php
@@ -0,0 +1,26 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+?>
+<h3><code><?=count($profile->owner->challengeSolvers)?></code> Challenges solved</h2>
+<div class="row">
+  <?php foreach($profile->owner->challengeSolvers as $cs):?>
+    <div class="col col-sm-6 col-md-6 col-lg-6 col-xl-3">
+      <div class="iconic-card bg-dark">
+            <?=$cs->challenge->icon?>
+            <p><?php if($cs->first):?><img title="1st solver of the challenge" alt="1st solver of the challenge" align="left" src="/images/1sthelmet.svg" class="img-fluid" style="max-width: 30px"/><?php endif;?><b><?=Html::a(
+                    $cs->challenge->name,
+                      Url::to(['/challenge/default/view', 'id'=>$cs->challenge_id]),
+                      [
+                        'style'=>'float: bottom;',
+                        'title' => 'View challenge',
+                        'aria-label'=>'View challenge',
+                        'data-pjax' => '0',
+                      ]
+                  );?></b></p>
+                    <p><b><i class="fas fa-list-ul text-info"></i> <?=count($cs->challenge->questions)?> Questions / <?=$cs->challenge->difficulty?></b><br /><b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($cs->created_at,'long')?></b><br/>
+                    <i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($cs->timer)?></p>
+      </div>
+    </div>
+  <?php endforeach;?>
+</div>
--- frontend/themes/material/profile/index.php
+++ frontend/themes/material/profile/index.php
@@ -32,7 +32,7 @@ $profile->scenario='validator';
       </div>
     </div>
     <div class="row d-flex justify-content-center">
-      <div class="col-xl-9 d-flex justify-content-center">
+      <div class="col-lg-9 d-flex justify-content-center">
         <p class="h1">
         <?php if(trim($profile->twitter) && $profile->validate('twitter')):?><?=Html::a('<i class="fab fa-twitter text-twitter"></i>', "https://twitter.com/".Html::encode($profile->twitter), ['target'=>'_blank','title'=>"Twitter profile",'rel'=>'noopener noreferrer nofollow'])?><?php endif;?>
         <?php if(trim($profile->github) && $profile->validate('github')):?><?=Html::a('<i class="fab fa-github"></i>', "https://github.com/".Html::encode($profile->github), ['target'=>'_blank','style'=>'color: #808080;','title'=>"Github profile",'rel'=>'noopener noreferrer nofollow'])?><?php endif;?>
@@ -44,7 +44,7 @@ $profile->scenario='validator';
     </div>
 <?php endif;?>
     <div class="row d-flex justify-content-center">
-      <div class="col-xl-9">
+      <div class="col-lg-9">
         <?php \yii\widgets\Pjax::begin(['id'=>'target-listing', 'enablePushState'=>false, 'linkSelector'=>'#target-pager a', 'formSelector'=>false]);?>
         <?php echo TargetWidget::widget(['dataProvider' => null, 'player_id'=>$profile->player_id, 'profile'=>$profile, 'title'=>'Progress', 'category'=>'Pending progress of '.Html::encode($profile->owner->username).' on platform targets.', 'personal'=>true,'hidden_attributes'=>['id']]);?>
         <?php \yii\widgets\Pjax::end()?>
@@ -56,75 +56,16 @@ $profile->scenario='validator';
 <?php endif;?>
     </div>
     <?php if(count($profile->owner->challengeSolvers)>0):?>
-      <h3><code><?=count($profile->owner->challengeSolvers)?></code> Challenges solved</h2>
-      <div class="row">
-        <?php foreach($profile->owner->challengeSolvers as $cs):?>
-          <div class="col col-sm-6 col-md-6 col-lg-6 col-xl-3">
-            <div class="iconic-card">
-                  <?=$cs->challenge->icon?>
-                  <p><?php if($cs->first):?><img title="1st solver of the challenge" alt="1st solver of the challenge" align="left" src="/images/1sthelmet.svg" class="img-fluid" style="max-width: 30px"/><?php endif;?><b><?=Html::a(
-                          $cs->challenge->name,
-                            Url::to(['/challenge/default/view', 'id'=>$cs->challenge_id]),
-                            [
-                              'style'=>'float: bottom;',
-                              'title' => 'View challenge',
-                              'aria-label'=>'View challenge',
-                              'data-pjax' => '0',
-                            ]
-                        );?></b></p>
-                          <p><b><i class="fas fa-list-ul text-info"></i> <?=count($cs->challenge->questions)?> Questions / <?=$cs->challenge->difficulty?></b><br /><b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($cs->created_at,'long')?></b><br/>
-                          <i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($cs->timer)?></p>
-            </div>
-          </div>
-        <?php endforeach;?>
-      </div>
+    <?=$this->render('_challenge_solves',['profile'=>$profile]);?>
     <?php endif;?>
 
-    <?php if($profile->headshotsCount>0):?><h3><code><?=$profile->headshotsCount?></code> Headshots / <small>Average time: <?php
-      $hs=\app\modules\game\models\Headshot::find()->timed()->player_avg_time($profile->player_id)->one();
-      if($hs && $hs->average > 0)
-        echo number_format($hs->average / 60), " minutes";
-    ?> <sub>(ordered by date)</small></sub></h3>
-    <div class="row">
-      <?php foreach($profile->owner->headshots as $headshot):?>
-      <div class="col col-sm-12 col-md-6 col-lg-6 col-xl-3">
-        <div class="iconic-card">
-          <img align="right" src="<?=$headshot->target->thumbnail?>"/>
-          <p><?php if($headshot->first):?><img title="1st headshot on the target" alt="1st headshot on the target" align="left" src="/images/1sthelmet.svg" class="img-fluid" style="max-width: 30px"/><?php endif;?><b><?=Html::a(
-                       $headshot->target->name.' / '.long2ip($headshot->target->ip),
-                        Url::to(['/target/default/versus', 'id'=>$headshot->target_id, 'profile_id'=>$profile->id]),
-                        [
-                          'style'=>'float: bottom;',
-                          'title' => 'View target vs player card',
-                          'aria-label'=>'View target vs player card',
-                          'data-pjax' => '0',
-                        ]
-                    );?></b></p>
-          <p><b><i class="far fa-calendar-alt text-warning"></i> <?=\Yii::$app->formatter->asDate($headshot->created_at,'long')?></b><br/>
-          <?php if($headshot->writeup):?><b><i class="fas fa-book text-secondary"></i> Writeup submitted<?=$headshot->writeup->approved? '': ' ('.$headshot->writeup->status.')'?></b><br/><?php endif;?>
-          <i class="fas fa-stopwatch text-danger"></i> <?=\Yii::$app->formatter->asDuration($headshot->timer)?>
-          </p>
-        </div>
-      </div>
-      <?php endforeach;?>
-    </div>
+    <?php if($profile->headshotsCount>0):?>
+    <?=$this->render('_headshots',['profile'=>$profile]);?>
     <?php endif;?>
 
     <?php if($game->badges !== null && $game->badges->received_by($profile->player_id)->count() > 0):?><h3>Badges</h3>
     <div class="row game-badges">
-      <?php foreach($game->badges->received_by($profile->player_id)->all() as $badge):?>
-      <div class="col col-xl-4 col-sm-12 col-md-6 col-lg-6">
-        <div class="iconic-card">
-          <center><?=$badge->pubname?></center>
-          <h3><?=$badge->name?></h3>
-          <?php if(!Yii::$app->user->isGuest && $profile->player_id===Yii::$app->user->id):?>
-            <p><?=$badge->description?></p>
-          <?php else:?>
-            <p><?=$badge->pubdescription?></p>
-          <?php endif;?>
-        </div>
-      </div>
-      <?php endforeach;?>
+    <?=$this->render('_badges',['game'=>$game,'profile'=>$profile]);?>
     </div>
     <?php endif;?>
 
