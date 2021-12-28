5e531d7a5e102df4cd3450445497d9710cc6dee5
Pantelis Roditis <proditis@echothrust.com>
2021-12-29 00:58:49 +0200

    this is a lot better

--- frontend/themes/material/modules/target/views/default/_player_discoveries.php
+++ frontend/themes/material/modules/target/views/default/_player_discoveries.php
@@ -1,3 +1,7 @@
+<?php
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+?>
 <div class="card terminal">
   <div class="card-body">
     <pre style="font-size: 0.9em;">
@@ -6,13 +10,19 @@
     foreach(Yii::$app->user->identity->getFindings($target->id)->all() as $finding)
     {
       printf("* %s://%s:%d\n",$finding->protocol,long2ip($target->ip),$finding->port);
+      if($finding->hints!=[])
+      {
+        echo " <i class='fas fa-lightbulb text-success'></i> <code class='text-success'>", implode(', ', ArrayHelper::getColumn($finding->hints, 'title')), "</code>";
+      }
     }
-
     if(Yii::$app->user->identity->getTreasures($target->id)->count()>0) echo "\n",'# Discovered flags',"\n";
     foreach(Yii::$app->user->identity->getTreasures($target->id)->orderBy(['id' => SORT_DESC])->all() as $treasure)
     {
       printf("* (%s/%d pts) %s\n",$treasure->category,$treasure->points,$treasure->location);
-      //if(trim($treasure->solution)!=='') echo $treasure->solution,"\n";
+      if($treasure->hints!=[])
+      {
+        echo " <i class='fas fa-lightbulb text-success'></i> <code class='text-success'>", implode(', ', ArrayHelper::getColumn($treasure->hints, 'title')), "</code>";
+      }
     }
 ?></pre>
   </div>
