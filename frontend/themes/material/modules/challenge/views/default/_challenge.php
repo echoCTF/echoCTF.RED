91d2fa6bb0d441355aeda0643a24258c956a8e0a
Pantelis Roditis <proditis@echothrust.com>
2021-12-28 02:18:59 +0200

    display icon of challenge

--- frontend/themes/material/modules/challenge/views/default/_challenge.php
+++ frontend/themes/material/modules/challenge/views/default/_challenge.php
@@ -8,6 +8,7 @@ use app\widgets\Twitter;
 <div class="col-xl-4 col-sm-6 d-flex align-items-stretch">
   <div class="card">
     <div class="card-body">
+      <?=$model->icon?>
       <h4 class="card-title"><b><?=Html::a($model->name, ['view', 'id'=>$model->id])?>
       <?php if($model->completed):?><i class="fas fa-check-double text-primary"></i> <?=Twitter::widget(['message'=>'Hey check this out, I completed the challenge '.$model->name, 'url'=>Url::to(['view', 'id'=>$model->id], 'https')]);?><?php else:?><?=Twitter::widget(['message'=>'I currently grinding the challenge '.$model->name, 'url'=>Url::to(['view', 'id'=>$model->id], 'https')]);?><?php endif;?></b></h4>
       <p class="card-text"><b>Category:</b> <?=Html::encode($model->category);?><br/><b>Difficulty:</b> <?=Html::encode($model->difficulty)?></p>
