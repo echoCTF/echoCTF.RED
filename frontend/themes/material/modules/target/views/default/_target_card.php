6f9454eb6cf1bcf73f6e0559dc8d7fce4aa25caa
Pantelis Roditis <proditis@echothrust.com>
2022-01-03 22:51:50 +0200

    add player rating on target card

--- frontend/themes/material/modules/target/views/default/_target_card.php
+++ frontend/themes/material/modules/target/views/default/_target_card.php
@@ -2,18 +2,19 @@
 use app\widgets\Card;
 use app\modules\game\models\Headshot;
 
-$subtitleARR=[$target->category,ucfirst($target->difficultyText),boolval($target->rootable) ? "Rootable" : "Non rootable",$target->timer===0 ? null:'Timed'];
+$subtitleARR=[$target->category,'<abbr title="Player rating: '.ucfirst($target->getDifficultyText($target->player_rating)).'">'.ucfirst($target->difficultyText).'</abbr>',boolval($target->rootable) ? "Rootable" : "Non rootable",$target->timer===0 ? null:'Timed'];
 $subtitle=implode(", ",array_filter($subtitleARR));
 Card::begin([
             'header'=>'header-icon',
             'type'=>'card-stats',
+            'encode'=>false,
             'icon'=>sprintf('<img src="%s" class="img-fluid" style="max-width: 10rem; max-height: 4rem;" />', $target->logo),
             'color'=>'warning',
             'subtitle'=>$subtitle,
             'title'=>sprintf('%s / %s', $target->name, long2ip($target->ip)),
             'footer'=>sprintf('<div class="stats">%s</div><span>%s</span>', $target->purpose,  !Yii::$app->user->isGuest && $target->spinable ? $spinlink:""),
         ]);
-echo "<p class='text-danger'><i class='fas fa-flag'></i> ", $target->total_treasures, ": Flag".($target->total_treasures > 1 ? 's' : '')."<br/>";
+echo "<p class='text-danger'><i class='fas fa-flag'></i> ", $target->total_treasures, ": Flag".($target->total_treasures > 1 ? 's' : '')." ";
 echo  "<small>(<code class='text-danger'>";
 echo $target->treasureCategoriesFormatted;
 echo "</code>)</small><br/>";
