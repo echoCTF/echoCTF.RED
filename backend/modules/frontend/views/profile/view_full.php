e8cf8220fcffb5a618ae43e6c7842b60fdcceb4a
Pantelis Roditis <proditis@echothrust.com>
2022-01-09 01:28:48 +0200

    add ovpn view, action and quick actions menu on profile

--- backend/modules/frontend/views/profile/view_full.php
+++ backend/modules/frontend/views/profile/view_full.php
@@ -5,6 +5,7 @@ use yii\helpers\Url;
 use yii\widgets\DetailView;
 //use yii\bootstrap\Tabs;
 use kartik\tabs\TabsX;
+use yii\bootstrap\Dropdown;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\frontend\models\Profile */
@@ -31,6 +32,40 @@ $this->params['breadcrumbs'][]=$this->title;
     <?=$this->render('_heading',['model'=>$model]);?>
     <div class="row">
       <div class="col-md-4">
+        <div class="panel">
+          <div class="panel-heading">
+            <div class="dropdown">
+              <i class="fas fa-tasks"></i> <a href="#" data-toggle="dropdown" class="dropdown-toggle">Quick actions <b class="caret"></b></a>
+
+              <?php echo Dropdown::widget([
+                      'items' => [
+                          ['label' => 'Download player ovpn', 'url' => ['/frontend/player/ovpn','id'=>$model->player_id]],
+                          ['label' => 'Update profile', 'url' => ['update','id'=>$model->id]],
+                          ['label' => 'Delete profile', 'url' => ['delete','id'=>$model->id],'linkOptions'=>['data' => [
+                              'confirm' => Yii::t('app', 'Are you sure you want to delete this profile?'),
+                              'method' => 'post',
+                          ],]],
+                          ['label' => 'Update player', 'url' => ['player/update','id'=>$model->player_id]],
+                          ['label' => 'Delete player', 'url' => ['player/update','id'=>$model->player_id],'linkOptions'=>['data' => [
+                              'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                              'method' => 'post',
+                          ],]],
+                          ['label' => 'Player profile', 'url' => "//".Yii::$app->sys->offense_domain.'/profile/'.$model->id,'linkOptions'=>['target'=>'_blank']],
+                          ['label' => 'Reset auth_key', 'url' => ['player/reset-authkey','id'=>$model->player_id],'linkOptions'=>[
+                            'class' => 'text-danger',
+                            'title'=>'Reset player auth_key (force logout)',
+                            'data' => [
+                                'confirm' => Yii::t('app', 'Are you sure you want to reset the player auth_key?'),
+                                'method' => 'post',
+                            ],
+                          ]],
+
+                      ],
+                  ]);
+              ?>
+            </div>
+          </div>
+        </div>
         <?=$this->render('_player_details',['model'=>$model]);?>
         <?=$this->render('_player_memcache_details',['model'=>$model]);?>
         <?=$this->render('_player_counters_details',['model'=>$model]);?>
