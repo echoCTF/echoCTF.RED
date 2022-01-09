24b381be10ce70fe193b501a4c46a56ab91513cb
Pantelis Roditis <proditis@echothrust.com>
2022-01-09 18:28:42 +0200

    !!! IMPORTANT !!! This function is extremely dangerous as it runs eval to render the vpn template. If there is an error on the template then there will be exceptions on the frontend.

--- frontend/components/echoCTFView.php
+++ frontend/components/echoCTFView.php
@@ -114,4 +114,30 @@ class echoCTFView extends \yii\web\View
   {
     return ['name'=>'twitter:image:height', 'content'=>$this->_image_height];
   }
+
+  public function renderPhpContent($_content_, $_params_ = [])
+  {
+      $_obInitialLevel_ = ob_get_level();
+      ob_start();
+      ob_implicit_flush(false);
+      extract($_params_, EXTR_OVERWRITE);
+      try {
+          eval($_content_);
+          return ob_get_clean();
+      } catch (\Exception $e) {
+          while (ob_get_level() > $_obInitialLevel_) {
+              if (!@ob_end_clean()) {
+                  ob_clean();
+              }
+          }
+          throw $e;
+      } catch (\Throwable $e) {
+          while (ob_get_level() > $_obInitialLevel_) {
+              if (!@ob_end_clean()) {
+                  ob_clean();
+              }
+          }
+          throw $e;
+      }
+  }
 }
