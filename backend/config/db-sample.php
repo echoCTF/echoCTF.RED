92c212e84847b13e1530b63deae63884415a561c
Pantelis Roditis <proditis@echothrust.com>
2022-01-21 01:26:59 +0200

    update db-sample files

--- backend/config/db-sample.php
+++ backend/config/db-sample.php
@@ -5,7 +5,7 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=echoCTF',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     'enableSchemaCache' => true,
