ca72bad9b25c92a86d40ab42e546ee01212689ae
Pantelis Roditis <proditis@echothrust.com>
2022-04-08 14:11:46 +0300

    validate model early

--- backend/modules/infrastructure/controllers/TargetController.php
+++ backend/modules/infrastructure/controllers/TargetController.php
@@ -246,7 +246,7 @@ class TargetController extends \app\components\BaseController
         $model=$this->findModel($id);
         $modelOrig=$this->findModel($id);
         $msg="Server updated succesfully";
-        if($model->load(Yii::$app->request->post()))
+        if($model->load(Yii::$app->request->post()) && $model->validate())
         {
 
           // if the target has changed server destroy from old one
@@ -260,7 +260,7 @@ class TargetController extends \app\components\BaseController
             Yii::$app->session->setFlash('success', $msg);
             return $this->redirect(['view', 'id' => $model->id]);
           }
-          Yii::$app->session->setFlash('error', 'Server failed to be updated ['.implode(", ", $model->getErrors()).']');
+          Yii::$app->session->setFlash('error', 'Server failed to be updated ['.implode(", ", $model->errors).']');
         }
 
         return $this->render('update', [
