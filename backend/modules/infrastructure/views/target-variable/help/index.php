2339c250891f642a7bd7887cf36115a635d176a3
Pantelis Roditis <proditis@echothrust.com>
2022-04-08 19:33:51 +0300

    add help

--- backend/modules/infrastructure/views/target-variable/help/index.php
+++ backend/modules/infrastructure/views/target-variable/help/index.php
@@ -1,1 +1,10 @@
-<p>Comming soon...</p>
+Target variables represent key/value pairs that will be used as environment variables inside the target (currently supports only docker).
+
+These can be flags or configuration values used by the applications running on the target.
+
+The fields include
+* **Target**: The target that this volume belongs to
+* **Key**: The exact variable name to be used
+* **Val**: The exact value to be assigned to the variable
+
+For more information you can refer to [Docker: Set environment variables](https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file).
--- backend/modules/infrastructure/views/target-volume/help/index.php
+++ backend/modules/infrastructure/views/target-volume/help/index.php
@@ -1,1 +1,8 @@
-<p>Comming soon...</p>
+Target volumes represent files or directories from the host docker server to the guest target (vm or docker).
+
+The fields include
+* **Target**:  The target that this volume belongs to
+* **Volume**: The host server directory you want to make available to the target(docker)
+* **Bind**: The directory, within the target(docker), the volume above will be available to
+
+For more information you can refer to [Docker: Use volumes](https://docs.docker.com/storage/volumes/).
