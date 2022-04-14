7d13326b4e501cfc7a2e0c8272fea70622985676
Pantelis Roditis <proditis@echothrust.com>
2022-04-14 15:10:14 +0300

    cleanup the audit controller we are not going to use view/create/delete

--- backend/modules/infrastructure/controllers/TargetInstanceAuditController.php
+++ backend/modules/infrastructure/controllers/TargetInstanceAuditController.php
@@ -47,57 +47,6 @@ class TargetInstanceAuditController extends \app\components\BaseController
     }
 
     /**
-     * Displays a single TargetInstanceAudit model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-//    /**
-//     * Creates a new TargetInstanceAudit model.
-//     * If creation is successful, the browser will be redirected to the 'view' page.
-//     * @return mixed
-//     */
-//    public function actionCreate()
-//    {
-//        $model = new TargetInstanceAudit();
-//
-//        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-//            return $this->redirect(['view', 'id' => $model->id]);
-//        }
-//
-//        return $this->render('create', [
-//            'model' => $model,
-//        ]);
-//    }
-//
-//    /**
-//     * Updates an existing TargetInstanceAudit model.
-//     * If update is successful, the browser will be redirected to the 'view' page.
-//     * @param integer $id
-//     * @return mixed
-//     * @throws NotFoundHttpException if the model cannot be found
-//     */
-//    public function actionUpdate($id)
-//    {
-//        $model = $this->findModel($id);
-//
-//        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-//            return $this->redirect(['view', 'id' => $model->id]);
-//        }
-//
-//        return $this->render('update', [
-//            'model' => $model,
-//        ]);
-//    }
-//
-    /**
      * Truncate the existing records from TargetInstanceAudit table.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @return mixed
