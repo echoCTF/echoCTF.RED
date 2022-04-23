57139b18c64812daf66244d10c46e80fe82e0290
Pantelis Roditis <proditis@echothrust.com>
2022-04-23 15:16:17 +0300

    when a player is deleted the relation stays for historical reasons we keep the old id related to the player

--- backend/modules/frontend/views/profile/_player_relations.php
+++ backend/modules/frontend/views/profile/_player_relations.php
@@ -12,7 +12,7 @@ use app\modules\frontend\models\PlayerRelation as PR;
   <div class="panel-body pb5">
     <?php foreach(PR::find()->where(['OR',['=','player_id',$model->player_id],['=','referred_id',$model->player_id]])->all() as $ref):?>
     <?php if($ref->player_id===$model->player_id):?>
-      <span class="label label-success mr5 mb10 ib lh15"><i class="fas fa-child"></i> <?=Html::encode($ref->referred->username)?></span>
+      <span class="label label-success mr5 mb10 ib lh15"><i class="fas fa-child"></i> <?=Html::encode($ref->referred ? $ref->referred->username: "user deleted")?></span>
     <?php else:?>
       <span class="label label-warning mr5 mb10 ib lh15"><i class="fas fa-brain"></i> <?=Html::encode($ref->player->username)?></span>
     <?php endif;?>
