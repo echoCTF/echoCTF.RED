bc5fc9ef7b6746d4d4aa5e62f07108d52f369e43
Pantelis Roditis <proditis@echothrust.com>
2022-04-25 18:40:39 +0300

    fix typo

--- backend/migrations/m220425_123411_create_tad_player_disabledroute_trigger.php
+++ backend/migrations/m220425_123411_create_tad_player_disabledroute_trigger.php
@@ -14,16 +14,16 @@ class m220425_123411_create_tad_player_disabledroute_trigger extends Migration
       IF (@TRIGGER_CHECKS = FALSE) THEN
         LEAVE thisBegin;
       END IF;
-      SET routes:=(SELECT CONCAT('[',GROUP_CONCAT(JSON_OBJECT('player_id',player_id, 'route', route) ORDER BY player),']') FROM player_disabledroute ORDER BY player_id);
+      SET routes:=(SELECT CONCAT('[',GROUP_CONCAT(JSON_OBJECT('player_id',player_id, 'route', route) ORDER BY player_id,route),']') FROM player_disabledroute ORDER BY player_id);
       INSERT INTO sysconfig (id,val) VALUES ('player_disabled_routes',routes) ON DUPLICATE KEY UPDATE val=VALUES(val);
     END";
-  
+
     public function up()
     {
       $this->db->createCommand($this->DROP_SQL)->execute();
       $this->db->createCommand($this->CREATE_SQL)->execute();
     }
-  
+
     public function down()
     {
       $this->db->createCommand($this->DROP_SQL)->execute();
