d3023f21cbe6ff8541589b35a5af50242054acec
Pantelis Roditis <proditis@echothrust.com>
2022-05-06 15:20:58 +0300

    fix creation form and make TS text

--- backend/modules/activity/views/player-score-monthly/_form.php
+++ backend/modules/activity/views/player-score-monthly/_form.php
@@ -12,14 +12,19 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'player_id')->textInput() ?>
+    <?= $form->field($model, 'player_id')->widget(app\widgets\sleifer\autocompleteAjax\AutocompleteAjax::class, [
+        'multiple' => false,
+        'url' => ['/frontend/player/ajax-search'],
+        'options' => ['placeholder' => 'Find player by email, username, id or profile.']
+    ])->hint('The player that this SSL will belong to.');  ?>
 
     <?= $form->field($model, 'points')->textInput() ?>
 
     <?= $form->field($model, 'dated_at')->textInput() ?>
 
-    <?= $form->field($model, 'ts')->textInput() ?>
-
+    <?php if(!$model->isNewRecord):?>
+    <p><b>Last update:</b> <?= $model->ts?></p>
+    <?php endif;?>
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
