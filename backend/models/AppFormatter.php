1df7b509bfc1946b969cbaa9704851f18d472e07
Pantelis Roditis <proditis@echothrust.com>
2022-05-13 19:22:57 +0300

    add support to our formatter for simple codeblocks

--- backend/models/AppFormatter.php
+++ backend/models/AppFormatter.php
@@ -2,9 +2,24 @@
 namespace app\models;
 
 use Yii;
+use yii\helpers\Html;
 
 class AppFormatter extends \yii\i18n\Formatter
 {
+    /**
+     * Formats the value as an HTML-encoded <pre><code></code></pre> blocks
+     * @param string|null $value the value to be formatted.
+     * @return string the formatted result.
+     */
+    public function asCodeblock($value)
+    {
+        if ($value === null) {
+            return $this->nullDisplay;
+        }
+
+        return sprintf("<pre><code>%s</pre></code>",Html::encode($value));
+    }
+
     public function asPlayerType($value)
     {
         switch($value) {
