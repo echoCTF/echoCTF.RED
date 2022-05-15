9dc0498dd4a4738921d3440eb44ba9f6bb0827a6
Pantelis Roditis <proditis@echothrust.com>
2022-05-16 02:02:10 +0300

    fix triggers to properly update timer_avg

--- backend/migrations/m220514_230150_update_tai_headshot_trigger_add_target_state.php
+++ backend/migrations/m220514_230150_update_tai_headshot_trigger_add_target_state.php
@@ -16,7 +16,7 @@ class m220514_230150_update_tai_headshot_trigger_add_target_state extends Migrat
     IF (SELECT headshot_spin FROM target WHERE id=NEW.target_id)>0 THEN
       INSERT IGNORE INTO spin_queue (target_id, player_id,created_at) VALUES (NEW.target_id,NEW.player_id,NOW());
     END IF;
-    UPDATE target_state SET total_headshots=total_headshots+1,timer_avg=timer_avg+ifnull(NEW.timer,0) WHERE id=NEW.target_id;
+    UPDATE target_state SET total_headshots=total_headshots+1,timer_avg=(SELECT round(avg(timer)) FROM headshot WHERE target_id=NEW.target_id) WHERE id=NEW.target_id;
     END";
 
     public function up()
--- backend/migrations/m220514_230211_update_tad_headshot_trigger_add_target_state.php
+++ backend/migrations/m220514_230211_update_tad_headshot_trigger_add_target_state.php
@@ -13,7 +13,7 @@ class m220514_230211_update_tad_headshot_trigger_add_target_state extends Migrat
     IF (@TRIGGER_CHECKS = FALSE) THEN
         LEAVE thisBegin;
     END IF;
-    UPDATE target_state SET total_headshots=total_headshots-1,timer_avg=timer_avg-ifnull(OLD.timer,0) WHERE id=OLD.target_id;
+    UPDATE target_state SET total_headshots=total_headshots-1,timer_avg=(SELECT round(avg(timer)) FROM headshot WHERE target_id=OLD.target_id) WHERE id=OLD.target_id;
     END";
 
     public function up()
