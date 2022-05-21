c7da79abec8908943ac089b2fb1d089f394e2560
Pantelis Roditis <proditis@echothrust.com>
2022-05-22 02:10:47 +0300

    remove txtcrt and add txtCert method uses openssl_x509_parse on the

--- backend/modules/frontend/models/PlayerSsl.php
+++ backend/modules/frontend/models/PlayerSsl.php
@@ -15,7 +15,6 @@ use yii\db\Expression;
  * @property string $subject
  * @property string $csr
  * @property string $crt
- * @property string $txtcrt
  * @property string $privkey
  * @property string $ts
  *
@@ -48,9 +47,9 @@ class PlayerSsl extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['player_id', 'subject', 'csr', 'crt', 'txtcrt', 'privkey'], 'required'],
+            [['player_id', 'subject', 'csr', 'crt', 'privkey'], 'required'],
             [['player_id','serial'], 'integer'],
-            [['subject', 'csr', 'crt', 'txtcrt', 'privkey'], 'string'],
+            [['subject', 'csr', 'crt', 'privkey'], 'string'],
             [['ts','serial'], 'safe'],
             [['player_id','serial'], 'unique'],
             [['player_id'], 'exist', 'skipOnError' => true, 'targetClass' => Player::class, 'targetAttribute' => ['player_id' => 'id']],
@@ -68,7 +67,6 @@ class PlayerSsl extends \yii\db\ActiveRecord
             'subject' => 'Subject',
             'csr' => 'Csr',
             'crt' => 'Crt',
-            'txtcrt' => 'Txtcrt',
             'privkey' => 'Privkey',
             'ts' => 'Ts',
         ];
@@ -122,7 +120,6 @@ class PlayerSsl extends \yii\db\ActiveRecord
           $this->serial=$serial;
           $this->csr=$csrout;
           $this->crt=$crtout;
-          $this->txtcrt=$certout;
           $this->privkey=$pkeyout;
           if(!$this->isNewRecord)
             $this->touch('ts');
@@ -146,4 +143,13 @@ class PlayerSsl extends \yii\db\ActiveRecord
           return new PlayerSslQuery(get_called_class());
       }
 
+      public function getTxtCert()
+      {
+        $cert=openssl_x509_parse($this->crt);
+        $l[]="Version: ".$cert['version'];
+        $l[]=sprintf("Serial: %s (%s)",$cert['serialNumber'], $cert['serialNumberHex']);
+        $l[]=sprintf("Subject: %s",$cert['name']);
+        $l[]=sprintf("Valid: %s - %s",date_create_from_format('ymdHise', $cert['validFrom'])->format('c'),date_create_from_format('ymdHise', $cert['validTo'])->format('c'));
+        return implode("\n",$l);
+      }
 }
