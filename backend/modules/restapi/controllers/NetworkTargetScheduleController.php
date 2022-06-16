4872be7282034bb6ae312cc2b0e259f98ba4200a
Pantelis Roditis <proditis@echothrust.com>
2022-06-16 21:07:27 +0300

    Introduce our rest controller

---
+++ backend/modules/restapi/controllers/NetworkTargetScheduleController.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\modules\restapi\controllers;
+
+use yii\rest\ActiveController;
+
+class NetworkTargetScheduleController extends ActiveController
+{
+    public $modelClass='app\modules\infrastructure\models\NetworkTargetSchedule';
+}
