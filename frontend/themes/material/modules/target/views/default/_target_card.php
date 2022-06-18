f6dab522ca732b160f0752e37226958a2a112dee
Pantelis Roditis <proditis@echothrust.com>
2022-06-18 20:41:38 +0300

    sync the display of target IP on title and target card

--- frontend/themes/material/modules/target/views/default/_target_card.php
+++ frontend/themes/material/modules/target/views/default/_target_card.php
@@ -5,13 +5,19 @@ use app\widgets\Card;
 use app\widgets\targetcardactions\TargetCardActions;
 use app\modules\game\models\Headshot;
 $target_ip=long2ip($target->ip);
+$private=false;
 if($target->on_ondemand && $target->ondemand_state===-1)
   $target_ip="0.0.0.0";
 
 if(!Yii::$app->user->isGuest && Yii::$app->user->identity->instance !== NULL && Yii::$app->user->identity->instance->target_id===$target->id && Yii::$app->user->identity->instance->player_id===$identity->player_id)
 {
-  $target_ip='<b class="text-danger">'.long2ip(Yii::$app->user->identity->instance->ip).'</b>';
+  $private=true;
+  $target_ip=long2ip(Yii::$app->user->identity->instance->ip);
 }
+
+$this->title=Yii::$app->sys->event_name.' Target: '.$target->name. ' / '.$target_ip;
+
+
 $subtitleARR=[$target->category,'<abbr title="Player rating: '.ucfirst($target->getDifficultyText($target->player_rating)).'">'.ucfirst($target->difficultyText).'</abbr>',boolval($target->rootable) ? "Rootable" : "Non rootable",$target->timer===false ? null:'Timed'];
 $subtitle=implode(", ",array_filter($subtitleARR));
 Card::begin([
@@ -21,7 +27,7 @@ Card::begin([
             'icon'=>sprintf('<img src="%s" class="img-fluid" style="max-width: 10rem; max-height: 4rem;" />', $target->logo),
             'color'=>'warning',
             'subtitle'=>$subtitle,
-            'title'=>sprintf('%s / %s', $target->name, $target_ip),
+            'title'=>sprintf('%s / %s', $target->name, $private ? Html::tag('b',$target_ip,["class"=>'text-danger']): $target_ip),
             'footer'=>sprintf('<div class="stats">%s</div><span>%s</span>', $target->purpose, TargetCardActions::widget(['model'=>$target,'identity'=>$identity]) ),
         ]);
 echo "<p class='text-danger'><i class='fas fa-flag'></i> ", $target->total_treasures, ": Flag".($target->total_treasures > 1 ? 's' : '')." ";
