be5a6bda10f3ffda60a4665b40f3a21a746dc226
Pantelis Roditis <proditis@echothrust.com>
2022-10-09 15:57:58 +0300

    make notifications popup

--- frontend/widgets/NotificationsWidget.php
+++ frontend/widgets/NotificationsWidget.php
@@ -22,11 +22,14 @@ class NotificationsWidget extends Widget
       {
         $notifications=\app\models\Notification::find()->forPlayer(\Yii::$app->user->id)->forAjax()->orderBy(['created_at'=>SORT_DESC, 'id'=>SORT_DESC])->limit(5)->all();
       }
+      $view = $this->getView();
 
       foreach($notifications as $n)
       {
         if(intval($n->archived) === 0)
         {
+          $js = '$.notify({"id":"w'.$n->id.'","message":"'.$n->title.'","icon":"done"},{"timer":"4000","type":"success","offset":{"y":"40","x":"20"}});';
+          $view->registerJs($js, $view::POS_READY);
           $n->touch('updated_at');
           $n->updateAttributes(['archived' => 1,'updated_at']);
         }
