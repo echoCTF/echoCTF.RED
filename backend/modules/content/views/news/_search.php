7a0c2e8ec6628fb6813a6519987fb8c35c5ab3cf
Pantelis Roditis <proditis@echothrust.com>
2022-12-13 23:28:53 +0200

    typo

--- backend/modules/content/views/news/_search.php
+++ backend/modules/content/views/news/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\content\models\NewsSearcj */
+/* @var $model app\modules\content\models\NewsSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
