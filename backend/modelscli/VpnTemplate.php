94770a82fd882924c97db961299ac827a58c2bcd
Pantelis Roditis <proditis@users.noreply.github.com>
2022-12-15 13:38:23 +0200

    Backend ovpn dl (#760)
    * use the same methods for displaying ovpn on the backend as with front

---
+++ backend/modelscli/VpnTemplate.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace app\modelscli;
+
+use Yii;
+use yii\behaviors\AttributeTypecastBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "vpn_template".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $filename
+ * @property int $client
+ * @property int $server
+ * @property int $active
+ * @property int $visible
+ * @property string|null $description
+ * @property string|null $content
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ */
+class VpnTemplate extends \yii\db\ActiveRecord
+{
+
+    public function behaviors()
+    {
+        return [
+            'typecast' => [
+                'class' => AttributeTypecastBehavior::class,
+                'attributeTypes' => [
+                    'client' => AttributeTypecastBehavior::TYPE_BOOLEAN,
+                    'server' => AttributeTypecastBehavior::TYPE_BOOLEAN,
+                    'active' => AttributeTypecastBehavior::TYPE_BOOLEAN,
+                    'visible' => AttributeTypecastBehavior::TYPE_BOOLEAN,
+                ],
+                'typecastAfterValidate' => true,
+                'typecastBeforeSave' => true,
+                'typecastAfterFind' => true,
+            ],
+            'timestamp' => [
+                'class' => TimestampBehavior::class,
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'vpn_template';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name','filename'], 'required'],
+            [['client', 'server','active','visible'], 'integer'],
+            [['client', 'server','active','visible'], 'boolean'],
+            [['client', 'active','visible'], 'default','value'=>true],
+            [['server'], 'default','value'=>false],
+            [['description', 'content'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name','filename'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'client' => Yii::t('app', 'Client'),
+            'server' => Yii::t('app', 'Server'),
+            'description' => Yii::t('app', 'Description'),
+            'content' => Yii::t('app', 'Content'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return VpnTemplateQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new VpnTemplateQuery(get_called_class());
+    }
+}
