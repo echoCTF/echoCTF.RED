509a0141d3f14a26a4a12b571de8d94c295606a9
Pantelis Roditis <proditis@echothrust.com>
2022-12-15 13:35:51 +0200

    use the same methods for displaying ovpn on the backend as with front

--- backend/components/BaseController.php
+++ backend/components/BaseController.php
@@ -53,4 +53,29 @@ class BaseController extends \yii\web\Controller
         parent::init();
     }
 
+    public function renderPhpContent($_content_, $_params_ = [])
+    {
+        $_obInitialLevel_ = ob_get_level();
+        ob_start();
+        ob_implicit_flush(false);
+        extract($_params_, EXTR_OVERWRITE);
+        try {
+            eval($_content_);
+            return ob_get_clean();
+        } catch (\Exception $e) {
+            while (ob_get_level() > $_obInitialLevel_) {
+                if (!@ob_end_clean()) {
+                    ob_clean();
+                }
+            }
+            throw $e;
+        } catch (\Throwable $e) {
+            while (ob_get_level() > $_obInitialLevel_) {
+                if (!@ob_end_clean()) {
+                    ob_clean();
+                }
+            }
+            throw $e;
+        }
+    }
 }
