9c4857b85f0925149ef307cff2e4250912b08ff8
Pantelis Roditis <proditis@echothrust.com>
2022-12-20 00:52:40 +0200

    add announce column for network

--- backend/modules/infrastructure/views/network/view.php
+++ backend/modules/infrastructure/views/network/view.php
@@ -36,6 +36,7 @@ $this->params['breadcrumbs'][]=$this->title;
           'description:ntext',
           'public:boolean',
           'active:boolean',
+          'announce:boolean',
           'weight:integer',
           'ts',
         ],
