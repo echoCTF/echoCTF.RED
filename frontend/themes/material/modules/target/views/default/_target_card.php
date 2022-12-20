6b77bc56c728ceab269846bdbb472d2c83695239
Pantelis Roditis <proditis@echothrust.com>
2022-12-21 01:43:43 +0200

    include IP for a player with private instance

--- frontend/themes/material/modules/target/views/default/_target_card.php
+++ frontend/themes/material/modules/target/views/default/_target_card.php
@@ -19,10 +19,9 @@ if(!Yii::$app->user->isGuest && Yii::$app->user->identity->instance !== NULL && 
     $display_ip=Html::tag('b',$target_ip,["class"=>'text-danger','data-toggle'=>'tooltip','title'=>\Yii::t('app',"The IP of your private instance will become visible once its powered up.")]);
   else
     $display_ip=Html::tag('b',$target_ip,["class"=>'text-danger','data-toggle'=>'tooltip','title'=>\Yii::t('app',"The IP of your private instance.")]);
+
+  $this->title=\Yii::t('app','{event_name} Target: {target_name} / {ipaddress}',['target_name'=>$target->name,'ipaddress'=>$target_ip,'event_name'=>\Yii::$app->sys->event_name]);
 }
-
-$this->title=\Yii::t('app','{event_name} Target: {target_name} / {ipaddress}',['target_name'=>$target->name,'ipaddress'=>$target_ip,'event_name'=>\Yii::$app->sys->event_name]);
-
 
 $subtitleARR=[$target->category,ucfirst($target->getDifficultyText($target->average_rating)),boolval($target->rootable) ? "Rootable" : "Non rootable",$target->timer===false ? null:'Timed'];
 $subtitle=implode(", ",array_filter($subtitleARR));
