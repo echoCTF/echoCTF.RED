4242101c1e820f667353ff126ca6636a13332655
Pantelis Roditis <proditis@echothrust.com>
2022-12-21 01:40:05 +0200

    use the hostname if the target is not powered up

--- frontend/modules/target/models/Target.php
+++ frontend/modules/target/models/Target.php
@@ -30,6 +30,7 @@ class Target extends TargetAR
       "guru",
       "insane",
     ];
+
     public function getTreasureCategories()
     {
       $categories=[];
@@ -186,7 +187,7 @@ class Target extends TargetAR
       return self::DEFAULT_LOGO;
     }
     /**
-     * Get thumbnail image for target to be used by <img> and related tags
+     * Get the displayable IP for the target
      */
     public function getDisplayIp()
     {
@@ -202,7 +203,7 @@ class Target extends TargetAR
       }
       elseif($this->on_ondemand && $this->ondemand_state===-1)
       {
-        return \yii\helpers\Html::tag('abbr','0.0.0.0',['style'=>'padding-top: 10px; padding-bottom: 10px','data-toggle'=>'tooltip','title'=>Yii::t('app',"System currently powered down. Go to the target page to power it up.")]);
+        return \yii\helpers\Html::tag('abbr',$this->name,['style'=>'padding-top: 10px; padding-bottom: 10px','data-toggle'=>'tooltip','title'=>Yii::t('app',"System currently powered down. Go to the target page to power it up.")]);
       }
       return $this->ipoctet;
     }
