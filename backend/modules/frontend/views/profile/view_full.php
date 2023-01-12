9c383348480aabcb45526873d287cf98ff1179d6
Pantelis Roditis <proditis@echothrust.com>
2023-01-12 23:13:00 +0200

    indent and disable caching on tabs

--- backend/modules/frontend/views/profile/view_full.php
+++ backend/modules/frontend/views/profile/view_full.php
@@ -9,6 +9,7 @@ use yii\bootstrap\Dropdown;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\frontend\models\Profile */
+
 $js = <<<SCRIPT
 $(function () {
     $("[data-toggle='tooltip']").tooltip();
@@ -20,16 +21,16 @@ SCRIPT;
 // Register tooltip/popover initialization javascript
 $this->registerJs($js);
 
-$this->title="View Profile for ".Html::encode($model->owner->username)." profile: ".$model->id;
-$this->params['breadcrumbs'][]=['label' => Yii::t('app', 'Profiles'), 'url' => ['index']];
-$this->params['breadcrumbs'][]=$this->title;
+$this->title = "View Profile for " . Html::encode($model->owner->username) . " profile: " . $model->id;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Profiles'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <section class="container">
   <div class="profile-view-full">
     <!-- Begin .page-heading -->
     <p></p>
-    <?=$this->render('_heading',['model'=>$model]);?>
+    <?= $this->render('_heading', ['model' => $model]); ?>
     <div class="row">
       <div class="col-md-4">
         <div class="panel">
@@ -38,85 +39,89 @@ $this->params['breadcrumbs'][]=$this->title;
               <i class="fas fa-tasks"></i> <a href="#" data-toggle="dropdown" class="dropdown-toggle">Quick actions <b class="caret"></b></a>
 
               <?php echo Dropdown::widget([
-                      'items' => [
-                          ['label' => 'Download player ovpn', 'url' => ['/frontend/player/ovpn','id'=>$model->player_id]],
-                          ['label' => 'Update profile', 'url' => ['update','id'=>$model->id]],
-                          ['label' => 'Delete profile', 'url' => ['delete','id'=>$model->id],'linkOptions'=>['data' => [
-                              'confirm' => Yii::t('app', 'Are you sure you want to delete this profile?'),
-                              'method' => 'post',
-                          ],]],
-                          ['label' => 'View player', 'url' => ['player/view','id'=>$model->player_id]],
-                          ['label' => 'Update player', 'url' => ['player/update','id'=>$model->player_id]],
-                          ['label' => 'Delete player', 'url' => ['player/update','id'=>$model->player_id],'linkOptions'=>['data' => [
-                              'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                              'method' => 'post',
-                          ],]],
-                          ['label' => 'Player profile', 'url' => "//".Yii::$app->sys->offense_domain.'/profile/'.$model->id,'linkOptions'=>['target'=>'_blank']],
-                          ['label' => 'Reset auth_key', 'url' => ['player/reset-authkey','id'=>$model->player_id],'linkOptions'=>[
-                            'class' => 'text-danger',
-                            'title'=>'Reset player auth_key (force logout)',
-                            'data' => [
-                                'confirm' => Yii::t('app', 'Are you sure you want to reset the player auth_key?'),
-                                'method' => 'post',
-                            ],
-                          ]],
-                          ['label' => 'Activation URL', 'url' => "//".Yii::$app->sys->offense_domain.'/verify-email?token='.$model->owner->verification_token,'linkOptions'=>['target'=>'_blank'],'visible'=>$model->owner->verification_token!=null],
-                          ['label' => 'Password Reset URL', 'url' => "//".Yii::$app->sys->offense_domain.'/reset-password?token='.$model->owner->password_reset_token,'linkOptions'=>['target'=>'_blank'],'visible'=>$model->owner->password_reset_token!=null],
-                      ],
-                  ]);
+                'items' => [
+                  ['label' => 'Download player ovpn', 'url' => ['/frontend/player/ovpn', 'id' => $model->player_id]],
+                  ['label' => 'Update profile', 'url' => ['update', 'id' => $model->id]],
+                  ['label' => 'Delete profile', 'url' => ['delete', 'id' => $model->id], 'linkOptions' => ['data' => [
+                    'confirm' => Yii::t('app', 'Are you sure you want to delete this profile?'),
+                    'method' => 'post',
+                  ],]],
+                  ['label' => 'View player', 'url' => ['player/view', 'id' => $model->player_id]],
+                  ['label' => 'Update player', 'url' => ['player/update', 'id' => $model->player_id]],
+                  ['label' => 'Delete player', 'url' => ['player/update', 'id' => $model->player_id], 'linkOptions' => ['data' => [
+                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                    'method' => 'post',
+                  ],]],
+                  ['label' => 'Player profile', 'url' => "//" . Yii::$app->sys->offense_domain . '/profile/' . $model->id, 'linkOptions' => ['target' => '_blank']],
+                  ['label' => 'Reset auth_key', 'url' => ['player/reset-authkey', 'id' => $model->player_id], 'linkOptions' => [
+                    'class' => 'text-danger',
+                    'title' => 'Reset player auth_key (force logout)',
+                    'data' => [
+                      'confirm' => Yii::t('app', 'Are you sure you want to reset the player auth_key?'),
+                      'method' => 'post',
+                    ],
+                  ]],
+                  ['label' => 'Activation URL', 'url' => "//" . Yii::$app->sys->offense_domain . '/verify-email?token=' . $model->owner->verification_token, 'linkOptions' => ['target' => '_blank'], 'visible' => $model->owner->verification_token != null],
+                  ['label' => 'Password Reset URL', 'url' => "//" . Yii::$app->sys->offense_domain . '/reset-password?token=' . $model->owner->password_reset_token, 'linkOptions' => ['target' => '_blank'], 'visible' => $model->owner->password_reset_token != null],
+                ],
+              ]);
               ?>
             </div>
           </div>
         </div>
-        <?=$this->render('_player_details',['model'=>$model]);?>
-        <?=$this->render('_player_memcache_details',['model'=>$model]);?>
-        <?=$this->render('_player_counters_details',['model'=>$model]);?>
-        <?=$this->render('_player_date_details',['model'=>$model]);?>
-        <?=$this->render('_player_meta_details',['model'=>$model]);?>
-        <?=$this->render('_player_relations',['model'=>$model]);?>
-        <?=$this->render('_player_badges',['model'=>$model]);?>
+        <?= $this->render('_player_details', ['model' => $model]); ?>
+        <?= $this->render('_player_memcache_details', ['model' => $model]); ?>
+        <?= $this->render('_player_counters_details', ['model' => $model]); ?>
+        <?= $this->render('_player_date_details', ['model' => $model]); ?>
+        <?= $this->render('_player_meta_details', ['model' => $model]); ?>
+        <?= $this->render('_player_relations', ['model' => $model]); ?>
+        <?= $this->render('_player_badges', ['model' => $model]); ?>
       </div>
       <div class="col-md-8">
         <div class="tab-block">
-<?php
-echo TabsX::widget([
-    'position' => TabsX::POS_ABOVE,
-    'align' => TabsX::ALIGN_LEFT,
-    'items' => [
-        [
-          'label' => 'Activity',
-          'content'=>$this->render('_activity_tab',['model'=>$model]),
-          'headerOptions' => ['style'=>'font-weight:bold'],
-          'options' => ['id' => 'stream-tab'],
-          'active'=>true,
-        ],
-        [
-          'label' => 'Headshots',
-          'headerOptions' => ['style'=>'font-weight:bold'],
-          'linkOptions'=>['data-url'=>Url::to(['headshots', 'id' => $model->id])],
-          'options' => ['id' => 'headshots-tab'],
-        ],
-        [
-          'label' => 'Writeups',
-          'headerOptions' => ['style'=>'font-weight:bold'],
-          'linkOptions'=>['data-url'=>Url::to(['writeups', 'id' => $model->id])],
-          'options' => ['id' => 'writeups-tab'],
-        ],
-        [
-          'label' => 'Solves',
-          'headerOptions' => ['style'=>'font-weight:bold'],
-          'linkOptions'=>['data-url'=>Url::to(['solves', 'id' => $model->id])],
-          'options' => ['id' => 'solves-tab'],
-        ],
-        [
-          'label' => 'VPN History',
-          'headerOptions' => ['style'=>'font-weight:bold'],
-          'linkOptions'=>['data-url'=>Url::to(['vpn-history', 'id' => $model->id])],
-          'options' => ['id' => 'vpn-history-tab'],
-        ],
-    ],
-]);
-?>
+          <?php
+          echo TabsX::widget([
+            'position' => TabsX::POS_ABOVE,
+            'align' => TabsX::ALIGN_LEFT,
+            'pluginOptions' => [
+              'enableCache' => false,
+              'cacheTimeout' => 10000
+            ],
+            'items' => [
+              [
+                'label' => 'Activity',
+                'content' => $this->render('_activity_tab', ['model' => $model]),
+                'headerOptions' => ['style' => 'font-weight:bold'],
+                'options' => ['id' => 'stream-tab'],
+                'active' => true,
+              ],
+              [
+                'label' => 'Headshots',
+                'headerOptions' => ['style' => 'font-weight:bold'],
+                'linkOptions' => ['data-url' => Url::to(['headshots', 'id' => $model->id])],
+                'options' => ['id' => 'headshots-tab'],
+              ],
+              [
+                'label' => 'Writeups',
+                'headerOptions' => ['style' => 'font-weight:bold'],
+                'linkOptions' => ['data-url' => Url::to(['writeups', 'id' => $model->id])],
+                'options' => ['id' => 'writeups-tab'],
+              ],
+              [
+                'label' => 'Solves',
+                'headerOptions' => ['style' => 'font-weight:bold'],
+                'linkOptions' => ['data-url' => Url::to(['solves', 'id' => $model->id])],
+                'options' => ['id' => 'solves-tab'],
+              ],
+              [
+                'label' => 'VPN History',
+                'headerOptions' => ['style' => 'font-weight:bold'],
+                'linkOptions' => ['data-url' => Url::to(['vpn-history', 'id' => $model->id])],
+                'options' => ['id' => 'vpn-history-tab'],
+              ],
+            ],
+          ]);
+          ?>
         </div>
       </div>
     </div>
