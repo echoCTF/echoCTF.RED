4e5ff1fdd29cb28fc427b52ef7daeafd53721a5b
Pantelis Roditis <proditis@echothrust.com>
2023-01-18 12:35:56 +0200

    add writeup count query

--- frontend/modules/target/models/WriteupQuery.php
+++ frontend/modules/target/models/WriteupQuery.php
@@ -13,7 +13,10 @@ class WriteupQuery extends \yii\db\ActiveQuery
     {
         return $this->andWhere('approved=1');
     }
-
+    public function totals()
+    {
+        return $this->addSelect(['*','COUNT(*) AS cnt'])->approved()->groupBy(['player_id'])->orderBy(['cnt' => SORT_DESC,'player_id'=>SORT_ASC]);
+    }
     /**
      * {@inheritdoc}
      * @return Writeup[]|array
