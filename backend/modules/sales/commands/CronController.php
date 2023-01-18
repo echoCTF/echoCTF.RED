f88c498036b1be1ff4a11ed811cae1b0eca78eb8
Pantelis Roditis <proditis@echothrust.com>
2023-01-18 11:15:43 +0200

    add the product import cli command

--- backend/modules/sales/commands/CronController.php
+++ backend/modules/sales/commands/CronController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\modules\sales\commands;
 
 use yii\console\Controller;
@@ -12,57 +13,59 @@ use app\modules\sales\models\PlayerCustomerSearch as PlayerCustomer;
  */
 class CronController extends Controller
 {
-    /**
-     * Find subscriptions that have expired for more than 4 hours and cause
-     * expiration and disconnect from the VPN.
-     *
-     * This operation is supposed to be run on the VPN Server
-     */
-    public function actionExpireSubscriptions()
-    {
-      $playerSubs=PlayerSubscription::find()->active()->expired();
-      foreach($playerSubs->all() as $rec)
-      {
-        $transaction = \Yii::$app->db->beginTransaction();
-        try
-        {
-          printf("Expiring: %s => %s\n", $rec->player->email,$rec->subscription_id);
-          if($rec->product)
-          {
-            \Yii::$app->db->createCommand("DELETE FROM network_player WHERE player_id=:player_id AND network_id IN (SELECT network_id FROM product_network WHERE product_id=:product_id)")
-            ->bindValue(':player_id',$rec->player_id)
-            ->bindValue(':product_id',$rec->product->id)
+  /**
+   * Find subscriptions that have expired for more than 4 hours and cause
+   * expiration and disconnect from the VPN.
+   *
+   * This operation is supposed to be run on the VPN Server
+   */
+  public function actionExpireSubscriptions()
+  {
+    $playerSubs = PlayerSubscription::find()->active()->expired();
+    foreach ($playerSubs->all() as $rec) {
+      $transaction = \Yii::$app->db->beginTransaction();
+      try {
+        printf("Expiring: %s => %s\n", $rec->player->email, $rec->subscription_id);
+        if ($rec->product) {
+          \Yii::$app->db->createCommand("DELETE FROM network_player WHERE player_id=:player_id AND network_id IN (SELECT network_id FROM product_network WHERE product_id=:product_id)")
+            ->bindValue(':player_id', $rec->player_id)
+            ->bindValue(':product_id', $rec->product->id)
             ->execute();
-            $rec->updateAttributes(['active'=>0]);
-          }
-          else
-          {
-            \Yii::$app->db->createCommand("DELETE FROM network_player WHERE player_id=:player_id")
-            ->bindValue(':player_id',$rec->player_id)
+          $rec->updateAttributes(['active' => 0]);
+        } else {
+          \Yii::$app->db->createCommand("DELETE FROM network_player WHERE player_id=:player_id")
+            ->bindValue(':player_id', $rec->player_id)
             ->execute();
-            $rec->delete();
-          }
-          $transaction->commit();
+          $rec->delete();
         }
-        catch (\Throwable $e)
-        {
-          $transaction->rollBack();
-          echo "Rolling back: ",$e->getMessage(),"\n";
-          throw $e;
-        }
+        $transaction->commit();
+      } catch (\Throwable $e) {
+        $transaction->rollBack();
+        echo "Rolling back: ", $e->getMessage(), "\n";
+        throw $e;
       }
     }
+  }
 
-    /**
-     * Fetch stripe customers details
-     */
-    public function actionStripeImport()
-    {
-      echo "Importing Player Customers\n";
-      PlayerCustomer::FetchStripe();
-      echo "Importing Products\n";
-      Product::FetchStripe();
-      echo "Importing Player subscriptions\n";
-      PlayerSubscription::FetchStripe();
-    }
+  /**
+   * Fetch stripe customers details
+   */
+  public function actionStripeImport()
+  {
+    echo "Importing Player Customers\n";
+    PlayerCustomer::FetchStripe();
+    echo "Importing Products\n";
+    Product::FetchStripe();
+    echo "Importing Player subscriptions\n";
+    PlayerSubscription::FetchStripe();
+  }
+
+  /**
+   * Fetch stripe customers details
+   */
+  public function actionStripeImportProducts()
+  {
+    echo "Importing Products\n";
+    Product::FetchStripe();
+  }
 }
