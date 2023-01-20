018f36f4b96ef995331c1288fea708b66efa712d
Pantelis Roditis <proditis@echothrust.com>
2023-01-20 15:05:11 +0200

    add activated-writeups action and view

---
+++ backend/modules/frontend/views/profile/_activated_writeups.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\widgets\Pjax;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+?>
+<?php Pjax::begin(['id' => 'activated-writeupsPJ', 'enablePushState' => false, 'enableReplaceState' => false,]); ?>
+<?= GridView::widget([
+    'id' => 'activated-writeups',
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+
+        [
+            'attribute' => 'target_id',
+            'headerOptions' => ['style' => 'width:3vw'],
+        ],
+        [
+            'attribute' => 'target_name',
+            'value' => 'target.name',
+        ],
+        'created_at',
+    ],
+]);
+Pjax::end();
