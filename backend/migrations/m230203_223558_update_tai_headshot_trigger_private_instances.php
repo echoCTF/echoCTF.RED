4e2df7367e13fadfb8caf22fe2d8d4e518cac1a0
Pantelis Roditis <proditis@echothrust.com>
2023-02-04 01:01:23 +0200

    fix commands

--- backend/migrations/m230203_223558_update_tai_headshot_trigger_private_instances.php
+++ backend/migrations/m230203_223558_update_tai_headshot_trigger_private_instances.php
@@ -10,14 +10,14 @@ class m230203_223558_update_tai_headshot_trigger_private_instances extends Migra
     public $DROP_SQL="DROP TRIGGER IF EXISTS {{%tai_headshot}}";
     public $CREATE_SQL="CREATE TRIGGER {{%tai_headshot}} AFTER INSERT ON {{%headshot}} FOR EACH ROW
     thisBegin:BEGIN
+    DECLARE private_instance int;
     IF (@TRIGGER_CHECKS = FALSE) THEN
         LEAVE thisBegin;
     END IF;
-    DECLARE private_instance smallint;
-    SET private_instance=(SELECT count(*) FROM target_instance where player_id=NEW.player_id and target_id=NEW.target_id);
+    SET private_instance=(SELECT COUNT(*) FROM target_instance WHERE player_id=NEW.player_id AND target_id=NEW.target_id);
     IF (SELECT headshot_spin FROM target WHERE id=NEW.target_id)>0 AND private_instance<1 THEN
       INSERT IGNORE INTO spin_queue (target_id, player_id,created_at) VALUES (NEW.target_id,NEW.player_id,NOW());
-    ELSE IF private_instance>0 THEN
+    ELSEIF private_instance>0 THEN
       UPDATE target_instance SET reboot=2 WHERE player_id=NEW.player_id AND target_id=NEW.target_id;
     END IF;
     IF (SELECT count(*) FROM target_ondemand WHERE target_id=NEW.target_id AND state=1)>0 THEN
