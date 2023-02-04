5d80dabaadefda220f29b66449026f8583ffebc7
Pantelis Roditis <proditis@users.noreply.github.com>
2023-02-04 02:31:36 +0200

    Private instances dashboard (#864)
    * if there is a private instance show it

--- frontend/themes/material/dashboard/_news_item.php
+++ frontend/themes/material/dashboard/_news_item.php
@@ -1,5 +1,4 @@
 <?php
-use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
 ?>
 <div class="news-item">
---
+++ frontend/themes/material/dashboard/_target_instance_card.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use app\widgets\Card;
+
+$target = Yii::$app->user->identity->instance->target;
+$target_ip = long2ip(Yii::$app->user->identity->instance->ip);
+if (Yii::$app->user->identity->instance->ip === null)
+  $display_ip = Html::tag('b', $target_ip, ["class" => 'text-danger', 'data-toggle' => 'tooltip', 'title' => \Yii::t('app', "The IP of your private instance will become visible once its powered up.")]);
+else
+  $display_ip = Html::tag('b', $target_ip, ["class" => 'text-primary', 'data-toggle' => 'tooltip', 'title' => \Yii::t('app', "The IP of your private instance.")]);
+
+$this->title = \Yii::t('app', '{event_name} Target: {target_name} / {ipaddress}', ['target_name' => $target->name, 'ipaddress' => $target_ip, 'event_name' => \Yii::$app->sys->event_name]);
+Card::begin([
+  'header' => 'header-icon',
+  'type' => 'card-stats bg-dark',
+  'encode' => false,
+  'icon' => sprintf('<img src="%s" class="img-fluid" style="max-width: 10rem; max-height: 4rem;" />', $target->logo),
+  'color' => 'target',
+  'subtitle' => "",
+  'title' => Html::a(sprintf('%s / %s', $target->name, $display_ip), ['/target/default/view', 'id' => $target->id], ['class' => 'text-primary', 'style' => 'font-size: 0.9em;']),
+  'footer' => false,
+]);
+
+Card::end();
