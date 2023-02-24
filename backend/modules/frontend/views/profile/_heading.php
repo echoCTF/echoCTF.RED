419b1ab0c4f19f69042de64cc22f5bbb164166f7
Pantelis Roditis <proditis@echothrust.com>
2023-02-24 03:58:20 +0200

    minor improvements for tooltips

--- backend/modules/frontend/views/profile/_heading.php
+++ backend/modules/frontend/views/profile/_heading.php
@@ -18,47 +18,47 @@ use yii\helpers\Html;
           <ul class="list-inline list-unstyled breadcrumb">
           <?php if ($model->owner->stripe_customer_id):?>
             <li>
-              <?=Html::a('<span class="fab fa-stripe fs-2 text-info"></span>', "https://dashboard.stripe.com/customers/" . $model->owner->stripe_customer_id, ['target' => '_blank','title'=>'Go to stripe customer']);?>
+              <?=Html::a('<span class="fab fa-stripe fs-2 text-info"></span>', "https://dashboard.stripe.com/customers/" . $model->owner->stripe_customer_id, ['target' => '_blank','title'=>'Go to stripe customer','data-toggle'=>"tooltip", 'data-placement'=>"top" ]);?>
             </li>
           <?php endif;?>
           <?php if($model->twitter):?>
             <li>
-              <a href="https://twitter.com/<?=Html::encode($model->twitter)?>" title="<?=Html::encode($model->twitter)?>" target="_blank">
+              <a href="https://twitter.com/<?=Html::encode($model->twitter)?>"  data-toggle="tooltip" data-placement="top" title="<?=Html::encode($model->twitter)?>" target="_blank">
                 <span class="fab fa-twitter fs-2 <?=$model->validate('twitter') ? "text-info" : "text-danger" ?>"></span>
               </a>
             </li>
             <?php endif;?>
             <?php if($model->github):?>
             <li>
-              <a href="https://github.com/<?=Html::encode($model->github)?>" title="<?=Html::encode($model->github)?>">
+              <a href="https://github.com/<?=Html::encode($model->github)?>" data-toggle="tooltip" data-placement="top" title="<?=Html::encode($model->github)?>">
                 <span class="fab fa-github fs-2 <?=$model->validate('github') ? "text-info" : "text-danger" ?>"></span>
               </a>
             </li>
             <?php endif;?>
             <?php if($model->htb):?>
             <li>
-              <a href="https://hackthebox.eu/<?=Html::encode($model->htb)?>" title="<?=Html::encode($model->htb)?>">
+              <a href="https://hackthebox.eu/<?=Html::encode($model->htb)?>" data-toggle="tooltip" data-placement="top" title="<?=Html::encode($model->htb)?>">
                 <span class="fab fa-codepen fs-2 <?=$model->validate('htb') ? "text-info" : "text-danger" ?>"></span>
               </a>
             </li>
             <?php endif;?>
             <?php if($model->youtube):?>
             <li>
-              <a href="https://youtube.com/<?=Html::encode($model->youtube)?>" title="<?=Html::encode($model->youtube)?>">
+              <a href="https://youtube.com/<?=Html::encode($model->youtube)?>" data-toggle="tooltip" data-placement="top" title="<?=Html::encode($model->youtube)?>">
                 <span class="fab fa-youtube fs-2 <?=$model->validate('youtube') ? "text-info" : "text-danger" ?>"></span>
               </a>
             </li>
             <?php endif;?>
             <?php if($model->twitch):?>
             <li>
-              <a href="https://twitch.tv/<?=Html::encode($model->twitch)?>" title="<?=Html::encode($model->twitch)?>">
+              <a href="https://twitch.tv/<?=Html::encode($model->twitch)?>" data-toggle="tooltip" data-placement="top" title="<?=Html::encode($model->twitch)?>">
                 <span class="fab fa-twitch fs-2 <?=$model->validate('twitch') ? "text-info" : "text-danger" ?>"></span>
               </a>
             </li>
             <?php endif;?>
             <?php if($model->discord):?>
             <li>
-              <a href="https://discord.gg/<?=Html::encode($model->discord)?>" title="<?=Html::encode($model->discord)?>" target="_blank">
+              <a href="https://discord.gg/<?=Html::encode($model->discord)?>" data-toggle="tooltip" data-placement="top" title="<?=Html::encode($model->discord)?>" target="_blank">
                 <span class="fab fa-discord fs-2 <?=$model->validate('discord') ? "text-info" : "text-danger" ?>"></span>
               </a>
             </li>
