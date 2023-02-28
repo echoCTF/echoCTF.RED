a93be29b7a86c864c086b04a172dba80d746ef44
Pantelis Roditis <proditis@echothrust.com>
2023-03-01 00:37:30 +0200

    remove container overload

--- backend/web/index.php
+++ backend/web/index.php
@@ -9,6 +9,5 @@ require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
 
 $config=require __DIR__.'/../config/web.php';
-require(__DIR__.'/../config/container.php');
 
 (new yii\web\Application($config))->run();
