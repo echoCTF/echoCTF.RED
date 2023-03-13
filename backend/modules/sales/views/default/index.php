cdc709f9461cf3f5f9fca0dd546f140bec382f75
Pantelis Roditis <proditis@echothrust.com>
2023-03-13 14:03:38 +0200

    update sales dashboard to include statscards

--- backend/modules/sales/views/default/index.php
+++ backend/modules/sales/views/default/index.php
@@ -1,36 +1,45 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\Pjax;
-/* @var $this yii\web\View */
-/* @var $searchModel app\modules\sales\models\PlayerCustomerSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
 use app\modules\frontend\models\Player;
 use app\modules\sales\models\Product;
 use app\modules\sales\models\PlayerSubscription;
-use app\modules\sales\models\StripeWebhook as Webhook;
+use app\widgets\statscard\StatsCardModel;
+
 $this->title = Yii::t('app', 'Sales Dashboard');
 $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['index']];
 ?>
 <div class="player-customer-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
+  <div class="body-content">
     <div class="row">
-      <div class="col-lg-3">
-          <h2><?= Html::a('Customers &raquo;', ['/sales/player-customer/index']) ?> <?=Player::find()->where('stripe_customer_id is not null')->count()?></h2>
+      <div class="col-xl-3 col-lg-6">
+        <?= StatsCardModel::widget([
+          'icon' => 'fas fa-envelope-open-text',
+          'color' => 'primary',
+          'modelClass' => 'app\modules\sales\models\StripeWebhook',
+          'field' => 'ts',
+          'title' => Html::a('Webhooks', ['/sales/stripe-webhook/index'], ['title' => 'Webhooks'])
+        ]); ?>
       </div>
-      <div class="col-lg-3">
-          <h2><?= Html::a('Subscriptions &raquo;', ['/sales/player-subscription/index']) ?> <?=PlayerSubscription::find()->count()?></h2>
-          <ul>
-            <li class="h4">Active: <?=PlayerSubscription::find()->active()->count()?></li>
-          </ul>
+      <div class="col-xl-3 col-lg-6">
+        <?= StatsCardModel::widget([
+          'icon' => 'fab fa-cc-stripe',
+          'color' => 'primary',
+          'modelClass' => "app\modules\sales\models\PlayerSubscription",
+          'total' => PlayerSubscription::find()->count() . ' ' . '<small class="text-muted"><abbr title="Active">' . PlayerSubscription::find()->active()->count() . '</abbr></small>',
+          'title' => Html::a('Subscriptions', ['/sales/player-subscription/index'])
+        ]); ?>
       </div>
-      <div class="col-lg-3">
-          <h2><?= Html::a('Products &raquo;', ['/sales/product/index']) ?> <?=Product::find()->count()?></h2>
-      </div>
-      <div class="col-lg-3">
-          <h2><?= Html::a('Webhooks &raquo;', ['/sales/stripe-webhook/index']) ?> <?=Webhook::find()->count()?></h2>
+      <div class="col">
+        <div class="row">
+          <h2><?= Html::a('Customers &raquo;', ['/sales/player-customer/index']) ?> <?= Player::find()->where('stripe_customer_id is not null')->count() ?></h2>
+        </div>
+        <div class="row">
+          <h2><?= Html::a('Products &raquo;', ['/sales/product/index']) ?> <?= Product::find()->count() ?></h2>
+        </div>
       </div>
     </div>
+  </div>
 </div>
