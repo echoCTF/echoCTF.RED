0f5fde859d486d0e54369caccd002a7a2b9e7a32
Pantelis Roditis <proditis@echothrust.com>
2023-03-23 01:40:09 +0200

    update the view  include our bool column and rotate button

--- backend/modules/activity/views/notification/index.php
+++ backend/modules/activity/views/notification/index.php
@@ -7,43 +7,57 @@ use yii\grid\GridView;
 /* @var $searchModel app\modules\activity\models\NotificationSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title='Notifications';
-$this->params['breadcrumbs'][]=['label' => 'Notifications', 'url' => ['index']];
+$this->title = 'Notifications';
+$this->params['breadcrumbs'][] = ['label' => 'Notifications', 'url' => ['index']];
 yii\bootstrap5\Modal::begin([
-    'title' => '<h2><i class="bi bi-info-circle-fill"></i> '.Html::encode($this->title).' Help</h2>',
-    'toggleButton' => ['label' => '<i class="bi bi-info-circle-fill"></i> Help','class'=>'btn btn-info'],
+  'title' => '<h2><i class="bi bi-info-circle-fill"></i> ' . Html::encode($this->title) . ' Help</h2>',
+  'toggleButton' => ['label' => '<i class="bi bi-info-circle-fill"></i> Help', 'class' => 'btn btn-info'],
 ]);
-echo yii\helpers\Markdown::process($this->render('help/'.$this->context->action->id), 'gfm');
+echo yii\helpers\Markdown::process($this->render('help/' . $this->context->action->id), 'gfm');
 yii\bootstrap5\Modal::end();
 ?>
 <div class="notification-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Notification', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+  <p>
+    <?= Html::a('Create', ['create'], ['class' => 'btn btn-success', 'data-toggle' => 'tooltip', 'title' => 'Create a new notification']) ?>
+    <?= Html::a('Rotate', ['rotate'], [
+      'class' => 'btn btn-warning',
+      'data-toggle' => 'tooltip',
+      'title' => 'Rotate old notifications',
+      'data' => [
+        'confirm' => Yii::t('app', 'This clears archived and pending notifications using the default settings of 180 minutes for the archived and 3 days for pending notifications about target operations.'),
+      ],
+    ]) ?>
+  </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+  <?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'rowOptions'=>['class'=>'align-middle'],
+    'columns' => [
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
+      'id',
+      ['class' => 'app\components\columns\ProfileColumn', 'attribute' => 'player'],
+      'category',
+      'title',
+      [
+        'class' => 'app\components\columns\BooleanColumn',
+        'attribute' => 'archived',
+        'contentOptions'=>['class'=>'text-center fs-5'],
+        'filter' => ['1' => 'Archived ', '0' => 'Pending ']
+      ],
+      [
+        'attribute' => 'created_at',
+        'format'=>'raw',
+        'value'=>function($model){return Html::tag('abbr',Yii::$app->formatter->asRelativeTime($model->created_at),['title'=>$model->created_at]);}
+      ],
+      //'updated_at',
 
-            'id',
-            'player_id',
-            ['class' => 'app\components\columns\ProfileColumn','attribute'=>'player'],
-            'category',
-            'title',
-            'body:html',
-            'archived',
-            'created_at',
-            'updated_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]);?>
+      ['class' => 'yii\grid\ActionColumn'],
+    ],
+  ]); ?>
 
 
 </div>
