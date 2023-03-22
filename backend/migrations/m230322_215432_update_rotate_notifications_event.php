da26ce9d6cde2041d116cf7e3a91d9bc91efeda0
Pantelis Roditis <proditis@echothrust.com>
2023-03-23 00:19:05 +0200

    update the event to use the procedure

--- backend/migrations/m230322_215432_update_rotate_notifications_event.php
+++ backend/migrations/m230322_215432_update_rotate_notifications_event.php
@@ -11,10 +11,9 @@ class m230322_215432_update_rotate_notifications_event extends Migration
   public $CREATE_SQL = "CREATE EVENT {{%rotate_notifications}} ON SCHEDULE EVERY 12 HOUR STARTS date_format(now(),'%Y-%m-%d 00:00:01') ON COMPLETION PRESERVE ENABLE DO
     BEGIN
       ALTER EVENT {{%rotate_notifications}} DISABLE;
-      DELETE FROM `notification` WHERE
-        (`archived`=1 AND date(`updated_at`) < NOW() - INTERVAL 3 HOUR) OR
-        (created_at is null and updated_at is null) OR
-        (title like '%target%' and date(`created_at`) < now() - interval 3 day);
+      -- Rotate archived notifications older than 3 hours (180)
+      -- pending target after 3 days ((24*3)*60)
+      CALL rotate_notifications(180,(24*3)*60);
       ALTER EVENT {{%rotate_notifications}} ENABLE;
     END";
 
