30d0ba8d3ec59c7605b2ad63ee2857a6bfd6af6d
Pantelis Roditis <proditis@echothrust.com>
2023-04-28 00:03:01 +0300

    typo post_exploitation instead of post_credits

--- frontend/themes/material/modules/target/views/default/_target_metadata.php
+++ frontend/themes/material/modules/target/views/default/_target_metadata.php
@@ -5,5 +5,5 @@
   <?php if(!empty($target->metadata->pre_credits)):?><b><?=\Yii::t('app','Pre exploitation credits')?></b>: <?=$target->metadata->pre_credits?><br/><?php endif;?>
   <?php if(!empty($target->metadata->pre_exploitation)):?><b><?=\Yii::t('app','Pre exploitation details')?></b>: <?=$target->metadata->pre_exploitation?><br/><?php endif;?>
   <?php if((($identity->player_id===Yii::$app->user->id && $target->progress==100) || Yii::$app->user->identity->isAdmin) && !empty($target->metadata->post_exploitation)):?><b><?=\Yii::t('app','Post exploitation')?></b>: <?=$target->metadata->post_exploitation?><br/><?php endif;?>
-  <?php if((($identity->player_id===Yii::$app->user->id && $target->progress==100) || Yii::$app->user->identity->isAdmin) && !empty($target->metadata->post_exploitation)):?><b><?=\Yii::t('app','Post exploitation credits')?></b>: <?=$target->metadata->post_credits?><br/><?php endif;?>
+  <?php if((($identity->player_id===Yii::$app->user->id && $target->progress==100) || Yii::$app->user->identity->isAdmin) && !empty($target->metadata->post_credits)):?><b><?=\Yii::t('app','Post exploitation credits')?></b>: <?=$target->metadata->post_credits?><br/><?php endif;?>
 <?php endif;?>
