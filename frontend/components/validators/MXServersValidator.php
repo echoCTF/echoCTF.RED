9f5d326d062426e46afa6ea5c9ef10d974277000
Pantelis Roditis <proditis@echothrust.com>
2023-08-15 20:06:34 +0300

    ensure MX validator returns on error

--- frontend/components/validators/MXServersValidator.php
+++ frontend/components/validators/MXServersValidator.php
@@ -12,7 +12,7 @@ class MXServersValidator extends Validator
     public $range;
     public $banned_message="Sorry but you are using an email server that is banned!";
     public $nxmessage="Sorry but you are using a domain that does not exist!";
-
+    public $mxonly=false;
     public function init()
     {
         parent::init();
@@ -30,7 +30,7 @@ class MXServersValidator extends Validator
         $value=$matches['domain'];
       }
 
-      if(getmxrr($value, $hosts)===false)
+      if(getmxrr($value, $hosts)===false && $this->mxonly===false)
       {
         return [$this->nxmessage, [
             'domain' => $value,
@@ -49,21 +49,26 @@ class MXServersValidator extends Validator
     public function validateAttribute($model, $attribute)
     {
         $value = $model->$attribute;
+        $hosts=[];
         // if not email assume value is a domain
         if (preg_match('/^(?P<name>(?:"?([^"]*)"?\s)?)(?:\s+)?(?:(?P<open><?)((?P<local>.+)@(?P<domain>[^>]+))(?P<close>>?))$/i', $value, $matches))
         {
           $value=$matches['domain'];
         }
 
-        if(!getmxrr($value, $hosts))
+        if(getmxrr($value, $hosts)===false && $this->mxonly===false)
         {
           $model->addError($attribute, $this->nxmessage);
+          return;
         }
 
         foreach($this->range as $key)
         {
           if(array_search($key, $hosts)!==false)
+          {
             $model->addError($attribute, $this->banned_message);
+            return;
+          }
         }
     }
 }
