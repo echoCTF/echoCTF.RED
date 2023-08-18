43cb8c5b6525a83a9909e2534950504bac2e03e7
Pantelis Roditis <proditis@echothrust.com>
2023-08-18 15:47:52 +0300

    vpn container changes * make openvpn admin password a variable install a few extra packages needed for findingsd.pl * add db and cache creation every time * move dhparm first to avoid having to sleep

--- contrib/entrypoint-vpn.sh
+++ contrib/entrypoint-vpn.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+echo "<?php return [ 'class' => 'yii\db\Connection', 'dsn' => 'mysql:host=${MYSQL_HOST};dbname=${MYSQL_DATABASE}', 'username' => '${MYSQL_USER}', 'password' => '${MYSQL_PASSWORD}', 'charset' => 'utf8mb4',  ];">/var/www/echoCTF.RED/backend/config/db.php
+cp /var/www/echoCTF.RED/backend/config/cache-local.php /var/www/echoCTF.RED/backend/config/cache.php
+sed -ie "s/127.0.0.1/${MYSQL_HOST}/g" /var/www/echoCTF.RED/backend/config/cache.php
+
 if [ ! -f /etc/openvpn/.configured ]; then
-    echo "OpenVPN not configured, sleeping 30 seconds for mysql to come up"
-    sleep 30
+    echo "OpenVPN not configured"
+    openssl dhparam -out /etc/openvpn/dh.pem 2048
     mkdir -p /etc/openvpn/certs /etc/openvpn/client_confs /var/log/openvpn /etc/openvpn/crl /etc/openvpn/ccd
     install -d -m 700 /etc/openvpn/private
     cp contrib/openvpn_tun0.conf /etc/openvpn
-    echo "<?php return [ 'class' => 'yii\db\Connection', 'dsn' => 'mysql:host=${MYSQL_HOST};dbname=${MYSQL_DATABASE}', 'username' => '${MYSQL_USER}', 'password' => '${MYSQL_PASSWORD}', 'charset' => 'utf8mb4',  ];">backend/config/db.php
     sed -e "s#{{db.host}}#${MYSQL_HOST}#g" contrib/echoctf_updown_mysql.sh > /etc/openvpn/echoctf_updown_mysql.sh
     sed -e "s#ksh#bash#" -e "s#127.0.0.1#${MYSQL_HOST}#g" -e "s#{{db.user}}#${MYSQL_USER}#g" -e "s#{{db.pass}}#${MYSQL_PASSWORD}#g" -i /etc/openvpn/echoctf_updown_mysql.sh
     chmod 555 /etc/openvpn/echoctf_updown_mysql.sh
     cp contrib/crl_openssl.conf /etc/openvpn/crl/
     touch /etc/openvpn/crl/index.txt
     echo "00" > /etc/openvpn/crl/number
-    echo "OPENVPN_ADMIN_PASSWORD">/etc/openvpn/private/mgmt.pwd
-    openssl dhparam -out /etc/openvpn/dh.pem 2048
+    echo "${OPENVPN_ADMIN_PASSWORD}">/etc/openvpn/private/mgmt.pwd
     openvpn --genkey secret /etc/openvpn/private/vpn-ta.key
     /var/www/echoCTF.RED/backend/yii migrate --interactive=0
     /var/www/echoCTF.RED/backend/yii migrate-sales/up --interactive=0
