b964f658af65986c394df4556d33b4b18868329c
Pantelis Roditis <proditis@echothrust.com>
2023-08-25 23:51:50 +0300

    dont fail registration when avatar folder is not writable

--- frontend/models/forms/VerifyEmailForm.php
+++ frontend/models/forms/VerifyEmailForm.php
@@ -67,6 +67,11 @@ class VerifyEmailForm extends Model
     }
     private function genAvatar()
     {
+      if(is_writable(\Yii::getAlias('@app/web/images/avatars/'))===false || is_writable(\Yii::getAlias('@app/web/images/avatars/'.$this->_player->profile->id.'.png'))===false)
+      {
+        \Yii::error('The avatars folder or file is not writeable, correct the permissions for the avatars to be generated.');
+        return ;
+      }
       $dst_img=\Yii::getAlias('@app/web/images/avatars/'.$this->_player->profile->id.'.png');
       if(file_exists($dst_img))
         return;
