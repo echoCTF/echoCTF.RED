4423a643bced7d3a5ca59d3f21d487aff08cd597
Pantelis Roditis <proditis@echothrust.com>
2023-09-26 00:44:34 +0300

    add export of top15 teams and players

--- backend/modules/activity/controllers/TeamScoreController.php
+++ backend/modules/activity/controllers/TeamScoreController.php
@@ -105,6 +105,62 @@ class TeamScoreController extends \app\components\BaseController
     }
 
     /**
+     * Lists all TeamScore models.
+     * @return mixed
+     */
+    public function actionTop15Inclusive()
+    {
+        $searchModel=new TeamScoreSearch();
+        $csv = fopen('php://temp/maxmemory:'. (5*1024*1024), 'r+');
+        fputcsv($csv, ['Rank','Team','Full name','Username','Email','category','affiliation']);
+        for($academic=0;$academic<3;$academic++)
+        {
+            $query=TeamScore::find()->joinWith(['team'])->andFilterWhere(['team.academic' => $academic])
+                ->orderBy(['points'=>SORT_DESC, 'ts'=>SORT_ASC, 'team_id'=>SORT_ASC])
+                ->limit(15);
+            $rank=1;
+            foreach($query->all() as $ts)
+            {
+                foreach($ts->team->teamPlayers as $tp)
+                {
+                    if($tp->approved==0)
+                        continue;
+                    fputcsv($csv, [$rank,$ts->team->name,$tp->player->fullname, $tp->player->username,$tp->player->email,Yii::$app->sys->{"academic_".$tp->player->academic.'short'},$tp->player->affiliation]);
+                }
+                $rank++;
+            }
+        }
+        rewind($csv);
+        return \Yii::$app->response->sendStreamAsFile($csv,'top15-inclusive.csv');
+    }
+
+    /**
+     * Lists all TeamScore models.
+     * @return mixed
+     */
+    public function actionTop15()
+    {
+        $searchModel=new TeamScoreSearch();
+        $params=Yii::$app->request->queryParams;
+        $csv = fopen('php://temp/maxmemory:'. (5*1024*1024), 'r+');
+        for($i=0;$i<3;$i++)
+        {
+            $params['TeamScoreSearch']['team_academic']=$i;
+            $dataProvider=$searchModel->search($params);
+            $dataProvider->pagination->pageSize=15;
+            fputcsv($csv, ['---------',Yii::$app->sys->{"event_name_".$i},'---------']);
+            fputcsv($csv, ['Rank','Team','Points']);
+            foreach($dataProvider->getModels() as $k=>$line)
+            {
+                fputcsv($csv, [$k+1,$line->team->name,$line->points]);
+            }
+        }
+        rewind($csv);
+
+        return \Yii::$app->response->sendStreamAsFile($csv,'top15.csv');
+    }
+
+    /**
      * Finds the TeamScore model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
