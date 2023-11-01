95b3d01521d82b3212ad9518836c5e1b7b6e3b0f
Pantelis Roditis <proditis@echothrust.com>
2023-11-01 13:45:48 +0200

    fix logic bug from team_stream by cleaning it last we created empty team score records since teams were deleted already

---
+++ backend/migrations/m231101_114257_update_tad_team_trigger_fix_order.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231101_114257_update_tad_team_trigger_fix_order
+ */
+class m231101_114257_update_tad_team_trigger_fix_order extends Migration
+{
+    public $DROP_SQL="DROP TRIGGER IF EXISTS {{%tad_team}}";
+    public $CREATE_SQL="CREATE TRIGGER {{%tad_team}} AFTER DELETE ON {{%team}} FOR EACH ROW
+    thisBegin:BEGIN
+      IF (@TRIGGER_CHECKS = FALSE) THEN
+        LEAVE thisBegin;
+      END IF;
+      DELETE FROM {{%team_stream}} WHERE team_id=OLD.id;
+      DELETE FROM {{%team_rank}} WHERE team_id=OLD.id;
+      DELETE FROM {{%team_score}} WHERE team_id=OLD.id;
+    END";
+
+    public function up()
+    {
+      $this->db->createCommand($this->DROP_SQL)->execute();
+      $this->db->createCommand($this->CREATE_SQL)->execute();
+    }
+
+    public function down()
+    {
+      $this->db->createCommand($this->DROP_SQL)->execute();
+    }
+}
