bfc52e1ceef2019a0f04518a40f118600a3b5fd6
Pantelis Roditis <proditis@echothrust.com>
2023-11-02 12:19:07 +0200

    add player_id to audit

--- backend/migrations/m231102_081144_create_team_audit_table.php
+++ backend/migrations/m231102_081144_create_team_audit_table.php
@@ -14,12 +14,16 @@ class m231102_081144_create_team_audit_table extends Migration
     {
         $this->createTable('{{%team_audit}}', [
             'id' => $this->primaryKey(),
-            'team_id' => $this->integer(),
-            'action' => $this->string(20),
+            'team_id' => $this->integer()->notNull(),
+            'player_id' => $this->integer(),
+            'action' => $this->string(20)->notNull()->defaultValue('default'),
             'message' => $this->text(),
             'ts' => $this->timestamp(),
         ]);
         $this->createIndex('team_id_idx','{{%team_audit}}','team_id');
+        $this->createIndex('player_id_idx','{{%team_audit}}','player_id');
+        $this->createIndex('action_idx','{{%team_audit}}','action');
+        $this->createIndex('ts_idx','{{%team_audit}}','ts');
     }
 
     /**
