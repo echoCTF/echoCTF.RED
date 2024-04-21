6e41f85e6b186c6c8b82d35441ffb7dc3dfccc91
Pantelis Roditis <proditis@echothrust.com>
2024-04-21 10:19:10 +0300

    Update static pages help

--- backend/modules/content/views/pages/help/index.php
+++ backend/modules/content/views/pages/help/index.php
@@ -1,1 +1,11 @@
-<p>Comming soon...</p>
+Manage static pages to be displayed on the frontend.
+
+The listing displays the following fields:
+* `ID`: The unique id of the page
+* `Title`: The title of the page
+* `Slug`: A unique URL slug used at <a href="/index.php?r=content/pages/index" target="_blank">URL Routes</a>
+* `Created at`/`Updated at`: The creation date and the date it was last updated
+
+**NOTE**: In order for the pages to be visible into the frontend you either have to create a custom page and `findOne()` or create a URL rule for the slug to point to `static-page/view` such as
+* **source** => **`<slug:myurl>`**
+* **destination** => **`static-page/view`**
