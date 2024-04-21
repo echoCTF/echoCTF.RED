eda6cf7012ca7c01b204fa92328d68358365385c
Pantelis Roditis <proditis@echothrust.com>
2024-04-21 10:17:08 +0300

    update help for email-templates

--- backend/modules/content/views/email-template/help/index.php
+++ backend/modules/content/views/email-template/help/index.php
@@ -1,1 +1,7 @@
-<p>Comming soon...</p>
+Manage email templates that can be used by the platform for registration and password reset mails.
+
+The displayed fields include:
+* `ID`: The unique id of the template
+* `Name`: The internal name that the platform uses to find this mail template
+* `Title`: The title for the template
+* `Created at`/`Updated at`: The creation and last modification dates of the template
--- backend/modules/content/views/email-template/help/update.php
+++ backend/modules/content/views/email-template/help/update.php
@@ -1,1 +1,9 @@
-<p>Comming soon...</p>
+Update an existing email template that can be used by the platform or your custom modules and scripts.
+
+The form fields include:
+* `Name`: The internal name that the platform uses to find this mail template
+* `Title`: The email subject link
+* `Html`: The HTML body of the email (supports PHP)
+* `Txt`: The plain text body of the email (supports PHP)
+
+The HTML and TXT version have the current player object assigned as the variable `$user`.
