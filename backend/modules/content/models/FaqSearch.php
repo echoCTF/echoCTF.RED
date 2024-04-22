63e0c175b3619bd0c43b3714db028b8bd687cae3
Pantelis Roditis <proditis@echothrust.com>
2024-04-22 11:12:04 +0300

    add default order by weight

--- backend/modules/content/models/FaqSearch.php
+++ backend/modules/content/models/FaqSearch.php
@@ -11,61 +11,62 @@ use app\modules\content\models\Faq;
  */
 class FaqSearch extends Faq
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['id', 'weight'], 'integer'],
-            [['title', 'body'], 'safe'],
-        ];
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['id', 'weight'], 'integer'],
+      [['title', 'body'], 'safe'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function scenarios()
+  {
+    // bypass scenarios() implementation in the parent class
+    return Model::scenarios();
+  }
+
+  /**
+   * Creates data provider instance with search query applied
+   *
+   * @param array $params
+   *
+   * @return ActiveDataProvider
+   */
+  public function search($params)
+  {
+    $query = Faq::find();
+
+    // add conditions that should always apply here
+
+    $dataProvider = new ActiveDataProvider([
+      'query' => $query,
+    ]);
+
+    $this->load($params);
+
+    if (!$this->validate()) {
+      // uncomment the following line if you do not want to return any records when validation fails
+      // $query->where('0=1');
+      return $dataProvider;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+    // grid filtering conditions
+    $query->andFilterWhere([
+      'id' => $this->id,
+      'weight' => $this->weight,
+    ]);
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query=Faq::find();
+    $query->andFilterWhere(['like', 'title', $this->title])
+      ->andFilterWhere(['like', 'body', $this->body]);
 
-        // add conditions that should always apply here
+    $dataProvider->setSort(['defaultOrder' => ['weight' => SORT_ASC]]);
 
-        $dataProvider=new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params);
-
-        if(!$this->validate())
-        {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'weight' => $this->weight,
-        ]);
-
-        $query->andFilterWhere(['like', 'title', $this->title])
-            ->andFilterWhere(['like', 'body', $this->body]);
-
-        return $dataProvider;
-    }
+    return $dataProvider;
+  }
 }
--- backend/modules/content/models/InstructionSearch.php
+++ backend/modules/content/models/InstructionSearch.php
@@ -11,63 +11,64 @@ use app\modules\content\models\Instruction;
  */
 class InstructionSearch extends Instruction
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['id', 'weight'], 'integer'],
-            [['title', 'player_type', 'message', 'ts'], 'safe'],
-        ];
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['id', 'weight'], 'integer'],
+      [['title', 'player_type', 'message', 'ts'], 'safe'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function scenarios()
+  {
+    // bypass scenarios() implementation in the parent class
+    return Model::scenarios();
+  }
+
+  /**
+   * Creates data provider instance with search query applied
+   *
+   * @param array $params
+   *
+   * @return ActiveDataProvider
+   */
+  public function search($params)
+  {
+    $query = Instruction::find();
+
+    // add conditions that should always apply here
+
+    $dataProvider = new ActiveDataProvider([
+      'query' => $query,
+    ]);
+
+    $this->load($params);
+
+    if (!$this->validate()) {
+      // uncomment the following line if you do not want to return any records when validation fails
+      // $query->where('0=1');
+      return $dataProvider;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+    // grid filtering conditions
+    $query->andFilterWhere([
+      'id' => $this->id,
+      'weight' => $this->weight,
+      'ts' => $this->ts,
+    ]);
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query=Instruction::find();
+    $query->andFilterWhere(['like', 'title', $this->title])
+      ->andFilterWhere(['like', 'player_type', $this->player_type])
+      ->andFilterWhere(['like', 'message', $this->message]);
 
-        // add conditions that should always apply here
+    $dataProvider->setSort(['defaultOrder' => ['weight' => SORT_ASC]]);
 
-        $dataProvider=new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params);
-
-        if(!$this->validate())
-        {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'weight' => $this->weight,
-            'ts' => $this->ts,
-        ]);
-
-        $query->andFilterWhere(['like', 'title', $this->title])
-            ->andFilterWhere(['like', 'player_type', $this->player_type])
-            ->andFilterWhere(['like', 'message', $this->message]);
-
-        return $dataProvider;
-    }
+    return $dataProvider;
+  }
 }
