aff9202b8cca309368381006a69521ec8699769f
Pantelis Roditis <proditis@echothrust.com>
2024-05-04 15:47:52 +0300

    make the sending of the email operation unified

--- backend/modules/frontend/actions/player/MailAction.php
+++ backend/modules/frontend/actions/player/MailAction.php
@@ -29,19 +29,19 @@ class MailAction extends \yii\base\Action
     }
     elseif ($player->status == 9)
     {
-      if(\Yii::$app->sys->players_require_approval === true && $player->approval>0 && $player->approval<=2)
+      if (\Yii::$app->sys->players_require_approval === true && $player->approval > 0 && $player->approval <= 2)
         $this->approvalMail($player);
-      elseif(\Yii::$app->sys->players_require_approval === true && $player->approval>2 && $player->approval<=4)
+      elseif (\Yii::$app->sys->players_require_approval === true && $player->approval > 2 && $player->approval <= 4)
         $this->rejectionMail($player);
     }
     elseif ($player->status == 0)
     {
-        $this->rejectionMail($player);
+      $this->rejectionMail($player);
     }
 
-    if($player->approval==1)
+    if ($player->approval == 1)
       $player->updateAttributes(['approval' => 2]);
-    elseif($player->approval==3)
+    elseif ($player->approval == 3)
       $player->updateAttributes(['approval' => 4]);
 
     return $this->controller->goBack(Yii::$app->request->referrer);
@@ -53,16 +53,12 @@ class MailAction extends \yii\base\Action
   private function rejectionMail($player)
   {
     try {
-      Yii::$app
-        ->mailer
-        ->compose(
-          ['html' => 'rejectVerify-html', 'text' => 'rejectVerify-text'],
-          ['user' => $player]
-        )
-        ->setFrom([Yii::$app->sys->mail_from => Yii::$app->sys->mail_fromName . ' robot'])
-        ->setTo([$player->email => $player->fullname])
-        ->setSubject(Yii::t('app', '{event_name} Account rejected', ['event_name' => trim(Yii::$app->sys->event_name)]))
-        ->send();
+      $this->sendMail(
+        $player,
+        'rejectVerify-html',
+        'rejectVerify-text',
+        Yii::t('app', '{event_name} Account rejected', ['event_name' => trim(Yii::$app->sys->event_name)])
+      );
       \Yii::$app->getSession()->setFlash('success', Yii::t('app', 'Player rejection mail send.'));
     } catch (\Exception $e) {
       \Yii::$app->getSession()->setFlash('error', Yii::t('app', 'Failed to mail rejection to player. {exception}', ['exception' => Html::encode($e->getMessage())]));
@@ -76,16 +72,12 @@ class MailAction extends \yii\base\Action
   {
     try {
       $activationURL = sprintf("https://%s/verify-email?token=%s", \Yii::$app->sys->offense_domain, $player->verification_token);
-      Yii::$app
-        ->mailer
-        ->compose(
-          ['html' => 'emailVerify-html', 'text' => 'emailVerify-text'],
-          ['user' => $player, 'verifyLink' => $activationURL]
-        )
-        ->setFrom([Yii::$app->sys->mail_from => Yii::$app->sys->mail_fromName . ' robot'])
-        ->setTo([$player->email => $player->fullname])
-        ->setSubject(Yii::t('app', '{event_name} Account approved', ['event_name' => trim(Yii::$app->sys->event_name)]))
-        ->send();
+      $this->sendMail(
+        $player,
+        'emailVerify-html',
+        'emailVerify-text',
+        Yii::t('app', '{event_name} Account approved', ['event_name' => trim(Yii::$app->sys->event_name)])
+      );
       if (Yii::$app->sys->players_require_approval === true && $player->approval == 1) {
         $player->updateAttributes(['approval' => 2]);
         \Yii::$app->getSession()->setFlash('success', Yii::t('app', 'Player activation mail send and approval status updated.'));
@@ -95,5 +87,19 @@ class MailAction extends \yii\base\Action
     } catch (\Exception $e) {
       \Yii::$app->getSession()->setFlash('error', Yii::t('app', 'Failed to mail player. {exception}', ['exception' => Html::encode($e->getMessage())]));
     }
+  }
+
+  private function sendMail($player, $html, $text, $subject)
+  {
+    Yii::$app
+      ->mailer
+      ->compose(
+        ['html' => $html, 'text' => $text],
+        ['user' => $player]
+      )
+      ->setFrom([Yii::$app->sys->mail_from => Yii::$app->sys->mail_fromName . ' robot'])
+      ->setTo([$player->email => $player->fullname])
+      ->setSubject($subject)
+      ->send();
   }
 }
