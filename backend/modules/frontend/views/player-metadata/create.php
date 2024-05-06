6e5873c1a11826971fb98e512bf1e2e1660ed2e9
Pantelis Roditis <proditis@echothrust.com>
2024-05-06 14:06:40 +0300

    add help sections

--- backend/modules/frontend/views/player-metadata/create.php
+++ backend/modules/frontend/views/player-metadata/create.php
@@ -8,6 +8,13 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Create Player Metadata');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Player Metadatas'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+yii\bootstrap5\Modal::begin([
+  'title' => '<h2><i class="bi bi-info-circle-fill"></i> '.Html::encode($this->title).' Help</h2>',
+  'toggleButton' => ['label' => '<i class="bi bi-info-circle-fill"></i> Help', 'class' => 'btn btn-info'],
+'options'=>['class'=>'modal-lg']
+]);
+echo yii\helpers\Markdown::process($this->render('help/'.$this->context->action->id), 'gfm');
+yii\bootstrap5\Modal::end();
 ?>
 <div class="player-metadata-create">
 
--- backend/modules/frontend/views/player-metadata/update.php
+++ backend/modules/frontend/views/player-metadata/update.php
@@ -11,6 +11,14 @@ $this->title = Yii::t('app', 'Update Player Metadata: {name}', [
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Player Metadatas'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->player_id, 'url' => ['view', 'player_id' => $model->player_id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+yii\bootstrap5\Modal::begin([
+  'title' => '<h2><i class="bi bi-info-circle-fill"></i> '.Html::encode($this->title).' Help</h2>',
+  'toggleButton' => ['label' => '<i class="bi bi-info-circle-fill"></i> Help', 'class' => 'btn btn-info'],
+'options'=>['class'=>'modal-lg']
+]);
+echo yii\helpers\Markdown::process($this->render('help/'.$this->context->action->id), 'gfm');
+yii\bootstrap5\Modal::end();
+
 ?>
 <div class="player-metadata-update">
 
