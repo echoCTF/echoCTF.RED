d7594daeda8ecc9521742d3609163564cf80ce69
Pantelis Roditis <proditis@echothrust.com>
2024-05-06 21:06:47 +0300

    simplify mailer and use DSN instead of the complex configuration we had.
    Transparently support both though

--- frontend/components/Mailer.php
+++ frontend/components/Mailer.php
@@ -6,43 +6,20 @@ class Mailer extends \yii\symfonymailer\Mailer
   public function init()
   {
     parent::init();
-
-    $this->useFileTransport=Yii::$app->sys->mail_useFileTransport;
-
-    $config['scheme']='smtp';
-    $config['options']['local_domain']=\Yii::$app->sys->offense_domain;
-    if(Yii::$app->sys->mail_host !== false)
-    {
-      $config['host']=Yii::$app->sys->mail_host;
-    }
-
-    if(Yii::$app->sys->mail_port !== false)
-    {
-      $config['port']=intval(Yii::$app->sys->mail_port);
-    }
-
-    if(Yii::$app->sys->mail_username !== false)
-    {
-      $config['username']=Yii::$app->sys->mail_username;
-    }
-
-    if(Yii::$app->sys->mail_password !== false)
-    {
-      $config['password']=Yii::$app->sys->mail_password;
-    }
-
-    if(Yii::$app->sys->mail_encryption !== false && trim(Yii::$app->sys->mail_encryption)!='')
-    {
-      $config['scheme']=Yii::$app->sys->mail_encryption;
-    }
-
-    if(Yii::$app->sys->mail_verify_peer !== false)
-    {
-      $this->ssl['verify_peer']=Yii::$app->sys->mail_verify_peer;
-    }
-    if(Yii::$app->sys->mail_verify_peer_name !== false)
-    {
-      $this->ssl['verify_peer_name']=Yii::$app->sys->mail_verify_peer_name;
+    $this->useFileTransport = (bool)Yii::$app->sys->mail_useFileTransport;
+    if (\Yii::$app->sys->dsn !== false) {
+      $config['dsn']=trim(\Yii::$app->sys->dsn);
+    } else {
+      $config['dsn'] = Yii::t('app', '{mail_encryption}://{mail_userpass}{mail_host}:{mail_port}?verify_peer={verify_peer}&local_domain={local_domain}&verify_peer_name={verify_peer_name}', [
+        'mail_encryption' => \Yii::$app->sys->mail_encryption ?? "smtp",
+        'mail_userpass' => (trim(\Yii::$app->sys->mail_username) != "" && trim(\Yii::$app->sys->mail_password) != "") ? trim(\Yii::$app->sys->mail_username) . ':' . trim(\Yii::$app->sys->mail_password) . '@' : '',
+        'mail_password' => \Yii::$app->sys->mail_password,
+        'mail_host' => \Yii::$app->sys->mail_host,
+        'mail_port' => \Yii::$app->sys->mail_port ?? "25",
+        'verify_peer' => intval(\Yii::$app->sys->verify_peer),
+        'local_domain' => \Yii::$app->sys->offense_domain,
+        'verify_peer_name' => intval(\Yii::$app->sys->verify_peer_name),
+      ]);
     }
     $this->setTransport($config);
   }
