b2b9589a0836f9087b017776d998401205cf045c
Pantelis Roditis <proditis@echothrust.com>
2024-05-07 07:42:31 +0300

    change heading to include twitter image

--- backend/modules/infrastructure/views/target/full-view/_heading.php
+++ backend/modules/infrastructure/views/target/full-view/_heading.php
@@ -4,15 +4,15 @@ use yii\helpers\Html;
 ?>
 <div class="page-heading">
   <div class="media-left pr30">
-    <div class="row">
-      <div class="col-lg-1">
+    <div class="row d-flex h-100">
+      <div class="col-lg-4">
         <a href="//<?= Yii::$app->sys->offense_domain ?>/target/<?= $model->id ?>" target="_blank">
-          <img width="100px" class="img-fluid img-thumbnail" src="//<?= Yii::$app->sys->offense_domain ?>/images/targets/_<?= $model->name ?>-thumbnail.png" alt="<?= Yii::$app->sys->offense_domain ?>/images/targets/_<?= $model->name ?>-thumbnail.png">
+          <img width="100%" class="img-fluid img-thumbnail" src="//<?= Yii::$app->sys->offense_domain ?>/images/targets/<?= $model->name ?>.png" alt="<?= Yii::$app->sys->offense_domain ?>/images/targets/<?= $model->name ?>.png">
         </a>
       </div>
-      <div class="col">
-      <h2 class="media-heading"><?= Html::encode($model->name) ?> - <span class="text-muted h5"><?=Html::encode($model->purpose)?></span></h2>
-      <?= $model->description ?>
+      <div class="col justify-content-center align-self-center">
+        <h2 class="media-heading"><?= Html::encode($model->name) ?> - <span class="text-muted h5"><?= Html::encode($model->purpose) ?></span></h2>
+        <?= $model->description ?>
       </div>
     </div>
   </div>
