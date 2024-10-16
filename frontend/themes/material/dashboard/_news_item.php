6758b0de39ad687cab61aac4007072864be85d06
Pantelis Roditis <proditis@echothrust.com>
2024-10-17 00:41:42 +0300

    dont show time ago its getting mixed up

--- frontend/themes/material/dashboard/_news_item.php
+++ frontend/themes/material/dashboard/_news_item.php
@@ -3,5 +3,5 @@ use yii\helpers\HtmlPurifier;
 ?>
 <div class="news-item">
     <h4 class="text-warning"><?=$model->category;?> <?= HtmlPurifier::process($model->title) ?></h4>
-    <p><?= HtmlPurifier::process($model->body) ?> <small><?=\Yii::$app->formatter->asRelativeTime($model->created_at)?></small></p>
+    <p><?= HtmlPurifier::process($model->body) ?></p>
 </div>
