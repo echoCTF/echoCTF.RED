0f2ae78777aacc511246bf8f10b91632acfe39e4
Pantelis Roditis <proditis@echothrust.com>
2024-10-16 22:13:20 +0300

    indent and make it so that we dont error out when duplicate record is attempted to be added fixes #1250

--- backend/modules/infrastructure/controllers/TargetOndemandController.php
+++ backend/modules/infrastructure/controllers/TargetOndemandController.php
@@ -6,8 +6,8 @@ use Yii;
 use app\modules\gameplay\models\TargetOndemand;
 use app\modules\gameplay\models\TargetOndemandSearch;
 use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 
 /**
  * TargetOndemandController implements the CRUD actions for TargetOndemand model.
@@ -17,104 +17,107 @@ class TargetOndemandController extends \app\components\BaseController
   /**
    * {@inheritdoc}
    */
-    public function behaviors()
-    {
-      return ArrayHelper::merge(parent::behaviors(),[]);
+  public function behaviors()
+  {
+    return ArrayHelper::merge(parent::behaviors(), []);
+  }
+
+  /**
+   * Lists all TargetOndemand models.
+   * @return mixed
+   */
+  public function actionIndex()
+  {
+    $searchModel = new TargetOndemandSearch();
+    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+    return $this->render('index', [
+      'searchModel' => $searchModel,
+      'dataProvider' => $dataProvider,
+    ]);
+  }
+
+  /**
+   * Displays a single TargetOndemand model.
+   * @param integer $id
+   * @return mixed
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  public function actionView($id)
+  {
+    return $this->render('view', [
+      'model' => $this->findModel($id),
+    ]);
+  }
+
+  /**
+   * Creates a new TargetOndemand model.
+   * If creation is successful, the browser will be redirected to the 'view' page.
+   * @return mixed
+   */
+  public function actionCreate()
+  {
+    $model = new TargetOndemand();
+    try {
+      if ($model->load(Yii::$app->request->post()) && $model->save()) {
+        return $this->redirect(['view', 'id' => $model->target_id]);
+      }
+    } catch (\Exception $e) {
+      Yii::$app->session->setFlash('error', Yii::t('app', "Failed to add OnDemand entry for target. <b>{exception}</b>", ['exception' => Html::encode($e->getMessage())]));
     }
 
-    /**
-     * Lists all TargetOndemand models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new TargetOndemandSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+    return $this->render('create', [
+      'model' => $model,
+    ]);
+  }
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+  /**
+   * Updates an existing TargetOndemand model.
+   * If update is successful, the browser will be redirected to the 'view' page.
+   * @param integer $id
+   * @return mixed
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  public function actionUpdate($id)
+  {
+    $model = $this->findModel($id);
+
+    if ($model->load(Yii::$app->request->post()) && $model->save()) {
+      return $this->redirect(['view', 'id' => $model->target_id]);
     }
 
-    /**
-     * Displays a single TargetOndemand model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
+    return $this->render('update', [
+      'model' => $model,
+    ]);
+  }
+
+  /**
+   * Deletes an existing TargetOndemand model.
+   * If deletion is successful, the browser will be redirected to the 'index' page.
+   * @param integer $id
+   * @return mixed
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  public function actionDelete($id)
+  {
+    $this->findModel($id)->delete();
+
+    return $this->redirect(['index']);
+  }
+
+  /**
+   * Finds the TargetOndemand model based on its primary key value.
+   * If the model is not found, a 404 HTTP exception will be thrown.
+   * @param integer $id
+   * @return TargetOndemand the loaded model
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  protected function findModel($id)
+  {
+    if (($model = TargetOndemand::findOne($id)) !== null) {
+      return $model;
     }
 
-    /**
-     * Creates a new TargetOndemand model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new TargetOndemand();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->target_id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing TargetOndemand model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->target_id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing TargetOndemand model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the TargetOndemand model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return TargetOndemand the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = TargetOndemand::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
-    }
+    throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+  }
 }
