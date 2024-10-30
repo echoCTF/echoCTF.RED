7047e3ba934b4ead80055e8cd1e35096fd5e24d2
Pantelis Roditis <proditis@echothrust.com>
2024-10-30 22:52:54 +0200

    move IP's into sysconfig so that we can manage it easier

--- frontend/modules/subscription/controllers/DefaultController.php
+++ frontend/modules/subscription/controllers/DefaultController.php
@@ -51,22 +51,7 @@ class DefaultController extends \app\components\BaseController
                       'allow'=>true,
                       'roles'=>['?'],
                       'verbs'=>['post'],
-                      'ips' => [
-                        //https://stripe.com/docs/ips
-                        '3.18.12.63',
-                        '3.130.192.231',
-                        '13.235.14.237',
-                        '13.235.122.149',
-                        '18.211.135.69',
-                        '35.154.171.200',
-                        '52.15.183.38',
-                        '54.88.130.119',
-                        '54.88.130.237',
-                        '54.187.174.169',
-                        '54.187.205.235',
-                        '54.187.216.72',
-                        '127.0.0.1',
-                      ],
+                      'ips' => explode(",",Yii::$app->sys->stripe_webhook_ips), // https://stripe.com/docs/ips
                     ]
                 ],
             ],
