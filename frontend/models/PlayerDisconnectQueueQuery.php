750455995579fde7e0109076f58f2affe48538c0
Pantelis Roditis <proditis@echothrust.com>
2024-11-03 01:28:25 +0200

    add relation to disconnect queue

---
+++ frontend/models/PlayerDisconnectQueueQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the ActiveQuery class for [[PlayerDisconnectQueue]].
+ *
+ * @see PlayerDisconnectQueue
+ */
+class PlayerDisconnectQueueQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * {@inheritdoc}
+     * @return PlayerDisconnectQueue[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return PlayerDisconnectQueue|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
