9b2a57bf7ac01d76bbf66d3b35ffcb0763389d0a
Pantelis Roditis <proditis@echothrust.com>
2024-11-05 01:47:38 +0200

    populate player_token and drop columns from player

--- backend/migrations/m241104_201004_drop_token_columns_from_player_table.php
+++ backend/migrations/m241104_201004_drop_token_columns_from_player_table.php
@@ -12,6 +12,8 @@ class m241104_201004_drop_token_columns_from_player_table extends Migration
      */
     public function safeUp()
     {
+      $this->db->createCommand("INSERT INTO player_token (player_id,type,token,expires_at,created_at) SELECT id,'password_reset',substr(password_reset_token,1,30),now()+ INTERVAL 24 HOUR,now() FROM player WHERE password_reset_token is not null and password_reset_token!=''")->execute();
+      $this->db->createCommand("INSERT INTO player_token (player_id,type,token,expires_at,created_at) SELECT id,'email_verification',substr(verification_token,1,30),now()+ INTERVAL 24 HOUR,now() FROM player WHERE verification_token is not null and verification_token!=''")->execute();
       $this->dropColumn('player', 'password_reset_token');
       $this->dropColumn('player', 'verification_token');
     }
