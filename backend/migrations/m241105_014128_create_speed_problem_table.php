9534aed1a1d43dcbae09a9d9bd37fc97a60d857a
Pantelis Roditis <proditis@echothrust.com>
2024-11-05 04:25:08 +0200

    add migrations for initial tables

---
+++ backend/migrations/m241105_014128_create_speed_problem_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%speed_problem}}`.
+ */
+class m241105_014128_create_speed_problem_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+      $this->createTable('{{%speed_problem}}', [
+        'id' => $this->primaryKey(),
+        'name' => $this->string(),
+        'description' => $this->text(),
+        'challenge_image' => $this->string(),
+        'validator_image' => $this->string(),
+        'created_at' => $this->datetime(),
+        'updated_at' => $this->datetime(),
+      ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%speed_problem}}');
+    }
+}
