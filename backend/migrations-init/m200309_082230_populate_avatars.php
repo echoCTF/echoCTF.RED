7bb2d92da7b5ced31b062ed836bfb6bbf895340a
Pantelis Roditis <proditis@echothrust.com>
2024-11-12 14:34:53 +0200

    make sure init_data can be re-applied with no errors

--- backend/migrations-init/m200309_082230_populate_avatars.php
+++ backend/migrations-init/m200309_082230_populate_avatars.php
@@ -12,54 +12,13 @@ class m200309_082230_populate_avatars extends Migration
      */
     public function safeUp()
     {
-      $CREATE_SQL="INSERT INTO avatar value ('256_10.png');
-                   INSERT INTO avatar value ('256_11.png');
-                   INSERT INTO avatar value ('256_12.png');
-                   INSERT INTO avatar value ('256_13.png');
-                   INSERT INTO avatar value ('256_14.png');
-                   INSERT INTO avatar value ('256_15.png');
-                   INSERT INTO avatar value ('256_16.png');
-                   INSERT INTO avatar value ('256_1.png');
-                   INSERT INTO avatar value ('256_2.png');
-                   INSERT INTO avatar value ('256_3.png');
-                   INSERT INTO avatar value ('256_4.png');
-                   INSERT INTO avatar value ('256_5.png');
-                   INSERT INTO avatar value ('256_6.png');
-                   INSERT INTO avatar value ('256_7.png');
-                   INSERT INTO avatar value ('256_8.png');
-                   INSERT INTO avatar value ('256_9.png');
-                   INSERT INTO avatar value ('avatar-1.png');
-                   INSERT INTO avatar value ('avatar-2.png');
-                   INSERT INTO avatar value ('avatar-3.png');
-                   INSERT INTO avatar value ('avatar-4.png');
-                   INSERT INTO avatar value ('avatar-5.png');
-                   INSERT INTO avatar value ('avatar-6.png');
-                   INSERT INTO avatar value ('avatar-7.png');
-                   INSERT INTO avatar value ('avatar-8.png');
-                   INSERT INTO avatar value ('Bride.png');
-                   INSERT INTO avatar value ('default.png');
-                   INSERT INTO avatar value ('Franky.png');
-                   INSERT INTO avatar value ('Skeleton.png');
-                   INSERT INTO avatar value ('users-10.svg');
-                   INSERT INTO avatar value ('users-11.svg');
-                   INSERT INTO avatar value ('users-12.svg');
-                   INSERT INTO avatar value ('users-13.svg');
-                   INSERT INTO avatar value ('users-14.svg');
-                   INSERT INTO avatar value ('users-15.svg');
-                   INSERT INTO avatar value ('users-16.svg');
-                   INSERT INTO avatar value ('users-1.svg');
-                   INSERT INTO avatar value ('users-2.svg');
-                   INSERT INTO avatar value ('users-3.svg');
-                   INSERT INTO avatar value ('users-4.svg');
-                   INSERT INTO avatar value ('users-5.svg');
-                   INSERT INTO avatar value ('users-6.svg');
-                   INSERT INTO avatar value ('users-7.svg');
-                   INSERT INTO avatar value ('users-8.svg');
-                   INSERT INTO avatar value ('users-9.svg');
-                   INSERT INTO avatar value ('Vampire-Girl.png');
-                   INSERT INTO avatar value ('Vampire.png');
-                   INSERT INTO avatar value ('Witch.png');
-";
+      $CREATE_SQL="INSERT IGNORE INTO avatar value ('256_10.png'),('256_11.png'),('256_12.png'),('256_13.png'),('256_14.png'),
+('256_15.png'),('256_16.png'), ('256_1.png'), ('256_2.png'), ('256_3.png'), ('256_4.png'), ('256_5.png'), ('256_6.png'),
+('256_7.png'), ('256_8.png'), ('256_9.png'), ('avatar-1.png'), ('avatar-2.png'), ('avatar-3.png'), ('avatar-4.png'), ('avatar-5.png'),
+('avatar-6.png'), ('avatar-7.png'), ('avatar-8.png'), ('Bride.png'), ('default.png'), ('Franky.png'),
+('Skeleton.png'), ('users-10.svg'), ('users-11.svg'), ('users-12.svg'), ('users-13.svg'), ('users-14.svg'), ('users-15.svg'),
+('users-16.svg'), ('users-1.svg'), ('users-2.svg'), ('users-3.svg'), ('users-4.svg'), ('users-5.svg'), ('users-6.svg'),
+('users-7.svg'), ('users-8.svg'), ('users-9.svg'), ('Vampire-Girl.png'), ('Vampire.png'), ('Witch.png')";
             $this->db->createCommand($CREATE_SQL)->execute();
     }
 
