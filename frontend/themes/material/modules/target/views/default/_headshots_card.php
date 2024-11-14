9a2a94c5df78692502e64e393d40d221f0e7ca89
Pantelis Roditis <proditis@echothrust.com>
2024-11-14 15:31:45 +0200

    dont perform 50 queries but rather 1 for all

--- frontend/themes/material/modules/target/views/default/_headshots_card.php
+++ frontend/themes/material/modules/target/views/default/_headshots_card.php
@@ -1,27 +1,29 @@
+<?php use yii\helpers\Html; ?>
 <div class="card bg-dark headshots">
   <div class="card-header">
-    <h4><i class="fas fa-skull"></i> <?=\Yii::t('app','{headshots,plural,=0{No headshots yet} =1{# Headshot} other{# Headshots (newer first)}}',['headshots'=>count($target->headshots)])?>
+    <h4><i class="fas fa-skull"></i> <?= \Yii::t('app', '{headshots,plural,=0{No headshots yet} =1{# Headshot} other{# Headshots (newer first)}}', ['headshots' => count($target->headshots)]) ?>
     </h4>
   </div>
   <div class="card-body table-responsive">
     <?php
-    $headshots=[];
-    foreach($target->lastHeadshots as $hs)
-    {
-      if((int) $hs->player->active === 1)
-        $headshots[]=$hs->player->profile->link;
-    }
-    if(!empty($headshots))
-    {
-      echo "<code>",implode(", ", array_slice($headshots, 0,19)),"</code>";
-      if(count($headshots)>19){
-        echo "<details class=\"headshotters\">";
-        echo "<summary data-open=\"Hide more\" data-close=\"Show more\"></summary>";
-        echo "<code>",implode(", ", array_slice($headshots, 19)),"</code>";
-        echo "</details>";
+    $headshots = [];
+    foreach ($target->lastHeadshots as $hs) {
+      $to = $hs->playerWithProfile->profile->linkto;
+      if ($to !== null) {
+        $headshots[] = Html::a(Html::encode($hs->playerWithProfile->username), $to, ['data-pjax' => 0]);
       }
     }
-    else
-      echo '<code>'.\Yii::t('app','no one yet...').'</code>';?>
+    if (!empty($headshots)) {
+      echo "<code>", implode(", ", array_slice($headshots, 0, 19)), "</code>";
+      if (count($headshots) > 19) {
+        echo "<details class=\"headshotters\">";
+        echo "<summary data-open=\"Hide more\" data-close=\"Show more\"></summary>";
+        echo "<code>", implode(", ", array_slice($headshots, 19)), "</code>";
+        echo "</details>";
+      }
+    } else {
+      echo '<code>' . \Yii::t('app', 'no one yet...') . '</code>';
+    }
+    ?>
   </div>
 </div>
