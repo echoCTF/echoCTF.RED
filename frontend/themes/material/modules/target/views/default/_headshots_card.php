6f08ac82e1954fab948a0eeb8bc8fb5921ee3d2b
Pantelis Roditis <proditis@echothrust.com>
2024-11-15 00:15:52 +0200

    use a widget for the headshots

--- frontend/themes/material/modules/target/views/default/_headshots_card.php
+++ frontend/themes/material/modules/target/views/default/_headshots_card.php
@@ -1,29 +1,13 @@
-<?php use yii\helpers\Html; ?>
+<?php
+use yii\helpers\Html;
+use app\widgets\target\HeadshotsWidget;
+?>
 <div class="card bg-dark headshots">
   <div class="card-header">
     <h4><i class="fas fa-skull"></i> <?= \Yii::t('app', '{headshots,plural,=0{No headshots yet} =1{# Headshot} other{# Headshots (newer first)}}', ['headshots' => count($target->headshots)]) ?>
     </h4>
   </div>
   <div class="card-body table-responsive">
-    <?php
-    $headshots = [];
-    foreach ($target->lastHeadshots as $hs) {
-      $to = $hs->playerWithProfile->profile->linkto;
-      if ($to !== null) {
-        $headshots[] = Html::a(Html::encode($hs->playerWithProfile->username), $to, ['data-pjax' => 0]);
-      }
-    }
-    if (!empty($headshots)) {
-      echo "<code>", implode(", ", array_slice($headshots, 0, 19)), "</code>";
-      if (count($headshots) > 19) {
-        echo "<details class=\"headshotters\">";
-        echo "<summary data-open=\"Hide more\" data-close=\"Show more\"></summary>";
-        echo "<code>", implode(", ", array_slice($headshots, 19)), "</code>";
-        echo "</details>";
-      }
-    } else {
-      echo '<code>' . \Yii::t('app', 'no one yet...') . '</code>';
-    }
-    ?>
+    <?php echo HeadshotsWidget::widget(['target_id'=>$target->id]);?>
   </div>
 </div>
