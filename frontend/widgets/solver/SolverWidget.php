d88cf43fef49c9b4a05f822cf68de60468fccbbc
Pantelis Roditis <proditis@echothrust.com>
2024-11-15 10:05:04 +0200

    update solvers to use cache and single query

--- frontend/widgets/solver/SolverWidget.php
+++ frontend/widgets/solver/SolverWidget.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Challenge Solver widget
  */
@@ -7,20 +8,38 @@ namespace app\widgets\solver;
 
 use yii\base\Widget;
 use yii\helpers\Html;
+use app\components\ProfileVisibility as PV;
+
 class SolverWidget extends Widget
 {
-    public $model;
-    public $slice=19;
-    public $htmlOptions=['class'=>'card bg-dark solves'];
-    public function init()
-    {
-        parent::init();
+  public $challenge_id;
+  public $slice = 19;
+  public $htmlOptions = ['class' => 'card bg-dark solves'];
+
+  public function init()
+  {
+    parent::init();
+  }
+
+  public function run()
+  {
+    // Register AssetBundle
+    SolverWidgetAsset::register($this->getView());
+    $solvers = [];
+    $sql = "SELECT player.id,player.username,profile.id as profile_id,profile.visibility FROM challenge_solver LEFT JOIN player ON player.id=challenge_solver.player_id LEFT JOIN profile on profile.player_id=player.id WHERE challenge_solver.challenge_id=:challenge_id AND player.status=10 ORDER BY challenge_solver.created_at DESC,challenge_solver.player_id asc LIMIT 50";
+    $result = \Yii::$app->db->createCommand($sql, [':challenge_id' => $this->challenge_id])->cache(180)->query();
+
+    foreach ($result as $hs) {
+      $to = Html::encode($hs['username']);
+
+      if (intval(\Yii::$app->user->id) === intval($hs['id']))
+        $to = Html::a(Html::encode($hs['username']), ['/profile/me'], ['data-pjax' => 0]);
+      else if (PV::visible($hs['id'], $hs['visibility']) === true)
+        $to = Html::a(Html::encode($hs['username']), ['/profile/index', 'id' => $hs['profile_id']], ['data-pjax' => 0]);
+
+      $solvers[] = $to;
     }
 
-    public function run()
-    {
-          // Register AssetBundle
-        SolverWidgetAsset::register($this->getView());
-        return $this->render('_card', ['solvers' => $this->model, 'htmlOptions'=>$this->htmlOptions,'slice'=>$this->slice]);
-    }
+    return $this->render('_card', ['solvers' => $solvers, 'htmlOptions' => $this->htmlOptions, 'slice' => $this->slice]);
+  }
 }
