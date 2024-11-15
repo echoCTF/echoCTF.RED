3e6c2306b7122f3baf0b824af8a5274b9e737c14
Pantelis Roditis <proditis@echothrust.com>
2024-11-15 12:22:48 +0200

    add visible criteria. Only show tab if there is something to show

--- backend/modules/frontend/views/profile/view_full.php
+++ backend/modules/frontend/views/profile/view_full.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
       <div class="col-md-4">
         <div class="panel">
           <div class="panel-heading">
-            <?= $this->render('_quick_actions',['model'=>$model]); ?>
+            <?= $this->render('_quick_actions', ['model' => $model]); ?>
           </div>
         </div>
         <?= $this->render('_player_details', ['model' => $model]); ?>
@@ -57,60 +57,70 @@ $this->params['breadcrumbs'][] = $this->title;
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['score-monthly', 'id' => $model->id])],
                 'options' => ['id' => 'score-monthly-tab'],
+                'visible' => count($model->owner->monthlyScores) > 0
               ],
               [
                 'label' => '<i class="fas fa-spinner" data-placement="top" title="Target Progress"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['target-progress', 'id' => $model->id])],
                 'options' => ['id' => 'target-progress-tab'],
+                'visible' => count($model->owner->progressTargets) > 0
               ],
               [
                 'label' => '<i class="fas fa-skull-crossbones" data-toggle="tooltip" data-placement="top" title="Headshots"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['headshots', 'id' => $model->id])],
                 'options' => ['id' => 'headshots-tab'],
+                'visible' => count($model->owner->headshots) > 0
               ],
               [
                 'label' => '<i class="fas fa-book-dead" data-toggle="tooltip" data-placement="top" title="Submitted Writeups"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['writeups', 'id' => $model->id])],
                 'options' => ['id' => 'writeups-tab'],
+                'visible' => count($model->owner->writeups) > 0
               ],
               [
                 'label' => '<i class="fas fa-notes-medical" data-toggle="tooltip" data-placement="top" title="Activated Writeups"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['activated-writeups', 'id' => $model->id])],
                 'options' => ['id' => 'activated-writeups-tab'],
+                'visible' => count($model->owner->playerTargetHelps) > 0
               ],
               [
                 'label' => '<i class="fas fa-comment-medical" data-toggle="tooltip" data-placement="top" title="Rated Writeups"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['writeup-rating', 'id' => $model->id])],
                 'options' => ['id' => 'writeup-rating-tab'],
+                'visible' => count($model->owner->writeupRatings) > 0
               ],
               [
                 'label' => '<i class="fas fa-tasks" data-toggle="tooltip" data-placement="top" title="Challenge Solves"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['solves', 'id' => $model->id])],
                 'options' => ['id' => 'solves-tab'],
+                'visible' => count($model->owner->challengeSolvers) > 0
               ],
               [
                 'label' => '<i class="fas fa-history" data-toggle="tooltip" data-placement="top" title="VPN History"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['vpn-history', 'id' => $model->id])],
                 'options' => ['id' => 'vpn-history-tab'],
+                'visible' => count($model->owner->vpnHistory) > 0
               ],
               [
                 'label' => '<i class="fas fa-sync" data-toggle="tooltip" data-placement="top" title="Spin History"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['spin-history', 'id' => $model->id])],
                 'options' => ['id' => 'spin-history-tab'],
+                'visible' => count($model->owner->spinHistory) > 0
               ],
               [
                 'label' => '<i class="fas fa-exclamation-triangle" data-toggle="tooltip" data-placement="top" title="Notifications"></i>',
                 'headerOptions' => ['style' => 'font-weight:bold'],
                 'linkOptions' => ['data-url' => Url::to(['notifications', 'id' => $model->id])],
                 'options' => ['id' => 'notifications-tab'],
+                'visible' => count($model->owner->notifications) > 0
               ],
             ],
           ]);
