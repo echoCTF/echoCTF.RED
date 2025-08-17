cfe84cbe119a6ec4ab1de9e20eefc8696f35e2c7
Pantelis Roditis <proditis@echothrust.com>
2025-08-17 20:15:58 +0300

    count the number of players per levels and display their percentage

--- frontend/themes/material/modules/help/views/experience/index.php
+++ frontend/themes/material/modules/help/views/experience/index.php
@@ -4,7 +4,6 @@ use yii\widgets\ListView;
 $this->title=Html::encode(Yii::$app->sys->event_name.' '.\Yii::t('app','Experience Levels'));
 $this->_description=\Yii::t('app','Experience levels and their requirements in points');
 $this->_url=\yii\helpers\Url::to(['index'], 'https');
-use app\components\formatters\Anchor;
 ?>
 <div class="faq-index">
   <div class="body-content">
@@ -15,7 +14,7 @@ use app\components\formatters\Anchor;
       <h4><?=\Yii::t('app','Table of Contents')?></h4>
       <ol class="orbitron text-white">
       <?php foreach($dataProvider->getModels() as $entry):?>
-        <li><?=Html::encode($entry->name);?> (<code><?=number_format($entry->min_points)?>-<?=number_format($entry->max_points)?></code>)</li>
+        <li><?=Html::encode($entry->name);?> (<code><?=number_format($entry->min_points)?>-<?=number_format($entry->max_points)?></code>): <small><?=number_format(($entry->player_count/$totalPlayers)*100,2)?>% of Players</small></li>
       <?php endforeach;?>
       </ol>
     <?php endif;?>
