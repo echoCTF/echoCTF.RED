ab66489ebe017b12ef6b7509efd05ba3f2542f7f
Pantelis Roditis <proditis@echothrust.com>
2025-08-22 01:25:33 +0300

    add default values for created/updated at and remove from form

--- backend/modules/moderation/views/abuser/_form.php
+++ backend/modules/moderation/views/abuser/_form.php
@@ -24,10 +24,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'model_id')->textInput() ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
