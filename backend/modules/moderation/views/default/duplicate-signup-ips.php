13b6a588094ffca67c87544bb1818286fc058b5c
Pantelis Roditis <proditis@echothrust.com>
2025-08-23 16:34:25 +0300

    remove view full from the column actions since there are multiple players on each row

--- backend/modules/moderation/views/default/duplicate-signup-ips.php
+++ backend/modules/moderation/views/default/duplicate-signup-ips.php
@@ -2,60 +2,60 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use app\widgets\ProfileLink;
+use app\widgets\ProfileActions;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\modules\activity\models\StreamSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title=ucfirst(Yii::$app->controller->module->id).' / '.ucfirst(Yii::$app->controller->id) . ' / Duplicate Signup IPs';
-$this->params['breadcrumbs'][] = ['label' => ucfirst(Yii::$app->controller->module->id) ];
-$this->params['breadcrumbs'][] = ['label' => ucfirst(Yii::$app->controller->id), 'url'=>['index']];
+$this->title = ucfirst(Yii::$app->controller->module->id) . ' / ' . ucfirst(Yii::$app->controller->id) . ' / Duplicate Signup IPs';
+$this->params['breadcrumbs'][] = ['label' => ucfirst(Yii::$app->controller->module->id)];
+$this->params['breadcrumbs'][] = ['label' => ucfirst(Yii::$app->controller->id), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => "Duplicate Signup IPs", 'url' => ['duplicate-signup-ips']];
 ?>
 <div class="stream-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <?php \yii\widgets\Pjax::begin(); ?>
+  <?php // echo $this->render('_search', ['model' => $searchModel]);
+  ?>
+  <?php \yii\widgets\Pjax::begin(); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            [
-              'attribute'=>'signup_ip',
-              'value'=>function($model) {return $model->signup_ip === NULL ? null : long2ip($model->signup_ip);},
-              'headerOptions'=>['style'=>'width: 160px'],
-            ],
-            [
-                'attribute'=>'duplicates',
-                'contentOptions'=>['style'=>'width: 50px'],
-            ],
-            [
-                'attribute'=>'offenders',
-                'contentOptions'=>['style'=>'word-wrap:break-word'],
-            ],
-            [
-              'class' => 'yii\grid\ActionColumn',
-              'template' => '{player-view-full} {view} {update} {delete}',
-              'buttons' => [
-                'player-view-full' => function($url, $model) {
-                  $url =  \yii\helpers\Url::to(['/frontend/profile/view-full', 'id' => $model->player->profile->id]);
-                  return Html::a(
-                      '<i class="bi bi-person-lines-fill"></i>',
-                      $url,
-                      [
-                        'title' => 'View full profile',
-                        'data-pjax' => '0',
-                      ]
-                  );
-                },
-              ]
-            ]
-        ],
-    ]);?>
-    <?php \yii\widgets\Pjax::end(); ?>
+  <?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+      [
+        'attribute' => 'signup_ip',
+        'format'=>'raw',
+        'value' => function ($model) {
+          return $model->signup_ip === NULL ? null : '<abbr title="'.gethostbyaddr(long2ip($model->signup_ip)).'">'.long2ip($model->signup_ip)."</abbr>";
+        },
+        'headerOptions' => ['style' => 'width: 160px'],
+      ],
+      [
+        'attribute' => 'duplicates',
+        'contentOptions' => ['style' => 'width: 50px'],
+      ],
+      [
+        'attribute' => 'offenders',
+        'contentOptions' => ['style' => 'word-wrap:break-word'],
+        'format' => 'html',
+        'value' => function ($model) {
+          $links = [];
+          foreach (explode(", ", $model->offenders) as $p) {
+            $links[] = ProfileLink::widget([
+              'username' => trim($p),
+              'actions'=>true
+            ]);
+          }
+          return implode(", ", $links);
+        }
+      ],
+    ],
+  ]); ?>
+  <?php \yii\widgets\Pjax::end(); ?>
 
 
 </div>
