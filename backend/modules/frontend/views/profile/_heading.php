15863fa1f222a9d37e119254ae0e2ccfecdda002
Pantelis Roditis <proditis@echothrust.com>
2025-08-26 13:08:10 +0300

    show academic grouping category if enabled

--- backend/modules/frontend/views/profile/_heading.php
+++ backend/modules/frontend/views/profile/_heading.php
@@ -13,7 +13,7 @@ use yii\helpers\Html;
       <?php endif;?>
     </div>
     <div class="media-body va-m">
-      <h2 class="media-heading"><?= Html::encode($model->owner->username) ?>
+      <h2 class="media-heading"><?php if(\Yii::$app->sys->academic_grouping!==false) echo $model->owner->academicShort.": ";?><?= Html::encode($model->owner->username) ?>
         <small><?php if(trim($model->owner->fullname)!=="") echo " - ",Html::encode($model->owner->fullname) ?><?php if($model->owner->metadata && trim($model->owner->metadata->affiliation)!=="") echo " - ",Html::encode($model->owner->metadata->affiliation) ?></small>
       </h2>
       <?php if($model->owner->teamPlayer):?><p>Team: <?php echo Html::a($model->owner->teamPlayer->team->name,['/frontend/team/view','id'=>$model->owner->teamPlayer->team_id]), " ",Html::a('<i class="fa-solid fa-up-right-from-square"></i>','//'.Yii::$app->sys->offense_domain.'/team/'.$model->owner->teamPlayer->team->token,['title'=>'Go to team frontend page']);?></p><?php endif;?>
