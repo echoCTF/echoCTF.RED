2fc523975a7cf828ea8d26999ffb206eceeffbe3
Pantelis Roditis <proditis@echothrust.com>
2025-09-03 01:13:29 +0300

    on network add send notification to players with access to this network

--- backend/modules/infrastructure/views/network/index.php
+++ backend/modules/infrastructure/views/network/index.php
@@ -7,12 +7,12 @@ use yii\grid\GridView;
 /* @var $searchModel app\modules\gameplay\models\NetworkSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title=Yii::t('app', 'Networks');
-$this->params['breadcrumbs'][]=['label' => 'Networks', 'url' => ['index']];
+$this->title = Yii::t('app', 'Networks');
+$this->params['breadcrumbs'][] = ['label' => 'Networks', 'url' => ['index']];
 yii\bootstrap5\Modal::begin([
-    'title' => '<h2><i class="bi bi-info-circle-fill"></i> '.Html::encode($this->title).' Help</h2>',
-    'toggleButton' => ['label' => '<i class="bi bi-info-circle-fill"></i> Help', 'class' => 'btn btn-info'],
-  'options'=>['class'=>'modal-lg']
+  'title' => '<h2><i class="bi bi-info-circle-fill"></i> ' . Html::encode($this->title) . ' Help</h2>',
+  'toggleButton' => ['label' => '<i class="bi bi-info-circle-fill"></i> Help', 'class' => 'btn btn-info'],
+  'options' => ['class' => 'modal-lg']
 ]);
 echo yii\helpers\Markdown::process($this->render('help/index.md'), 'gfm');
 yii\bootstrap5\Modal::end();
@@ -20,81 +20,87 @@ yii\bootstrap5\Modal::end();
 ?>
 <div class="network-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Network'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+  <p>
+    <?= Html::a(Yii::t('app', 'Create Network'), ['create'], ['class' => 'btn btn-success']) ?>
+  </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+  <?php // echo $this->render('_search', ['model' => $searchModel]);
+  ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            'id',
-            [
-                'attribute'=>'icon',
-                'format'=>'html',
-                'contentOptions'=>['class'=>'text-center'],
-                'value'=>function($model){
-                    if(substr($model->icon,0,1)!=='/') {
-                        return $model->icon;
-                    }
-                    return Html::img('//'.Yii::$app->sys->offense_domain.$model->icon,['style'=>'max-width: 50px;','class'=>'img-thumbnail']);
-                }
-            ],
-            'codename:text',
-            [
-                'attribute'=>'name',
-                'contentOptions'=>['style'=>'white-space: nowrap;']
+  <?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+      'id',
+      [
+        'attribute' => 'icon',
+        'format' => 'html',
+        'contentOptions' => ['class' => 'text-center'],
+        'value' => function ($model) {
+          if (substr($model->icon, 0, 1) !== '/') {
+            return $model->icon;
+          }
+          return Html::img('//' . Yii::$app->sys->offense_domain . $model->icon, ['style' => 'max-width: 50px;', 'class' => 'img-thumbnail']);
+        }
+      ],
+      'codename:text',
+      [
+        'attribute' => 'name',
+        'contentOptions' => ['style' => 'white-space: nowrap;']
 
-            ],
-            [
-                'attribute'=>'description',
-                'contentOptions'=>['class'=>'font-weight-light small text-monospace'],
-                'format'=>'html',
-            ],
-            [
-                'attribute'=>'public',
-                'format'=>'boolean',
-                'contentOptions'=>['class'=>'text-center'],
-            ],
-            [
-                'attribute'=>'guest',
-                'format'=>'boolean',
-                'contentOptions'=>['class'=>'text-center'],
-            ],
-            [
-                'attribute'=>'active',
-                'format'=>'boolean',
-                'contentOptions'=>['class'=>'text-center'],
-            ],
-            [
-                'attribute'=>'announce',
-                'format'=>'boolean',
-                'contentOptions'=>['class'=>'text-center'],
-            ],
-            [
-                'label'=>'Targets',
-                'format'=>'integer',
-                'attribute'=>'network_targets',
-                'value'=>function($model){ return count($model->networkTargets); },
-                'contentOptions'=>['class'=>'text-center'],
-            ],
-            [
-                'label'=>'Players',
-                'attribute'=>'network_players',
-                'format'=>'integer',
-                'value'=>function($model){ return count($model->networkPlayers); },
-                'contentOptions'=>['class'=>'text-center'],
-            ],
-            'weight:integer',
-//            'ts',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]);?>
+      ],
+      [
+        'attribute' => 'description',
+        'contentOptions' => ['class' => 'font-weight-light small text-monospace'],
+        'format' => 'html',
+      ],
+      [
+        'attribute' => 'public',
+        'format' => 'boolean',
+        'contentOptions' => ['class' => 'text-center'],
+      ],
+      [
+        'attribute' => 'guest',
+        'format' => 'boolean',
+        'contentOptions' => ['class' => 'text-center'],
+      ],
+      [
+        'attribute' => 'active',
+        'format' => 'boolean',
+        'contentOptions' => ['class' => 'text-center'],
+      ],
+      [
+        'attribute' => 'announce',
+        'format' => 'boolean',
+        'contentOptions' => ['class' => 'text-center'],
+      ],
+      [
+        'label' => 'Targets',
+        'format' => 'integer',
+        'attribute' => 'network_targets',
+        'value' => function ($model) {
+          return count($model->networkTargets);
+        },
+        'contentOptions' => ['class' => 'text-center'],
+      ],
+      [
+        'label' => 'Players',
+        'attribute' => 'network_players',
+        'format' => 'integer',
+        'value' => function ($model) {
+          return count($model->networkPlayers);
+        },
+        'contentOptions' => ['class' => 'text-center'],
+      ],
+      'weight:integer',
+      [
+        'class' => 'app\components\columns\ActionColumn',
+        'template' => '{view} {update} {delete} {notify}',
+      ],
+    ],
+  ]); ?>
 
 
 </div>
--- backend/modules/infrastructure/views/network/view.php
+++ backend/modules/infrastructure/views/network/view.php
@@ -2,45 +2,55 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\bootstrap5\Modal;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\gameplay\models\Network */
 
-$this->title=$model->name;
-$this->params['breadcrumbs'][]=['label' => Yii::t('app', 'Networks'), 'url' => ['index']];
-$this->params['breadcrumbs'][]=$this->title;
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Networks'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+Modal::begin([
+  'id' => 'notify-modal',
+  'title' => '<h2><i class="fas fa-paper-plane"></i> Notify players of [<code>' . Html::encode($this->title) . '</code>]</h2>',
+  'toggleButton' => false,
+  'options' => ['class' => 'modal-lg']
+]);
+echo '<div id="notificationContent"></div>'; // Content will be loaded here via AJAX
+Modal::end();
 ?>
 <div class="network-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-          'id',
-          'name',
-          'codename',
-          'icon',
-          'description:ntext',
-          'public:boolean',
-          'guest:boolean',
-          'active:boolean',
-          'announce:boolean',
-          'weight:integer',
-          'ts',
-        ],
+  <p>
+    <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+    <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+      'class' => 'btn btn-danger',
+      'data' => [
+        'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+        'method' => 'post',
+      ],
     ]) ?>
+    <?= \app\widgets\NotifyButton::widget(['url' => ['notify', 'id' => $model->id],]) ?>
+  </p>
+
+  <?= DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+      'id',
+      'name',
+      'codename',
+      'icon',
+      'description:ntext',
+      'public:boolean',
+      'guest:boolean',
+      'active:boolean',
+      'announce:boolean',
+      'weight:integer',
+      'ts',
+    ],
+  ]) ?>
 
 </div>
