b157ce9908f74dffe0953e936cd3ebea58f60476
Pantelis Roditis <proditis@echothrust.com>
2025-09-02 16:28:58 +0300

    typo in post credits

--- frontend/themes/material/modules/target/views/default/_target_metadata.php
+++ frontend/themes/material/modules/target/views/default/_target_metadata.php
@@ -7,5 +7,5 @@
   <?php if(!empty($metadata->pre_credits)):?><b><?=\Yii::t('app','Pre exploitation credits')?></b>: <?=\yii\helpers\Markdown::process($metadata->pre_credits,'gfm')?><br/><?php endif;?>
   <?php if(!empty($metadata->pre_exploitation)):?><b><?=\Yii::t('app','Pre exploitation details')?></b>: <?=\yii\helpers\Markdown::process($metadata->pre_exploitation,'gfm')?><br/><?php endif;?>
   <?php if((($identity->player_id===Yii::$app->user->id && $target->progress==100) || Yii::$app->user->identity->isAdmin) && !empty($metadata->post_exploitation)):?><b><?=\Yii::t('app','Post exploitation')?></b>: <?=\yii\helpers\Markdown::process($metadata->post_exploitation,'gfm')?><br/><?php endif;?>
-  <?php if((($identity->player_id===Yii::$app->user->id && $target->progress==100) || Yii::$app->user->identity->isAdmin) && !empty($metadata->post_credits)):?><b><?=\Yii::t('app','Post exploitation credits')?></b>: <?=\yii\helpers\Markdown::process($metadata->post_credit,'gfm')?><br/><?php endif;?>
+  <?php if((($identity->player_id===Yii::$app->user->id && $target->progress==100) || Yii::$app->user->identity->isAdmin) && !empty($metadata->post_credits)):?><b><?=\Yii::t('app','Post exploitation credits')?></b>: <?=\yii\helpers\Markdown::process($metadata->post_credits,'gfm')?><br/><?php endif;?>
 <?php endif;?>
