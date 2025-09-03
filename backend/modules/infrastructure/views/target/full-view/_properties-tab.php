618c19a68ae31c9e4a935a67a6cb07375212dfd7
Pantelis Roditis <proditis@echothrust.com>
2025-09-03 20:34:07 +0300

    on target full view show the example command

--- backend/modules/infrastructure/views/target/full-view/_properties-tab.php
+++ backend/modules/infrastructure/views/target/full-view/_properties-tab.php
@@ -19,5 +19,10 @@ use yii\widgets\DetailView;
               'attribute'=>'network.name'
             ],
             'parameters',
+            [
+              'label'=>'Examples',
+              'format'=>'raw',
+              'value'=>function($model){ return '<pre>'.sprintf("docker run -itd \\\n--name %s \\\n--dns %s \\\n--hostname %s \\\n--ip %s \\\n--mac-address %s \\\n--network %s \\\n%s", $model->name,$model->dns,$model->fqdn,$model->ipoctet,$model->mac,$model->net,$model->image).'</pre>'; }
+            ],
         ],
     ]) ?>
