b56254310265f8bff0453e0709daadea46862d16
Pantelis Roditis <proditis@echothrust.com>
2025-09-03 23:17:44 +0300

    add ts_ago estimation

--- backend/modules/activity/models/TeamStreamQuery.php
+++ backend/modules/activity/models/TeamStreamQuery.php
@@ -9,26 +9,28 @@ namespace app\modules\activity\models;
  */
 class TeamStreamQuery extends \yii\db\ActiveQuery
 {
-    /*public function active()
+  /*public function active()
     {
         return $this->andWhere('[[status]]=1');
     }*/
 
-    /**
-     * {@inheritdoc}
-     * @return TeamStream[]|array
-     */
-    public function all($db = null)
-    {
-        return parent::all($db);
-    }
+  /**
+   * {@inheritdoc}
+   * @return TeamStream[]|array
+   */
+  public function all($db = null)
+  {
+    $this->select(['team_stream.*', 'TS_AGO(team_stream.ts) as ts_ago']);
+    return parent::all($db);
+  }
 
-    /**
-     * {@inheritdoc}
-     * @return TeamStream|array|null
-     */
-    public function one($db = null)
-    {
-        return parent::one($db);
-    }
+  /**
+   * {@inheritdoc}
+   * @return TeamStream|array|null
+   */
+  public function one($db = null)
+  {
+    $this->select(['team_stream.*', 'TS_AGO(team_stream.ts) as ts_ago']);
+    return parent::one($db);
+  }
 }
