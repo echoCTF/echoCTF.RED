9e7d186ffb048825c593eefc4ba4436abcd7c074
Pantelis Roditis <proditis@echothrust.com>
2025-09-16 14:20:32 +0300

    update datepicker and make sure we load it just about in the end so that it doesnt fail

--- frontend/widgets/vote/VoteWidgetAsset.php
+++ frontend/widgets/vote/VoteWidgetAsset.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\widgets\vote;
 
 use yii\web\AssetBundle;
@@ -6,20 +7,19 @@ use yii\web\AssetBundle;
 class VoteWidgetAsset extends AssetBundle
 {
     public $js=[
-        'js/plugins/bootstrap-selectpicker.min.js',
-    ];
+  ];
 
     public $css=[
         'css/bootstrap-select.min.css',
-    ];
+  ];
 //    $this->registerJs('$.fn.selectpicker.Constructor.BootstrapVersion = "4";');
 
     public $depends=[];
 
-    public function init()
-    {
-        // Tell AssetBundle where the assets files are
-  //      $this->sourcePath=__DIR__."/assets";
-        parent::init();
-    }
+  public function init()
+  {
+    // Tell AssetBundle where the assets files are
+    //      $this->sourcePath=__DIR__."/assets";
+    parent::init();
+  }
 }
