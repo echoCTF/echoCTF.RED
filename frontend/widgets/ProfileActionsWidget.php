8b414ff79da377151db3ddbc963ca39a43402fac
Pantelis Roditis <proditis@echothrust.com>
2025-11-10 11:57:53 +0200

    fix logic for showing copy token

--- frontend/widgets/ProfileActionsWidget.php
+++ frontend/widgets/ProfileActionsWidget.php
@@ -33,10 +33,11 @@ class ProfileActionsWidget extends Widget
     if (\Yii::$app->sys->api_bearer_enable === true) {
       if (\Yii::$app->sys->subscriptions_feature_api===false && \Yii::$app->user->identity->apiToken === null) {
         $profile_actions['generate-token'] = ['encode' => false, 'label' => "<i class='fas fa-terminal'></i>&nbsp; Generate API Token", 'url' => Url::to(['profile/generate-token'], true), 'linkOptions' => ['class' => 'text-danger', 'data-swType' => 'question', 'data' => ['confirm' => 'You are about to generate a new API token! Are you sure?', 'method' => 'POST']]];
-      } else if(\Yii::$app->sys->subscriptions_feature_api===true && \Yii::$app->user->identity->apiToken !== null) {
+      } else if(\Yii::$app->user->identity->apiToken !== null) {
         $profile_actions['copy-token'] = ['encode' => false, 'label' => "<i class='fas fa-terminal'></i>&nbsp; Copy API Token", 'url' => $this->profile->owner->apiToken->token, 'linkOptions' => ['class' => 'copy-to-clipboard', 'swal-data' => 'API token copied to clipboard!']];
       }
     }
+
     if (\Yii::$app->user->identity->sSL && (time() - strtotime(\Yii::$app->user->identity->sSL->ts)) >= 3600)
       $profile_actions['revoke'] = ['encode' => false, 'label' => "<i class='fas fa-id-card'></i>&nbsp; Regenerate VPN Keys (revoke)", 'url' => Url::to(['profile/revoke'], true), 'linkOptions' => ['class' => 'text-danger', 'data-swType' => 'question', 'data' => ['confirm' => 'You are about to revoke your old keys and generate a new pair!', 'method' => 'POST']]];
 
