ed12ac4aeca7e6719f5e82c56b3ea0dc0e94dd47
Pantelis Roditis <proditis@echothrust.com>
2025-11-13 10:56:33 +0200

    add teaser support for item and read more

--- frontend/themes/material/dashboard/_news_item.php
+++ frontend/themes/material/dashboard/_news_item.php
@@ -1,7 +1,12 @@
 <?php
 use yii\helpers\HtmlPurifier;
+use yii\helpers\Html;
+list($teaser) = explode("<!--TEASER-->", $model->body);
 ?>
 <div class="news-item">
-    <h4 class="text-warning"><?=$model->category;?> <?= HtmlPurifier::process($model->title) ?></h4>
-    <p><?= HtmlPurifier::process($model->body) ?></p>
+    <h4 class="text-warning"><?=$model->category;?> <?= HtmlPurifier::process($model->title) ?> <span style="color: lightgray; font-size: 0.8em"><?= Yii::$app->formatter->asDate($model->created_at)?></span></h4>
+    <?= Yii::$app->formatter->asMarkdown($full ? $model->body : $teaser) ?>
+    <?php if(!$full && $teaser!==$model->body):?>
+    <p><?= Html::a("Read more...",['dashboard/news','id'=>$model->id]);?></p>
+    <?php endif;?>
 </div>
