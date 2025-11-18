85c7265fc6c30e785991fa367c93bcdfd50985ed
Pantelis Roditis <proditis@echothrust.com>
2025-11-18 11:11:05 +0200

    formatting and sort improvements

--- backend/modules/sales/models/PriceSearch.php
+++ backend/modules/sales/models/PriceSearch.php
@@ -11,62 +11,77 @@ use app\modules\sales\models\Price;
  */
 class PriceSearch extends Price
 {
+  public $product_name;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-          [['id','recurring_interval','metadata','currency','nickname','product_id','ptype'], 'string'],
-          [['active'], 'boolean'],
-          [['unit_amount','interval_count'],'safe']
-      ];
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['id', 'recurring_interval', 'metadata', 'currency', 'nickname', 'product_id', 'ptype'], 'string'],
+      [['active'], 'boolean'],
+      [['unit_amount', 'interval_count', 'product_name'], 'safe']
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function scenarios()
+  {
+    // bypass scenarios() implementation in the parent class
+    return Model::scenarios();
+  }
+
+  /**
+   * Creates data provider instance with search query applied
+   *
+   * @param array $params
+   *
+   * @return ActiveDataProvider
+   */
+  public function search($params)
+  {
+    $query = Price::find()->joinWith(['product']);
+
+    // add conditions that should always apply here
+
+    $dataProvider = new ActiveDataProvider([
+      'query' => $query,
+    ]);
+
+    $this->load($params);
+
+    if (!$this->validate()) {
+      // uncomment the following line if you do not want to return any records when validation fails
+      // $query->where('0=1');
+      return $dataProvider;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+    // grid filtering conditions
+    $query->andFilterWhere([
+      'active' => $this->active,
+      'unit_amount' => $this->active
+    ]);
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query = Price::find();
+    $query->andFilterWhere(['like', 'id', $this->id])
+      ->andFilterWhere(['like', 'product_id', $this->product_id])
+      ->andFilterWhere(['like', 'product.name', $this->product_name]);
 
-        // add conditions that should always apply here
+    $dataProvider->setSort([
+      'defaultOrder' => ['unit_amount' => SORT_ASC, 'id' => SORT_ASC],
+      'attributes' => array_merge(
+        $dataProvider->getSort()->attributes,
+        [
+          'product_name' => [
+            'asc' => ['product.name' => SORT_ASC],
+            'desc' => ['product.name' => SORT_DESC],
+          ],
+        ]
+      ),
+    ]);
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params);
-
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'active'=>$this->active,
-            'unit_amount'=>$this->active
-        ]);
-
-        $query->andFilterWhere(['like', 'id', $this->id])
-            ->andFilterWhere(['like', 'product_id', $this->product_id]);
-
-        return $dataProvider;
-    }
+    return $dataProvider;
+  }
 }
