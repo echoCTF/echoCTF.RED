5399704ef72cd30bbbeea357d5c4a7ff6c4d61a4
Pantelis Roditis <proditis@echothrust.com>
2025-11-19 14:58:45 +0200

    introduce stripe portal link for when the customer has no active products

---
+++ frontend/modules/subscription/views/default/_stripe_portal.php
@@ -0,0 +1,24 @@
+<?php
+
+use \yii\helpers\Html;
+
+$subscription = \Yii::$app->getModule('subscription');
+?>
+<div class="row d-flex justify-content-center">
+  <div class="col-md-6">
+    <?php \app\widgets\Card::begin([
+      'header' => 'header-icon',
+      'type' => 'card-stats',
+      'icon' => '<i class="fa-brands fa-stripe"></i>',
+      'encode' => false,
+      'color' => 'stripe',
+      'title' => '<b>' . \Yii::t('app', 'Stripe Customer Portal!') . '</b>',
+      'footer' => $subscription->getPortalLink($this, Html::a('Stripe Portal', ['/subscription/default/redirect-customer-portal'], [
+        'class' => 'h4 font-weight-bold btn btn-outline-stripe btn-block',
+        'id' => 'stripePortal',
+      ])),
+    ]); ?>
+    Go to your Stripe Portal to manage your payment and subscription details as well as ability to update your current subscription.
+    <?php \app\widgets\Card::end(); ?>
+  </div>
+</div>
