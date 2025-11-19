1f773bf9f0bd152ae047112c1a6a634671259a35
Pantelis Roditis <proditis@echothrust.com>
2025-11-19 14:58:26 +0200

    formatting

--- frontend/modules/subscription/views/default/_product.php
+++ frontend/modules/subscription/views/default/_product.php
@@ -17,22 +17,21 @@
           'label' => Yii::$app->formatter->asCurrency(intval($price->unit_amount / 100)) . '/' . $price->recurring_interval,
           'options' => ['class' => 'btn ' . $model->htmlOptions('class') . ' text-dark text-bold', 'id' => $price->id],
         ]);
-        $this->registerJs('document
-                              .getElementById("' . $price->id . '")
-                              .addEventListener("click", function(evt) {
-                                evt.preventDefault();
-                                createCheckoutSession("' . $price->id . '").then(function(data) {
-                                  Swal.fire({
-                                    title: "",
-                                    html: "' . \Yii::t('app', 'You will be redirected to Stripe to complete your payment') . '",
-                                    closeOnClickOutside: false
-                                  }).then((result) => {
-                                    if(data)
-                                      stripe.redirectToCheckout({ sessionId: data.sessionId }).then(handleResult);
-                                    });
+        $this->registerJs('document.getElementById("' . $price->id . '")
+                          .addEventListener("click", function(evt) {
+                            evt.preventDefault();
+                            createCheckoutSession("' . $price->id . '").then(function(data) {
+                              Swal.fire({
+                                title: "",
+                                html: "' . \Yii::t('app', 'You will be redirected to Stripe to complete your payment') . '",
+                                closeOnClickOutside: false
+                              }).then((result) => {
+                                if(data)
+                                  stripe.redirectToCheckout({ sessionId: data.sessionId }).then(handleResult);
                                 });
-                              });
-                            ', \yii\web\View::POS_READY);
+                            });
+                          });
+                        ', \yii\web\View::POS_READY);
         ?>
       </div>
     <?php endif; ?>
