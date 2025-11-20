efd8fe0bd866b332ec07caae5e38f7de3d7bb1b4
Pantelis Roditis <proditis@echothrust.com>
2025-11-20 12:12:45 +0200

    change to use payment intents instead

--- backend/modules/sales/migrations/m251117_133332_create_player_payments_history.php
+++ backend/modules/sales/migrations/m251117_133332_create_player_payments_history.php
@@ -12,8 +12,7 @@ class m251117_133332_create_player_payments_history extends Migration
     $this->createTable('{{%player_payment_history}}', [
       'id' => $this->primaryKey(),
       'player_id' => $this->integer()->notNull(),
-      'product_id' => $this->string(40)->notNull(),
-      'price_id' => $this->string(32)->notNull(),
+      'payment_id' => $this->string(40)->notNull(),
       'amount' => $this->integer()->unsigned()->defaultValue(0),
       'metadata LONGTEXT COLLATE utf8mb4_bin',
       'created_at' => $this->datetime(),
