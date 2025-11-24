3ea856d5d195f69b331d177f487acfdfddff3c83
Pantelis Roditis <proditis@echothrust.com>
2025-11-24 13:53:51 +0200

    add payments history and payment details url routes

--- backend/migrations/m251123_081343_add_subscription_perk_url_route.php
+++ backend/migrations/m251123_081343_add_subscription_perk_url_route.php
@@ -10,6 +10,8 @@ class m251123_081343_add_subscription_perk_url_route extends Migration
   public function safeUp()
   {
     $this->upsert('url_route', ['source' => 'subscription/perk/configure/<id>', 'destination' => 'subscription/perk/configure', 'weight' => 725], true);
+    $this->upsert('url_route', ['source' => 'subscription/payments', 'destination' => 'subscription/default/payments', 'weight' => 726], true);
+    $this->upsert('url_route', ['source' => 'subscription/payment/<id>', 'destination' => 'subscription/default/payment', 'weight' => 727], true);
   }
 
   /**
