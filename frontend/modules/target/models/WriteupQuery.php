f7e3589a475a176a4bc82d6a1888735d18e0657a
Pantelis Roditis <proditis@echothrust.com>
2025-11-24 13:56:15 +0200

    dont fail due to confusion of status column

--- frontend/modules/target/models/WriteupQuery.php
+++ frontend/modules/target/models/WriteupQuery.php
@@ -9,33 +9,36 @@ namespace app\modules\target\models;
  */
 class WriteupQuery extends \yii\db\ActiveQuery
 {
-    public function approved()
-    {
-        return $this->andWhere(['approved'=>'1','status'=>'OK']);
-    }
-    public function active()
-    {
-        return $this->joinWith('player')->andWhere(['player.status'=>\app\models\Player::STATUS_ACTIVE]);
-    }
-    public function totals()
-    {
-        return $this->addSelect(['writeup.*','COUNT(*) AS cnt'])->approved()->active()->groupBy(['player_id'])->orderBy(['cnt' => SORT_DESC,'player_id'=>SORT_ASC]);
-    }
-    /**
-     * {@inheritdoc}
-     * @return Writeup[]|array
-     */
-    public function all($db = null)
-    {
-        return parent::all($db);
-    }
+  public function approved()
+  {
+    return $this->andWhere(['writeup.approved' => '1', 'writeup.status' => 'OK']);
+  }
 
-    /**
-     * {@inheritdoc}
-     * @return Writeup|array|null
-     */
-    public function one($db = null)
-    {
-        return parent::one($db);
-    }
+  public function active()
+  {
+    return $this->joinWith('player')->andWhere(['player.status' => \app\models\Player::STATUS_ACTIVE]);
+  }
+
+  public function totals()
+  {
+    return $this->addSelect(['writeup.*', 'COUNT(*) AS cnt'])->approved()->active()->groupBy(['player_id'])->orderBy(['cnt' => SORT_DESC, 'player_id' => SORT_ASC]);
+  }
+
+  /**
+   * {@inheritdoc}
+   * @return Writeup[]|array
+   */
+  public function all($db = null)
+  {
+    return parent::all($db);
+  }
+
+  /**
+   * {@inheritdoc}
+   * @return Writeup|array|null
+   */
+  public function one($db = null)
+  {
+    return parent::one($db);
+  }
 }
