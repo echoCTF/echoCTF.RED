3996c7dfe34b613e5ba4f9572f84554f4374cb7c
Pantelis Roditis <proditis@echothrust.com>
2025-12-01 09:55:32 +0200

    add route for private network spin

--- backend/migrations/m251126_122227_add_private_network_url_route.php
+++ backend/migrations/m251126_122227_add_private_network_url_route.php
@@ -4,21 +4,20 @@ use yii\db\Migration;
 
 class m251126_122227_add_private_network_url_route extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-      $this->upsert('url_route', ['source' => 'network/private/<id:\d+>', 'destination' => 'network/private/view', 'weight' => 631], true);
+  /**
+   * {@inheritdoc}
+   */
+  public function safeUp()
+  {
+    $this->upsert('url_route', ['source' => 'network/private/<id:\d+>', 'destination' => 'network/private/view', 'weight' => 631], true);
+    $this->upsert('url_route', ['source' => 'network/private/spin/<network_id:\d+>/<target_id:\d+>', 'destination' => 'network/private/spin', 'weight' => 632], true);
+  }
 
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        echo "m251126_122227_add_private_network_url_route cannot be reverted.\n";
-    }
-
+  /**
+   * {@inheritdoc}
+   */
+  public function safeDown()
+  {
+    echo "m251126_122227_add_private_network_url_route cannot be reverted.\n";
+  }
 }
