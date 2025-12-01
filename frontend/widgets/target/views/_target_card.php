aacb24fa8aebe50711e7dd772f967de7b2c3305f
Pantelis Roditis <proditis@echothrust.com>
2025-12-01 21:17:39 +0200

    disable updates of IP's and change span into div

--- frontend/widgets/target/views/_target_card.php
+++ frontend/widgets/target/views/_target_card.php
@@ -1,3 +1,4 @@
+<!-- //_target_card -->
 <?php
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -12,7 +13,7 @@ if($model->ipoctet==='0.0.0.0' || $model->ipoctet === NULL)
 {
   $model_ip="0.0.0.0";
   $display_ip=Html::tag('b',$model_ip,['data-toggle'=>'tooltip','title'=>\Yii::t('app',"The IP will be visible once the system is powered up.")]);
-  $this->registerJs("targetUpdates({$model->id});", \yii\web\View::POS_READY);
+  //$this->registerJs("targetUpdates({$model->id});", \yii\web\View::POS_READY);
 }
 
 $subtitleARR=[$model->category,ucfirst($model->getDifficultyText($model->average_rating)),boolval($model->rootable) ? "Rootable" : "Non rootable",$model->timer===false ? null:'Timed'];
@@ -25,7 +26,7 @@ Card::begin([
             'color'=>'target',
             'subtitle'=>$subtitle,
             'title'=>sprintf('%s / <span id="target_id">%s</span>', $model->name, $display_ip),
-            'footer'=>sprintf('<div class="stats">%s</div><span class="card_actions">%s</span>', $model->purpose, TargetCardActions::widget(['model'=>$model,'identity'=>$identity]) ),
+            'footer'=>sprintf('<div class="stats">%s</div><div class="card_actions">%s</div>', $model->purpose, TargetCardActions::widget(['model'=>$model,'identity'=>$identity]) ),
         ]);
 echo "<p class='text-danger'><i class='fas fa-flag'></i> ", $model->total_treasures, ": Flag".($model->total_treasures > 1 ? 's' : '')." ";
 echo  "<small>(<code class='text-danger'>";
@@ -37,4 +38,5 @@ if($model->timer!==false && $model->timer_avg>0)
 echo "</p>";
 ?>
 <p><?= $model->description ?></p>
-<?php Card::end();
+<?php Card::end();?>
+<!-- //_target_card -->
