ea28cced664b6ee780c0e91fda6be7d182b8e66f
Pantelis Roditis <proditis@echothrust.com>
2025-12-05 11:36:39 +0200

    add named pending_action query  for private targets

--- backend/modules/infrastructure/models/PrivateNetworkTargetQuery.php
+++ backend/modules/infrastructure/models/PrivateNetworkTargetQuery.php
@@ -9,26 +9,44 @@ namespace app\modules\infrastructure\models;
  */
 class PrivateNetworkTargetQuery extends \yii\db\ActiveQuery
 {
-    /*public function active()
-    {
-        return $this->andWhere('[[status]]=1');
-    }*/
 
-    /**
-     * {@inheritdoc}
-     * @return PrivateNetworkTarget[]|array
-     */
-    public function all($db = null)
-    {
-        return parent::all($db);
-    }
+  public function pending_action()
+  {
+    return $this->andWhere([
+      'and',
+      [
+        'or', // Conditions to match the records you want to keep
+        ['is', 'ipoctet', null],
+        ['ipoctet' => "0.0.0.0"],
+        ['is', 'server_id', null],
+        ['>', 'state', 0]
+      ],
+      [
+        'not',
+        [
+          'and', // Exclude records where state = 2 and server_id is null
+          ['state' => 2],
+          ['is', 'server_id', null]
+        ]
+      ]
+    ]);
+  }
 
-    /**
-     * {@inheritdoc}
-     * @return PrivateNetworkTarget|array|null
-     */
-    public function one($db = null)
-    {
-        return parent::one($db);
-    }
+  /**
+   * {@inheritdoc}
+   * @return PrivateNetworkTarget[]|array
+   */
+  public function all($db = null)
+  {
+    return parent::all($db);
+  }
+
+  /**
+   * {@inheritdoc}
+   * @return PrivateNetworkTarget|array|null
+   */
+  public function one($db = null)
+  {
+    return parent::one($db);
+  }
 }
