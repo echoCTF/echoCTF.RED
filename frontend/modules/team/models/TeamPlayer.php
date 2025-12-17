de1925cd181bddcb00f43ec781cfb225c683dde4
Pantelis Roditis <proditis@echothrust.com>
2025-12-17 19:08:21 +0200

    make it clear that is player also format the code

--- frontend/modules/team/models/TeamPlayer.php
+++ frontend/modules/team/models/TeamPlayer.php
@@ -21,98 +21,100 @@ use app\models\Player;
  */
 class TeamPlayer extends \yii\db\ActiveRecord
 {
-    /**
-     * {@inheritdoc}
-     */
-    public static function tableName()
-    {
-        return 'team_player';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function tableName()
+  {
+    return 'team_player';
+  }
 
-    public function behaviors()
-    {
-      return [
-          [
-              'class' => TimestampBehavior::class,
-              'createdAtAttribute' => 'ts',
-              'updatedAtAttribute' => 'ts',
-              'value' => new Expression('NOW()'),
-          ],
-      ];
-    }
+  public function behaviors()
+  {
+    return [
+      [
+        'class' => TimestampBehavior::class,
+        'createdAtAttribute' => 'ts',
+        'updatedAtAttribute' => 'ts',
+        'value' => new Expression('NOW()'),
+      ],
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['team_id', 'player_id'], 'required'],
-            [['team_id', 'player_id', 'approved'], 'integer'],
-            [['player_id'], 'unique'],
-            [['team_id', 'player_id'], 'unique', 'targetAttribute' => ['team_id', 'player_id']],
-            [['team_id'], 'exist', 'skipOnError' => true, 'targetClass' => Team::class, 'targetAttribute' => ['team_id' => 'id']],
-            [['player_id'], 'exist', 'skipOnError' => true, 'targetClass' => Player::class, 'targetAttribute' => ['player_id' => 'id']],
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['team_id', 'player_id'], 'required'],
+      [['team_id', 'player_id', 'approved'], 'integer'],
+      [['player_id'], 'unique'],
+      [['team_id', 'player_id'], 'unique', 'targetAttribute' => ['team_id', 'player_id']],
+      [['team_id'], 'exist', 'skipOnError' => true, 'targetClass' => Team::class, 'targetAttribute' => ['team_id' => 'id']],
+      [['player_id'], 'exist', 'skipOnError' => true, 'targetClass' => Player::class, 'targetAttribute' => ['player_id' => 'id']],
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'team_id' => 'Team ID',
-            'player_id' => 'Player ID',
-            'approved' => 'Approved',
-            'ts' => 'Ts',
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function attributeLabels()
+  {
+    return [
+      'id' => 'ID',
+      'team_id' => 'Team ID',
+      'player_id' => 'Player ID',
+      'approved' => 'Approved',
+      'ts' => 'Ts',
+    ];
+  }
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getTeam()
-    {
-        return $this->hasOne(Team::class, ['id' => 'team_id']);
-    }
+  /**
+   * @return \yii\db\ActiveQuery
+   */
+  public function getTeam()
+  {
+    return $this->hasOne(Team::class, ['id' => 'team_id']);
+  }
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getPlayer()
-    {
-        return $this->hasOne(Player::class, ['id' => 'player_id']);
-    }
-    private function sendNotification($p,$msg="")
-    {
-      $p->notify('info',$msg,$msg);
-      return true;
-    }
+  /**
+   * @return \yii\db\ActiveQuery
+   */
+  public function getPlayer()
+  {
+    return $this->hasOne(Player::class, ['id' => 'player_id']);
+  }
 
-    public function notifyJoinOwner()
-    {
-      $msg=sprintf(\Yii::t('app','Hi there, [%s] just joined your team. Go to your team page and approve the player.'),$this->player->username);
-      return $this->sendNotification($this->team->owner,$msg);
-    }
-    public function notifyPartOwner()
-    {
-      $msg=sprintf(\Yii::t('app','Hi there, [%s] just left your team.'),$this->player->username);
-      return $this->sendNotification($this->team->owner,$msg);
-    }
+  private function sendNotification($player, $msg = "")
+  {
+    $player->notify('info', $msg, $msg);
+    return true;
+  }
 
-    public function notifyRejectPlayer()
-    {
-      // Don't notify the players for deleting their own team
-      if($this->player_id===$this->team->owner_id) return;
-      $msg=\Yii::t('app','Hi there, your team membership got rejected. Find another team to join.');
-      return $this->sendNotification($this->player,$msg);
-    }
-    public function notifyApprovePlayer()
-    {
-      $msg=\Yii::t('app','Hi there, your team membership got approved.');
-      return $this->sendNotification($this->player,$msg);
-    }
+  public function notifyJoinOwner()
+  {
+    $msg = sprintf(\Yii::t('app', 'Hi there, [%s] just joined your team. Go to your team page and approve the player.'), $this->player->username);
+    return $this->sendNotification($this->team->owner, $msg);
+  }
 
+  public function notifyPartOwner()
+  {
+    $msg = sprintf(\Yii::t('app', 'Hi there, [%s] just left your team.'), $this->player->username);
+    return $this->sendNotification($this->team->owner, $msg);
+  }
+
+  public function notifyRejectPlayer()
+  {
+    // Don't notify the players for deleting their own team
+    if ($this->player_id === $this->team->owner_id) return;
+    $msg = \Yii::t('app', 'Hi there, your team membership got rejected. Find another team to join.');
+    return $this->sendNotification($this->player, $msg);
+  }
+
+  public function notifyApprovePlayer()
+  {
+    $msg = \Yii::t('app', 'Hi there, your team membership got approved.');
+    return $this->sendNotification($this->player, $msg);
+  }
 }
