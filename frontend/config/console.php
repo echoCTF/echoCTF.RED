201a4f7a4f2e79fe509fe90111bbd269d80ac5d4
Pantelis Roditis <proditis@echothrust.com>
2026-01-14 14:18:57 +0200

    team module required for badge generation on team events

--- frontend/config/console.php
+++ frontend/config/console.php
@@ -1,54 +1,60 @@
 <?php
 
-$params=require __DIR__.'/params.php';
-$db=require __DIR__.'/db.php';
-$cache=require __DIR__.'/cache.php';
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+$cache = require __DIR__ . '/cache.php';
 
-$config=[
-    'id' => 'basic-console',
-//    'language' => 'el-GR',
-    'sourceLanguage' => 'en-US',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'app\commands',
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-        '@tests' => '@app/tests',
+$config = [
+  'id' => 'basic-console',
+  //    'language' => 'el-GR',
+  'sourceLanguage' => 'en-US',
+  'basePath' => dirname(__DIR__),
+  'bootstrap' => ['log'],
+  'controllerNamespace' => 'app\commands',
+  'aliases' => [
+    '@bower' => '@vendor/bower-asset',
+    '@npm'   => '@vendor/npm-asset',
+    '@tests' => '@app/tests',
+  ],
+  'modules' => [
+    'team' => [
+      'class' => 'app\modules\team\Module',
     ],
-    'components' => [
-        'i18n' => [
-          'translations' => [
-              'yii' => [
-              'class' => 'yii\i18n\PhpMessageSource',
-              ],
-              'app*' => [
-                  'class' => 'yii\i18n\PhpMessageSource',
-                  'basePath' => '@app/messages',
-                  'sourceLanguage' => 'en-US',
-                  'fileMap' => [
-                      'app' => 'app.php',
-                      'app/error' => 'error.php',
-                  ],
-              ],
+  ],
+
+  'components' => [
+    'i18n' => [
+      'translations' => [
+        'yii' => [
+          'class' => 'yii\i18n\PhpMessageSource',
+        ],
+        'app*' => [
+          'class' => 'yii\i18n\PhpMessageSource',
+          'basePath' => '@app/messages',
+          'sourceLanguage' => 'en-US',
+          'fileMap' => [
+            'app' => 'app.php',
+            'app/error' => 'error.php',
           ],
         ],
-        'sys'=> [
-          'class' => 'app\components\Sysconfig',
-        ],
-        'cache' => $cache,
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
+      ],
     ],
-    'params' => $params,
-    /*
+    'sys' => [
+      'class' => 'app\components\Sysconfig',
+    ],
+    'cache' => $cache,
+    'log' => [
+      'targets' => [
+        [
+          'class' => 'yii\log\FileTarget',
+          'levels' => ['error', 'warning'],
+        ],
+      ],
+    ],
+    'db' => $db,
+  ],
+  'params' => $params,
+  /*
     'controllerMap' => [
         'fixture' => [ // Fixture generation command line.
             'class' => 'yii\faker\FixtureController',
@@ -57,13 +63,12 @@ $config=[
     */
 ];
 
-if(YII_ENV_DEV)
-{
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][]='gii';
-    $config['modules']['gii']=[
-        'class' => 'yii\gii\Module',
-    ];
+if (YII_ENV_DEV) {
+  // configuration adjustments for 'dev' environment
+  $config['bootstrap'][] = 'gii';
+  $config['modules']['gii'] = [
+    'class' => 'yii\gii\Module',
+  ];
 }
 
 return $config;
