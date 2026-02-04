4169af7826df9f32196f6550f18ff7e269e1f29d
Pantelis Roditis <proditis@echothrust.com>
2026-02-04 13:31:42 +0200

    introduce event_shutdown script

--- contrib/event_shutdown.sh
+++ contrib/event_shutdown.sh
@@ -2,5 +2,7 @@
 PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/X11R6/bin:/usr/local/sbin:/usr/local/bin
 rcctl stop openvpn findingsd heartbeatd inetd cron
 supervisorctl stop all
+backend vpn/killall
+backend vpn/logoutall
 backend target/destroy-instances
 ifconfig tun0 down
