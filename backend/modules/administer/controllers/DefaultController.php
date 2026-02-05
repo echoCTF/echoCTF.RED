bd23f133b8622e74baf5434340dc80b629865080
Pantelis Roditis <proditis@echothrust.com>
2026-02-06 00:25:45 +0200

    fix behavior for default controller

--- backend/modules/administer/controllers/DefaultController.php
+++ backend/modules/administer/controllers/DefaultController.php
@@ -2,19 +2,39 @@
 
 namespace app\modules\administer\controllers;
 
-use yii\web\Controller;
+use Yii;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+use yii\helpers\ArrayHelper;
+use yii\filters\VerbFilter;
 
 /**
  * Default controller for the `administer` module
  */
-class DefaultController extends Controller
+class DefaultController extends \app\components\BaseController
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+  public function behaviors()
+  {
+    return ArrayHelper::merge(parent::behaviors(), [
+      'access' => [
+        'class' => \yii\filters\AccessControl::class,
+        'rules' => [
+          'authActions' => [
+            'allow' => \Yii::$app->user->identity && \Yii::$app->user->identity->isAdmin,
+            'actions' => ['index'],
+            'roles' => ['@'],
+          ],
+        ],
+      ],
+    ]);
+  }
+
+  /**
+   * Renders the index view for the module
+   * @return string
+   */
+  public function actionIndex()
+  {
+    return $this->render('index');
+  }
 }
