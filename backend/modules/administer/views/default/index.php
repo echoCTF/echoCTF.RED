07031f98f2f0b41a73529360f6ec0a34aacc9c41
Pantelis Roditis <proditis@echothrust.com>
2026-02-06 00:25:59 +0200

    update the default view

--- backend/modules/administer/views/default/index.php
+++ backend/modules/administer/views/default/index.php
@@ -1,12 +1,6 @@
 <div class="administer-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+  <h1>Administer</h1>
+  <p>
+    Administer differrent operations of your platform installation.
+  </p>
 </div>
