028d683cc1cd6750e5c5044f118b1934c4d2792d
Pantelis Roditis <proditis@echothrust.com>
2026-02-05 21:11:15 +0200

    add backend administer module

---
+++ backend/modules/administer/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\administer;
+
+/**
+ * administer module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\administer\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
