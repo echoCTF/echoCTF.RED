4ad77a82c4a7a78a809e64f6c39201dec27c867c
Pantelis Roditis <proditis@echothrust.com>
2026-02-05 11:16:00 +0200

    document methods and implement `server_assigned()` method

--- backend/modules/infrastructure/models/TargetInstanceQuery.php
+++ backend/modules/infrastructure/models/TargetInstanceQuery.php
@@ -9,9 +9,18 @@ namespace app\modules\infrastructure\models;
  */
 class TargetInstanceQuery extends \yii\db\ActiveQuery
 {
+
   /**
-   * Filters TargetInstances to those that are active
-   * and whose team has at least one approved member with vpn_local_address != 0
+   * Filters target instances that are active and have at least one approved team member
+   * with a non-null VPN local address.
+   *
+   * Joins:
+   * - `team_player` (`tp`) to match the instance's player and check approval.
+   * - `team` (`t`) to get the player's team.
+   * - `team_player` (`am`) to include other approved members of the team.
+   * - `player_last` (`al`) to ensure at least one approved member has a VPN local address.
+   *
+   * @return TargetInstanceQuery
    */
   public function withApprovedMemberHeartbeat()
   {
@@ -28,16 +37,60 @@ class TargetInstanceQuery extends \yii\db\ActiveQuery
       ->distinct();
   }
 
+  /**
+   * Filters target instances that are active.
+   *
+   * Active instances:
+   * - Have a non-null IP address.
+   * - Are not marked for destruction (`reboot != 2`).
+   *
+   * @return TargetInstanceQuery
+   */
   public function active()
   {
     return $this->andWhere('target_instance.[[ip]] IS NOT NULL')->andWhere('target_instance.[[reboot]]!=2');
   }
 
+  /**
+   * Filters target instances last updated more than the given number of seconds ago.
+   *
+   * @param int $seconds_ago Minimum age in seconds since last update.
+   * @return TargetInstanceQuery
+   */
   public function last_updated(int $seconds_ago = 1)
   {
     return $this->andWhere(['<', 'target_instance.[[updated_at]]', new \yii\db\Expression("NOW() - INTERVAL $seconds_ago SECOND")]);
   }
 
+  /**
+   * Filters target instances by assigned server.
+   *
+   * - If `$server_id` is 0, matches instances assigned to any server.
+   * - Otherwise, matches instances assigned to the specified server.
+   *
+   * @param int $server_id Server ID to filter by, or 0 for any assigned server.
+   * @return TargetInstanceQuery
+   */
+  public function server_assigned(int $server_id = 0)
+  {
+    if ($server_id === 0)
+      return $this->andWhere(['IS NOT', 'target_instance.server_id', null]);
+    return $this->andWhere(['target_instance.server_id' => $server_id]);
+  }
+
+  /**
+   * Selects target instances that require pending actions.
+   *
+   * Pending action criteria:
+   * - IP address is null.
+   * - `reboot` is greater than 0.
+   * - Last updated more than `$seconds_ago` seconds ago.
+   *
+   * Overrides `reboot` to 2 (destroy) if the instance hasn't been updated within `$seconds_ago` seconds.
+   *
+   * @param int $seconds_ago Threshold in seconds to consider an instance outdated.
+   * @return TargetInstanceQuery
+   */
   public function pending_action(int $seconds_ago = 1)
   {
     return $this->addSelect([
