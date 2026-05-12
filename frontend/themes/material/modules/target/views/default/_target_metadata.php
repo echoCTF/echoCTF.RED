26da5be713267c35b76dce3f528a0d55eb7d4309
Pantelis Roditis <proditis@echothrust.com>
2026-05-12 11:32:06 +0300

    Convert metadata to summary scenario, instructions and solution is not part of the details that are visible to everyone

--- frontend/themes/material/modules/target/views/default/_target_metadata.php
+++ frontend/themes/material/modules/target/views/default/_target_metadata.php
@@ -1,9 +1,14 @@
 <?php if(!Yii::$app->user->isGuest && $metadata):?>
   <?php $oldId=\Yii::$app->formatter->divID; ?>
-  <?php if(Yii::$app->user->identity->isAdmin):?>
-    <?php if(!empty($metadata->scenario)):?><b><?=\Yii::t('app','Scenario')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-scenario'; echo \Yii::$app->formatter->asMarkdown($metadata->scenario)?><?php endif;?>
-    <?php if(!empty($metadata->instructions)):?><b><?=\Yii::t('app','Instructions')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-instructions'; echo \Yii::$app->formatter->asMarkdown($metadata->instructions)?><?php endif;?>
-    <?php if(!empty($metadata->solution)):?><b><?=\Yii::t('app','Solution')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-solution'; echo \Yii::$app->formatter->asMarkdown($metadata->solution)?><?php endif;?>
+  <?php if(Yii::$app->user->identity->isAdmin && (!empty($metadata->scenario) || !empty($metadata->instructions) || !empty($metadata->solution))):?>
+    <details>
+      <summary>
+      Administrator
+      </summary>
+      <?php if(!empty($metadata->scenario)):?><b><?=\Yii::t('app','Scenario')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-scenario'; echo \Yii::$app->formatter->asMarkdown($metadata->scenario)?><?php endif;?>
+      <?php if(!empty($metadata->instructions)):?><b><?=\Yii::t('app','Instructions')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-instructions'; echo \Yii::$app->formatter->asMarkdown($metadata->instructions)?><?php endif;?>
+      <?php if(!empty($metadata->solution)):?><b><?=\Yii::t('app','Solution')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-solution'; echo \Yii::$app->formatter->asMarkdown($metadata->solution)?><?php endif;?>
+    </details>
   <?php endif;?>
   <?php if(!empty($metadata->pre_credits)):?><b><?=\Yii::t('app','Pre exploitation credits')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-pre-credits'; echo \Yii::$app->formatter->asMarkdown($metadata->pre_credits)?><?php endif;?>
   <?php if(!empty($metadata->pre_exploitation)):?><b><?=\Yii::t('app','Pre exploitation details')?></b>: <?php \Yii::$app->formatter->divID = 'markdown-pre-exploitation'; echo \Yii::$app->formatter->asMarkdown($metadata->pre_exploitation)?><?php endif;?>
