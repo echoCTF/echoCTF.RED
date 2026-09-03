f77e42339f6c296b3e5edcdf5595e14357922b6a
Pantelis Roditis <proditis@echothrust.com>
2026-09-03 21:16:32 +0300

    add player autocomplete to the token form

--- backend/modules/activity/views/ws-token/_form.php
+++ backend/modules/activity/views/ws-token/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\widgets\sleifer\autocompleteAjax\AutocompleteAjax;
 
 /** @var yii\web\View $this */
 /** @var app\modules\activity\models\WsToken $model */
@@ -12,9 +13,14 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'token')->textInput() ?>
 
-    <?= $form->field($model, 'player_id')->textInput() ?>
+    <?= $form->field($model, 'player_id')->widget(AutocompleteAjax::class, [
+      'multiple' => false,
+      'url' => ['/frontend/player/ajax-search'],
+      'options' => ['placeholder' => 'Find player by email, username, id or profile.']
+      ])->hint('The player that the token will belong to.');  ?>
+
+    <?= $form->field($model, 'token')->textInput() ?>
 
     <?= $form->field($model, 'subject_id')->textInput() ?>
 
