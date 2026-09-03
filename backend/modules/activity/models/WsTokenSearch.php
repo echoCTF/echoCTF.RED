70ca20ff8b89ede3a9bd45df404e1fc3013535e3
Pantelis Roditis <proditis@echothrust.com>
2026-09-03 21:15:55 +0300

    add username to the search and order

--- backend/modules/activity/models/WsTokenSearch.php
+++ backend/modules/activity/models/WsTokenSearch.php
@@ -11,62 +11,78 @@ use app\modules\activity\models\WsToken;
  */
 class WsTokenSearch extends WsToken
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['token', 'subject_id', 'expires_at'], 'safe'],
-            [['player_id', 'is_server'], 'integer'],
-        ];
+  public $username;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['token', 'subject_id', 'expires_at', 'username'], 'safe'],
+      [['player_id', 'is_server'], 'integer'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function scenarios()
+  {
+    // bypass scenarios() implementation in the parent class
+    return Model::scenarios();
+  }
+
+  /**
+   * Creates data provider instance with search query applied
+   *
+   * @param array $params
+   * @param string|null $formName Form name to be used into `->load()` method.
+   *
+   * @return ActiveDataProvider
+   */
+  public function search($params, $formName = null)
+  {
+    $query = WsToken::find()->joinWith(['player']);
+
+    // add conditions that should always apply here
+
+    $dataProvider = new ActiveDataProvider([
+      'query' => $query,
+    ]);
+
+    $this->load($params, $formName);
+
+    if (!$this->validate()) {
+      // uncomment the following line if you do not want to return any records when validation fails
+      // $query->where('0=1');
+      return $dataProvider;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+    // grid filtering conditions
+    $query->andFilterWhere([
+      'player_id' => $this->player_id,
+      'is_server' => $this->is_server,
+      'expires_at' => $this->expires_at,
+    ]);
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     * @param string|null $formName Form name to be used into `->load()` method.
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params, $formName = null)
-    {
-        $query = WsToken::find();
+    $query->andFilterWhere(['like', 'token', $this->token])
+      ->andFilterWhere(['like', 'subject_id', $this->subject_id])
+      ->andFilterWhere(['like', 'player.username', $this->username]);
 
-        // add conditions that should always apply here
+    $dataProvider->setSort([
+      'defaultOrder' => ['username' => SORT_DESC],
+      'attributes' => array_merge(
+        $dataProvider->getSort()->attributes,
+        [
+          'username' => [
+            'asc' => ['player.username' => SORT_ASC],
+            'desc' => ['player.username' => SORT_DESC],
+          ],
+        ]
+      ),
+    ]);
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params, $formName);
-
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'player_id' => $this->player_id,
-            'is_server' => $this->is_server,
-            'expires_at' => $this->expires_at,
-        ]);
-
-        $query->andFilterWhere(['like', 'token', $this->token])
-            ->andFilterWhere(['like', 'subject_id', $this->subject_id]);
-
-        return $dataProvider;
-    }
+    return $dataProvider;
+  }
 }
