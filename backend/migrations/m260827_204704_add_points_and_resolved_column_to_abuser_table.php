a6e2f6594fcc8d26c34e323742b873ecb957c9d3
Pantelis Roditis <proditis@echothrust.com>
2026-09-12 13:40:24 +0300

    add points and resolved status

--- backend/migrations/m260827_204704_add_points_and_resolved_column_to_abuser_table.php
+++ backend/migrations/m260827_204704_add_points_and_resolved_column_to_abuser_table.php
@@ -7,21 +7,21 @@ use yii\db\Migration;
  */
 class m260827_204704_add_points_and_resolved_column_to_abuser_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-        $this->addColumn('abuser', 'points', $this->integer()->notNull()->defaultValue(0));
-        $this->addColumn('abuser', 'resolved', $this->boolean()->notNull()->defaultValue(false));
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function safeUp()
+  {
+    $this->addColumn('abuser', 'points', $this->integer()->notNull()->defaultValue(0)->after('model_id'));
+    $this->addColumn('abuser', 'resolved', $this->boolean()->notNull()->defaultValue(0)->after('points'));
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        $this->dropColumn('abuser', 'resolved');
-        $this->dropColumn('abuser', 'points');
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function safeDown()
+  {
+    $this->dropColumn('abuser', 'resolved');
+    $this->dropColumn('abuser', 'points');
+  }
 }
