213251e3baa7e8b25fc078cbf9e61cf2ae0d35b6
Pantelis Roditis <proditis@echothrust.com>
2026-09-13 10:45:41 +0300

    add proper filtering by username and IP

--- backend/modules/activity/models/PlayerBandwidthSearch.php
+++ backend/modules/activity/models/PlayerBandwidthSearch.php
@@ -5,69 +5,111 @@ namespace app\modules\activity\models;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\modules\activity\models\PlayerBandwidth;
+use app\components\validators\ExtendedIpValidator;
 
 /**
  * PlayerBandwidthSearch represents the model behind the search form of `app\modules\activity\models\PlayerBandwidth`.
  */
 class PlayerBandwidthSearch extends PlayerBandwidth
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['id', 'player_id', 'vpn_local_address', 'bytes_received', 'bytes_sent', 'duration'], 'integer'],
-            [['ts'], 'safe'],
-        ];
+  public $username;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['id', 'player_id', 'bytes_received', 'bytes_sent', 'duration'], 'integer'],
+      [['vpn_local_address','username'],'filter','filter'=>'trim'],
+      [['ts','username','vpn_local_address'], 'safe'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function scenarios()
+  {
+    // bypass scenarios() implementation in the parent class
+    return Model::scenarios();
+  }
+
+  /**
+   * Creates data provider instance with search query applied
+   *
+   * @param array $params
+   * @param string|null $formName Form name to be used into `->load()` method.
+   *
+   * @return ActiveDataProvider
+   */
+  public function search($params, $formName = null)
+  {
+    $validator = new ExtendedIpValidator(['subnet' => true, 'expandIPv6' => false]);
+
+    $query = PlayerBandwidth::find()->joinWith(['player']);
+
+    // add conditions that should always apply here
+
+    $dataProvider = new ActiveDataProvider([
+      'query' => $query,
+      'sort'=> ['defaultOrder' => ['ts' => SORT_DESC]],
+    ]);
+
+    $this->load($params, $formName);
+
+    if (!$this->validate()) {
+      // uncomment the following line if you do not want to return any records when validation fails
+      // $query->where('0=1');
+      return $dataProvider;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function scenarios()
+    // grid filtering conditions
+    $query->andFilterWhere([
+      'id' => $this->id,
+      'player_id' => $this->player_id,
+      'bytes_received' => $this->bytes_received,
+      'bytes_sent' => $this->bytes_sent,
+      'duration' => $this->duration,
+      'ts' => $this->ts,
+    ]);
+    if ($validator->validate($this->vpn_local_address) !== false)
     {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
+      [$ip, $mask] = explode('/', $this->vpn_local_address, 2);
+      if (filter_var($mask, FILTER_VALIDATE_IP)) {
+        // Netmask style, keep as is
+          $netmask = $mask;
+      } else {
+        // Prefix length, convert to dotted netmask
+        $prefix = (int)$mask;
+        $netmaskLong = (~((1 << (32 - $prefix)) - 1)) & 0xFFFFFFFF;
+        $netmask = long2ip($netmaskLong);
+      }
+      $query->andFilterWhere(['=',new \yii\db\Expression('(vpn_local_address & inet_aton(:netmask))',[':netmask'=>$netmask]),new \yii\db\Expression('(inet_aton(:ip) & inet_aton(:netmask))',[':ip'=>$ip,':netmask'=>$netmask])]);
+    }
+    elseif (filter_var($this->vpn_local_address, FILTER_VALIDATE_IP))
+    {
+      $query->andFilterWhere(['vpn_local_address'=>ip2long($this->vpn_local_address)]);
+    }
+    else
+    {
+      $query->andFilterWhere(['like', 'INET_NTOA(vpn_local_address)', $this->vpn_local_address]);
     }
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     * @param string|null $formName Form name to be used into `->load()` method.
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params, $formName = null)
-    {
-        $query = PlayerBandwidth::find();
+    $query->andFilterWhere(['like', 'player.username', $this->username]);
 
-        // add conditions that should always apply here
+    $dataProvider->setSort([
+      'attributes' => array_merge(
+        $dataProvider->getSort()->attributes,
+        [
+          'username' => [
+            'asc' => ['player.username' => SORT_ASC],
+            'desc' => ['player.username' => SORT_DESC],
+          ],
+        ]
+      ),
+    ]);
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params, $formName);
-
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'player_id' => $this->player_id,
-            'vpn_local_address' => $this->vpn_local_address,
-            'bytes_received' => $this->bytes_received,
-            'bytes_sent' => $this->bytes_sent,
-            'duration' => $this->duration,
-            'ts' => $this->ts,
-        ]);
-
-        return $dataProvider;
-    }
+    return $dataProvider;
+  }
 }
